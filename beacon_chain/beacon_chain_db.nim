7e4d2648873138e6cfa3620b9b4243bef554328c
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2019-01-14 14:19:44 +0200

    Back the db by rocksdb backend

--- beacon_chain/beacon_chain_db.nim
+++ beacon_chain/beacon_chain_db.nim
@@ -1,32 +1,63 @@
 import
-  os, json, tables,
+  os, json, tables, options,
   chronicles, json_serialization, eth_common/eth_types_json_serialization,
-  spec/[datatypes, digest, crypto]
+  spec/[datatypes, digest, crypto],
+  eth_trie/db, ssz
 
 type
   BeaconChainDB* = ref object
-    dataRoot: string
-    blocks*: Table[Eth2Digest, BeaconBlock]
+    backend: TrieDatabaseRef
 
   BeaconStateRef* = ref BeaconState
 
-proc init*(T: type BeaconChainDB, dataDir: string): BeaconChainDB =
+  DbKey = object
+    data: array[33, byte]
+    dataEndPos: uint8
+
+  DbKeyKind = enum
+    kLastFinalizedState
+    kHashToBlock
+
+
+template toOpenArray*(k: DbKey): openarray[byte] =
+  k.data.toOpenArray(0, int(k.dataEndPos))
+
+proc lastFinalizedStateKey(): DbKey =
+  result.data[0] = byte ord(kLastFinalizedState)
+  result.dataEndPos = 0
+
+proc hashToBlockKey(h: Eth2Digest): DbKey =
+  result.data[0] = byte ord(kHashToBlock)
+  result.data[1 .. ^1] = h.data
+  result.dataEndPos = 32
+
+proc init*(T: type BeaconChainDB, backend: TrieDatabaseRef): BeaconChainDB =
   new result
-  result.dataRoot = dataDir / "beacon_db"
-  createDir(result.dataRoot)
+  result.backend = backend
 
 proc lastFinalizedState*(db: BeaconChainDB): BeaconStateRef =
   try:
-    let stateFile = db.dataRoot / "BeaconState.json"
-    if fileExists stateFile:
-      new result
-      # TODO serialization error: Json.loadFile(stateFile, result[])
+    let res = db.backend.get(lastFinalizedStateKey().toOpenArray)
+    if res.len != 0:
+      result.new
+      result[] = ssz.deserialize(res, BeaconState).get
   except:
     error "Failed to load the latest finalized state",
           err = getCurrentExceptionMsg()
     return nil
 
 proc persistBlock*(db: BeaconChainDB, s: BeaconState, b: BeaconBlock) =
-  Json.saveFile(db.dataRoot / "BeaconState.json", s, pretty = true)
-  debug "State persisted"
+  db.backend.put(lastFinalizedStateKey().toOpenArray, ssz.serialize(s))
+
+proc getBlock*(db: BeaconChainDB, hash: Eth2Digest, output: var BeaconBlock): bool =
+  let res = db.backend.get(hashToBlockKey(hash).toOpenArray)
+  if res.len != 0:
+    output = ssz.deserialize(res, BeaconBlock).get
+    true
+  else:
+    false
+
+proc getBlock*(db: BeaconChainDB, hash: Eth2Digest): BeaconBlock =
+  if not db.getBlock(hash, result):
+    raise newException(Exception, "Block not found")
 
