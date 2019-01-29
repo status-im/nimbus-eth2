1606bc6f92670e2addaf7d10f5625e4606050c31
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2019-01-29 12:13:26 +0200

    Cosmetics

--- beacon_chain/beacon_chain_db.nim
+++ beacon_chain/beacon_chain_db.nim
@@ -10,10 +10,6 @@ type
   BeaconChainDB* = ref object
     backend: TrieDatabaseRef
 
-  DbKey = object
-    data: array[33, byte]
-    dataEndPos: uint8
-
   DbKeyKind = enum
     kLastFinalizedState
     kHashToBlock
@@ -21,46 +17,37 @@ type
     kSlotToState
     kHashToValidatorRegistryChangeLog
 
+proc lastFinalizedStateKey(): array[1, byte] =
+  result[0] = byte ord(kLastFinalizedState)
 
-template toOpenArray*(k: DbKey): openarray[byte] =
-  k.data.toOpenArray(0, int(k.dataEndPos))
+proc hashToBlockKey(h: Eth2Digest): array[32 + 1, byte] =
+  result[0] = byte ord(kHashToBlock)
+  result[1 .. ^1] = h.data
 
-proc lastFinalizedStateKey(): DbKey =
-  result.data[0] = byte ord(kLastFinalizedState)
-  result.dataEndPos = 0
+proc slotToBlockHashKey(s: uint64): array[sizeof(uint64) + 1, byte] =
+  result[0] = byte ord(kSlotToBlockHash)
+  copyMem(addr result[1], unsafeAddr(s), sizeof(s))
 
-proc hashToBlockKey(h: Eth2Digest): DbKey =
-  result.data[0] = byte ord(kHashToBlock)
-  result.data[1 .. ^1] = h.data
-  result.dataEndPos = 32
+proc slotToStateKey(s: uint64): array[sizeof(uint64) + 1, byte] =
+  result[0] = byte ord(kSlotToState)
+  copyMem(addr result[1], unsafeAddr(s), sizeof(s))
 
-proc slotToBlockHashKey(s: uint64): DbKey =
-  result.data[0] = byte ord(kSlotToBlockHash)
-  copyMem(addr result.data[1], unsafeAddr(s), sizeof(s))
-  result.dataEndPos = uint8 sizeof(s)
-
-proc slotToStateKey(s: uint64): DbKey =
-  result.data[0] = byte ord(kSlotToState)
-  copyMem(addr result.data[1], unsafeAddr(s), sizeof(s))
-  result.dataEndPos = uint8 sizeof(s)
-
-proc hashToValidatorRegistryChangeLogKey(deltaChainTip: Eth2Digest): DbKey =
-  result.data[0] = byte ord(kHashToValidatorRegistryChangeLog)
-  result.data[1 .. ^1] = deltaChainTip.data
-  result.dataEndPos = 32
+proc hashToValidatorRegistryChangeLogKey(deltaChainTip: Eth2Digest): array[32 + 1, byte] =
+  result[0] = byte ord(kHashToValidatorRegistryChangeLog)
+  result[1 .. ^1] = deltaChainTip.data
 
 proc init*(T: type BeaconChainDB, backend: TrieDatabaseRef): BeaconChainDB =
   new result
   result.backend = backend
 
 proc lastFinalizedState*(db: BeaconChainDB): BeaconState =
-  let res = db.backend.get(lastFinalizedStateKey().toOpenArray)
+  let res = db.backend.get(lastFinalizedStateKey())
   if res.len == 0:
     raise newException(Exception, "Internal error: Database has no finalized state")
   ssz.deserialize(res, BeaconState).get
 
 proc isInitialized*(db: BeaconChainDB): bool =
-  db.backend.get(lastFinalizedStateKey().toOpenArray).len != 0
+  db.backend.get(lastFinalizedStateKey()).len != 0
 
 proc persistState*(db: BeaconChainDB, s: BeaconState) =
   if s.slot != GENESIS_SLOT:
@@ -79,11 +66,11 @@ proc persistState*(db: BeaconChainDB, s: BeaconState) =
       discard
 
   let serializedState = ssz.serialize(s)
-  db.backend.put(lastFinalizedStateKey().toOpenArray, serializedState)
+  db.backend.put(lastFinalizedStateKey(), serializedState)
 
   if s.slot mod STATE_STORAGE_PERIOD == 0:
     # Save slot to state mapping
-    db.backend.put(slotToStateKey(s.slot).toOpenArray, serializedState)
+    db.backend.put(slotToStateKey(s.slot), serializedState)
 
 proc persistBlock*(db: BeaconChainDB, s: BeaconState, b: BeaconBlock) =
   var prevState = db.lastFinalizedState()
@@ -91,13 +78,13 @@ proc persistBlock*(db: BeaconChainDB, s: BeaconState, b: BeaconBlock) =
   db.persistState(s)
 
   let blockHash = b.hash_tree_root_final
-  db.backend.put(hashToBlockKey(blockHash).toOpenArray, ssz.serialize(b))
-  db.backend.put(slotToBlockHashKey(b.slot).toOpenArray, blockHash.data)
+  db.backend.put(hashToBlockKey(blockHash), ssz.serialize(b))
+  db.backend.put(slotToBlockHashKey(b.slot), blockHash.data)
 
 # proc getValidatorChangeLog*(deltaChainTip: Eth2Digest)
 
 proc getBlock*(db: BeaconChainDB, hash: Eth2Digest, output: var BeaconBlock): bool =
-  let res = db.backend.get(hashToBlockKey(hash).toOpenArray)
+  let res = db.backend.get(hashToBlockKey(hash))
   if res.len != 0:
     output = ssz.deserialize(res, BeaconBlock).get
     true
