a0272c781ccfbfe822415160031dcae05241e119
Dustin Brody <tersec@users.noreply.github.com>
2019-03-08 09:33:46 -0800

    add (run-time only, alas) sanity check on signed_root

--- beacon_chain/ssz.nim
+++ beacon_chain/ssz.nim
@@ -292,15 +292,18 @@ func hash_tree_root*[T: object|tuple](x: T): array[32, byte] =
 
 # https://github.com/ethereum/eth2.0-specs/blob/0.4.0/specs/simple-serialize.md#signed-roots
 func signed_root*[T: object](x: T, field_name: string): array[32, byte] =
-  # As of 0.4.0, this is the only valid field_name.
   # TODO write tests for this (check vs hash_tree_root)
-  doAssert field_name == "signature"
+
+  var found_field_name = false
 
   withHash:
     for name, field in x.fieldPairs:
       if name == field_name:
+        found_field_name = true
         break
       h.update hash_tree_root(field)
+
+    doAssert found_field_name
 
 # #################################
 # hash_tree_root not part of official spec
