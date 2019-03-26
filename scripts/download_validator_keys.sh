5eae33b55efe6a19d28e9c0bc53de2aa14015194
Zahary Karadjov <zahary@gmail.com>
2019-03-26 22:20:50 +0200

    Helpful script for bulk downloading of validator keys

---
+++ scripts/download_validator_keys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+NETWORK_NAME=$1
+FIRST_VALIDATOR=$2
+LAST_VALIDATOR=$3
+DATA_DIR=${4:-~/.cache/nimbus/BeaconNode/$NETWORK_NAME}
+VALIDATORS_DIR=$DATA_DIR/validators
+
+mkdir -p $VALIDATORS_DIR
+
+FIRST_IDX=$(printf '%07d' $FIRST_VALIDATOR)
+LAST_IDX=$(printf '%07d' $LAST_VALIDATOR)
+
+curl "https://serenity-testnets.status.im/testnet0/v[$FIRST_IDX-$LAST_IDX].privkey" \
+  -o "$VALIDATORS_DIR/v#1.privkey" -s -w '%{url_effective} > %{filename_effective}\n'
+
