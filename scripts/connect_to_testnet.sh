819992a7869a6591f17319c33ff1f5730f1c1ec3
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2019-03-28 23:19:12 +0100

    Makefile: testnet targets wrapping connect_to_testnet.sh
    - also moved the binary into the "build" directory

--- scripts/connect_to_testnet.sh
+++ scripts/connect_to_testnet.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+
+[ -z "$1" ] && { echo "Usage: `basename $0` testnetX"; exit 1; }
+
 set -eu
+
 cd $(dirname "$0")
 
-NETWORK_NAME=$1
+NETWORK_NAME="$1"
 source "$NETWORK_NAME.env"
 
 cd ..
 
-NIM_FLAGS="-d:release --lineTrace:on -d:chronicles_log_level=DEBUG -d:SECONDS_PER_SLOT=$SECONDS_PER_SLOT -d:SHARD_COUNT=$SHARD_COUNT -d:SLOTS_PER_EPOCH=$SLOTS_PER_EPOCH"
+NIM_FLAGS="-d:release --lineTrace:on -d:chronicles_log_level=DEBUG -d:SECONDS_PER_SLOT=$SECONDS_PER_SLOT -d:SHARD_COUNT=$SHARD_COUNT -d:SLOTS_PER_EPOCH=$SLOTS_PER_EPOCH --hints:off --verbosity:0"
 
-BEACON_NODE_BIN=${NETWORK_NAME}_node
+BEACON_NODE_BIN="build/${NETWORK_NAME}_node"
 
-nim c $NIM_FLAGS -o:"./$BEACON_NODE_BIN" beacon_chain/beacon_node
+CMD="nim c $NIM_FLAGS -o:$BEACON_NODE_BIN beacon_chain/beacon_node"
+echo "$CMD"
+$CMD
 
-if [ ! -d ~/.cache/nimbus/BeaconNode/$NETWORK_NAME/validators ]; then
-  ./$BEACON_NODE_BIN --network=$NETWORK_NAME importValidator
+if [ ! -d ~/.cache/nimbus/BeaconNode/${NETWORK_NAME}/validators ]; then
+  $BEACON_NODE_BIN --network=$NETWORK_NAME importValidator
 fi
 
-./$BEACON_NODE_BIN --network=$NETWORK_NAME --tcpPort:$BOOTSTRAP_PORT --udpPort:$BOOTSTRAP_PORT
+$BEACON_NODE_BIN --network=$NETWORK_NAME --tcpPort:$BOOTSTRAP_PORT --udpPort:$BOOTSTRAP_PORT
 
