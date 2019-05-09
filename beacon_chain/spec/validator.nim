8cfbfdf565fde4fd6a4f25aa34c55cc83cdeea8d
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-05-09 12:01:01 +0200

    Fix SIGFPE on shuffling for 0

--- beacon_chain/spec/validator.nim
+++ beacon_chain/spec/validator.nim
@@ -25,6 +25,11 @@ func get_shuffled_seq*(seed: Eth2Digest,
   ##
   ## Invert the inner/outer loops from the spec, essentially. Most useful
   ## hash result re-use occurs within a round.
+
+  # Empty size -> empty list.
+  if list_size == 0:
+    return
+
   var
     # Share these buffers.
     pivot_buffer: array[(32+1), byte]
