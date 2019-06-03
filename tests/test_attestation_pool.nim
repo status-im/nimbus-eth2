16fb9e8d1140d6435a1d1d4105e91fb732e04fe1
Jacek Sieka <jacek@status.im>
2019-06-03 10:26:38 +0200

    Add support for dealing with overlapping attestations (#262)

--- tests/test_attestation_pool.nim
+++ tests/test_attestation_pool.nim
@@ -11,71 +11,145 @@ import
   ../beacon_chain/spec/[beaconstate, crypto, datatypes, digest, helpers, validator],
   ../beacon_chain/[beacon_node_types, attestation_pool, block_pool, extras, state_transition, ssz]
 
+template withPool(body: untyped) =
+  mixin genState, genBlock
+
+  var
+    blockPool {.inject.} = BlockPool.init(makeTestDB(genState, genBlock))
+    pool {.inject.} = AttestationPool.init(blockPool)
+    state {.inject.} = loadTailState(blockPool)
+  # Slot 0 is a finalized slot - won't be making attestations for it..
+  advanceState(state.data)
+
+  body
+
 suite "Attestation pool processing" & preset():
   ## For now just test that we can compile and execute block processing with
   ## mock data.
 
-  # Genesis state with minimal number of deposits
+  # Genesis state that results in 2 members per committee
   let
     genState = get_genesis_beacon_state(
-      makeInitialDeposits(flags = {skipValidation}), 0, Eth1Data(),
+      makeInitialDeposits(SLOTS_PER_EPOCH * 2, {skipValidation}), 0, Eth1Data(),
         {skipValidation})
     genBlock = get_initial_beacon_block(genState)
 
   test "Can add and retrieve simple attestation" & preset():
-    var
-      blockPool = BlockPool.init(makeTestDB(genState, genBlock))
-      pool = AttestationPool.init(blockPool)
-      state = blockPool.loadTailState()
-    # Slot 0 is a finalized slot - won't be making attestations for it..
-    advanceState(state.data)
+    withPool:
+      let
+        # Create an attestation for slot 1!
+        crosslink_committees =
+          get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
+        attestation = makeAttestation(
+          state.data.data, state.blck.root, crosslink_committees[0].committee[0])
 
-    let
-      # Create an attestation for slot 1 signed by the only attester we have!
-      crosslink_committees =
-        get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
-      attestation = makeAttestation(
-        state.data.data, state.blck.root, crosslink_committees[0].committee[0])
+      pool.add(state.data.data, attestation)
 
-    pool.add(state.data.data, attestation)
+      for i in 0..<MIN_ATTESTATION_INCLUSION_DELAY.int - 1:
+        advanceState(state.data)
 
-    let attestations = pool.getAttestationsForBlock(
-      state.data.data, state.data.data.slot + MIN_ATTESTATION_INCLUSION_DELAY)
+      let attestations = pool.getAttestationsForBlock(
+        state.data.data, state.data.data.slot + 1)
 
-    # TODO test needs fixing for new attestation validation
-    # check:
-    #   attestations.len == 1
+      check:
+        attestations.len == 1
 
   test "Attestations may arrive in any order" & preset():
-    var
-      blockPool = BlockPool.init(makeTestDB(genState, genBlock))
-      pool = AttestationPool.init(blockPool)
-      state = blockPool.loadTailState()
-    # Slot 0 is a finalized slot - won't be making attestations for it..
-    advanceState(state.data)
+    withPool:
+      let
+        # Create an attestation for slot 1!
+        cc0 =
+          get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
+        attestation0 = makeAttestation(
+          state.data.data, state.blck.root, cc0[0].committee[0])
 
-    let
-      # Create an attestation for slot 1 signed by the only attester we have!
-      crosslink_committees1 =
-        get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
-      attestation1 = makeAttestation(
-        state.data.data, state.blck.root, crosslink_committees1[0].committee[0])
+      advanceState(state.data)
 
-    advanceState(state.data)
+      let
+        cc1 =
+          get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
+        attestation1 = makeAttestation(
+          state.data.data, state.blck.root, cc1[0].committee[0])
 
-    let
-      crosslink_committees2 =
-        get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
-      attestation2 = makeAttestation(
-        state.data.data, state.blck.root, crosslink_committees2[0].committee[0])
+      # test reverse order
+      pool.add(state.data.data, attestation1)
+      pool.add(state.data.data, attestation0)
 
-    # test reverse order
-    pool.add(state.data.data, attestation2)
-    pool.add(state.data.data, attestation1)
+      for i in 0..<MIN_ATTESTATION_INCLUSION_DELAY.int - 1: advanceState(state.data)
 
-    let attestations = pool.getAttestationsForBlock(
-      state.data.data, state.data.data.slot + MIN_ATTESTATION_INCLUSION_DELAY)
+      let attestations = pool.getAttestationsForBlock(
+        state.data.data, state.data.data.slot + 1)
 
-    # TODO test needs fixing for new attestation validation
-    # check:
-    #   attestations.len == 1
+      check:
+        attestations.len == 1
+
+  test "Attestations should be combined" & preset():
+    withPool:
+      let
+        # Create an attestation for slot 1!
+        cc0 =
+          get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
+        attestation0 = makeAttestation(
+          state.data.data, state.blck.root, cc0[0].committee[0])
+        attestation1 = makeAttestation(
+          state.data.data, state.blck.root, cc0[0].committee[1])
+
+      pool.add(state.data.data, attestation0)
+      pool.add(state.data.data, attestation1)
+
+      for i in 0..<MIN_ATTESTATION_INCLUSION_DELAY.int - 1: advanceState(state.data)
+
+      let attestations = pool.getAttestationsForBlock(
+        state.data.data, state.data.data.slot + 1)
+
+      check:
+        attestations.len == 1
+
+  test "Attestations may overlap, bigger first" & preset():
+    withPool:
+
+      var
+        # Create an attestation for slot 1!
+        cc0 =
+          get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
+        attestation0 = makeAttestation(
+          state.data.data, state.blck.root, cc0[0].committee[0])
+        attestation1 = makeAttestation(
+          state.data.data, state.blck.root, cc0[0].committee[1])
+
+      attestation0.combine(attestation1, {skipValidation})
+
+      pool.add(state.data.data, attestation0)
+      pool.add(state.data.data, attestation1)
+
+      for i in 0..<MIN_ATTESTATION_INCLUSION_DELAY.int - 1: advanceState(state.data)
+
+      let attestations = pool.getAttestationsForBlock(
+        state.data.data, state.data.data.slot + 1)
+
+      check:
+        attestations.len == 1
+
+  test "Attestations may overlap, smaller first" & preset():
+    withPool:
+      var
+        # Create an attestation for slot 1!
+        cc0 =
+          get_crosslink_committees_at_slot(state.data.data, state.data.data.slot)
+        attestation0 = makeAttestation(
+          state.data.data, state.blck.root, cc0[0].committee[0])
+        attestation1 = makeAttestation(
+          state.data.data, state.blck.root, cc0[0].committee[1])
+
+      attestation0.combine(attestation1, {skipValidation})
+
+      pool.add(state.data.data, attestation1)
+      pool.add(state.data.data, attestation0)
+
+      for i in 0..<MIN_ATTESTATION_INCLUSION_DELAY.int - 1: advanceState(state.data)
+
+      let attestations = pool.getAttestationsForBlock(
+        state.data.data, state.data.data.slot + 1)
+
+      check:
+        attestations.len == 1
