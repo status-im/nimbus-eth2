e7058382a17d1dbbb34b867345cda9c1d998735e
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-06-11 16:28:23 +0200

    Add deserialization test. "wrong length" skipped due to https://github.com/status-im/nim-beacon-chain/issues/280

--- tests/official/test_fixture_ssz_uint.nim
+++ tests/official/test_fixture_ssz_uint.nim
@@ -26,22 +26,39 @@ func to(val: string, T: typedesc): T =
   else: # result is unsigned int
     val.parse(StUint[8 * sizeof(T)]).data
 
+# TODO strformat for the skipped checks
+
 template checkSerialization(test: SSZUint, T: typedesc) =
   if test.valid:
     let value: T = test.value.to(T)
     let serialized = SSZ.encode(value)
     check(serialized == test.ssz)
-  else:
+  elif test.value != "":
+    # No SSZ encoding -> expected failing serialization test
     if test.tags.anyIt(it == "uint_underflow"):
       # TODO: Stint throws RangeError for negative number parsing
       #       https://github.com/status-im/nim-stint/blob/ccf87daac1eef15238ff3d6d2edb138e22180d19/stint/io.nim#L130-L132
       expect RangeError, OverflowError:
         let value: T = test.value.to(T)
-    elif test.tags.anyIt(it == "wrong_length"):
-      let value: T = test.value.to(T)
     else:
       # TODO tag "uint_overflow" does not throw an exception at the moment
-      echo "         [Skipped] tags: ", test.tags
+      echo "         [Skipped - Serialization - TODO]  tags: ", test.tags
+  else:
+    echo "         [Skipped - Serialization - N/A]   tags: ", test.tags
+
+template checkDeserialization(test: SSZUint, T: typedesc) =
+  if test.valid:
+    let deser = SSZ.decode(test.ssz, T)
+    check($deser == test.value)
+  elif test.value == "":
+    # No literal value -> expected failing deserialization test
+    if test.tags.anyIt(it == "wrong_length"):
+      expect IndexError:
+        let deser = SSZ.decode(test.ssz, T)
+    else:
+      echo "         [Skipped - Deserialization]       tags: ", test.tags
+  else:
+    echo "         [Skipped - Deserialization - N/A] tags: ", test.tags
 
 proc runSSZUintTest(inputTests: Tests[SSZUint]) =
   # We use Stint string -> uint parser + casting
@@ -50,12 +67,16 @@ proc runSSZUintTest(inputTests: Tests[SSZUint]) =
   for test in inputTests.test_cases:
     if test.`type` == "uint8":
       test.checkSerialization(uint8)
+      test.checkDeserialization(uint8)
     elif test.`type` == "uint16":
       test.checkSerialization(uint16)
+      test.checkDeserialization(uint16)
     elif test.`type` == "uint32":
       test.checkSerialization(uint32)
+      test.checkDeserialization(uint32)
     elif test.`type` == "uint64":
       test.checkSerialization(uint64)
+      test.checkDeserialization(uint64)
     # TODO: Stint serialization
     # elif test.`type` == "uint128":
     #   test.checkSerialization(StUint[128])
@@ -74,3 +95,11 @@ suite "Official - SSZ unsigned integer tests" & preset():
     let uintRandom = parseTests(TestFolder / TestsPath / "uint_random.json", SSZUint)
     test uintRandom.summary & preset():
       runSSZUintTest(uintRandom)
+
+  # TODO: pending fix for https://github.com/status-im/nim-beacon-chain/issues/280
+  block: # "Serialized integers that are too short or too long"
+    let uintWrongLength = parseTests(TestFolder / TestsPath / "uint_wrong_length.json", SSZUint)
+    test "[Skipped] " & uintWrongLength.summary & preset():
+      # TODO: pending fix for https://github.com/status-im/nim-beacon-chain/issues/280
+      echo "         [Skipped] Pending https://github.com/status-im/nim-beacon-chain/issues/280"
+      # runSSZUintTest(uintWrongLength)
