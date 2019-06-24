0d4c2f0f74a9b31e26630420c797691d6f1362fb
Zahary Karadjov <zahary@gmail.com>
2019-06-24 15:57:22 +0300

    Switch the testnet1 back-end to libp2p_native
    Further simplifications and removal of support for multiple network
    back-ends per testnet.

--- scripts/reset_testnet.sh
+++ scripts/reset_testnet.sh
@@ -17,33 +17,26 @@ fi
 PUBLIC_IP=$(curl -s ifconfig.me)
 NETWORK_DIR=$WWW_DIR/$NETWORK_NAME
 
-regenTestnetFiles() {
-  NIM_FLAGS="-d:release -d:SECONDS_PER_SLOT=$SECONDS_PER_SLOT -d:SHARD_COUNT=$SHARD_COUNT -d:SLOTS_PER_EPOCH=$SLOTS_PER_EPOCH ${2:-}"
-  NETWORK_TYPE=$1
+NIM_FLAGS="-d:release -d:SECONDS_PER_SLOT=$SECONDS_PER_SLOT -d:SHARD_COUNT=$SHARD_COUNT -d:SLOTS_PER_EPOCH=$SLOTS_PER_EPOCH ${2:-}"
 
-  if [ ! -f $NETWORK_DIR/genesis.json ]; then
-    rm -f $NETWORK_DIR/*
-    nim c -r $NIM_FLAGS beacon_chain/validator_keygen \
-      --totalValidators=$VALIDATOR_COUNT \
-      --outputDir="$NETWORK_DIR"
-  fi
-
-  nim c -d:"network_type=$NETWORK_TYPE" -r $NIM_FLAGS beacon_chain/beacon_node \
-    --network=$NETWORK_NAME \
-    --dataDir=$DATA_DIR/node-0 \
-    createTestnet \
-    --networkId=$NETWORK_ID \
-    --validatorsDir=$NETWORK_DIR \
+if [ ! -f $NETWORK_DIR/genesis.json ]; then
+  rm -f $NETWORK_DIR/*
+  nim c -r $NIM_FLAGS beacon_chain/validator_keygen \
     --totalValidators=$VALIDATOR_COUNT \
-    --lastUserValidator=$LAST_USER_VALIDATOR \
-    --outputGenesis=$NETWORK_DIR/genesis.json \
-    --outputNetwork=$NETWORK_DIR/$NETWORK_TYPE-network.json \
-    --bootstrapAddress=$PUBLIC_IP \
-    --bootstrapPort=$BOOTSTRAP_PORT \
-    --genesisOffset=600 # Delay in seconds
-}
+    --outputDir="$NETWORK_DIR"
+fi
 
-regenTestnetFiles rlpx
-regenTestnetFiles libp2p_spec
-regenTestnetFiles libp2p_native
+nim c -d:"network_type=$NETWORK_TYPE" -r $NIM_FLAGS beacon_chain/beacon_node \
+  --network=$NETWORK_NAME \
+  --dataDir=$DATA_DIR/node-0 \
+  createTestnet \
+  --networkId=$NETWORK_ID \
+  --validatorsDir=$NETWORK_DIR \
+  --totalValidators=$VALIDATOR_COUNT \
+  --lastUserValidator=$LAST_USER_VALIDATOR \
+  --outputGenesis=$NETWORK_DIR/genesis.json \
+  --outputNetwork=$NETWORK_DIR/network.json \
+  --bootstrapAddress=$PUBLIC_IP \
+  --bootstrapPort=$BOOTSTRAP_PORT \
+  --genesisOffset=600 # Delay in seconds
 
