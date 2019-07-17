1b625158003645c985a1067b81fd1ab4f429f76a
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-07-17 21:11:45 +0200

    [Mock] Implement sparse merkle tree and merkle proof builder

--- tests/mocking/merkle_minimal.nim
+++ tests/mocking/merkle_minimal.nim
@@ -8,56 +8,108 @@
 # Merkle tree helpers
 # ---------------------------------------------------------------
 
-# References:
-#   - https://github.com/ethereum/eth2.0-specs/blob/v0.8.1/test_libs/pyspec/eth2spec/utils/merkle_minimal.py
-#   - https://github.com/ethereum/eth2.0-specs/blob/v0.8.1/specs/light_client/merkle_proofs.md
-#   - merkleize template in ssz.nim
-#   - Binary Indexed Trees / Fenwick Tree building:
-#       https://github.com/numforge/laser/blob/2a167b37b7eba1fab89068c1bb61f48a9fb51b05/benchmarks/random_sampling/fenwicktree.nim#L67-L118
-
 import
   # Specs
-  ../../beacon_chain/spec/[datatypes, digest],
+  ../../beacon_chain/spec/[datatypes, digest, beaconstate, helpers],
+  ../../beacon_chain/ssz,
   # shims
-  stew/bitops2
+  stew/objects
 
-func nextPowerOf2*(x: int): int =
-  ## Returns x if x is a power of 2
-  ## or the next biggest power of 2
-  1 shl (fastLog2(x.uint-1) + 1)
+func round_step_down*(x: Natural, step: static Natural): int {.inline.} =
+  ## Round the input to the previous multiple of "step"
+  when (step and (step - 1)) == 0:
+    # Step is a power of 2. (If compiler cannot prove that x>0 it does not make the optim)
+    result = x and not(step - 1)
+  else:
+    result = x - x mod step
 
-func merkleTreeFromLeaves(
+let ZeroHashes = block:
+  # hashes for a merkle tree full of zeros for leafs
+  var zh = @[Eth2Digest()]
+  for i in 1 ..< DEPOSIT_CONTRACT_TREE_DEPTH:
+    let nodehash = withEth2Hash:
+      h.update zh[i-1]
+      h.update zh[i-1]
+    zh.add nodehash
+  zh
+
+type SparseMerkleTree[Depth: static int] = object
+  ## Sparse Merkle tree
+  # There is an extra "depth" layer to store leaf nodes
+  # This stores leaves at depth = 0
+  # and the root hash at the last depth
+  nnznodes: array[Depth+1, seq[Eth2Digest]]  # nodes that leads to non-zero leaves
+
+proc merkleTreeFromLeaves*(
         values: openarray[Eth2Digest],
-        depth: static[int] = 32
-      ): seq[Eth2Digest] =
+        Depth: static[int] = DEPOSIT_CONTRACT_TREE_DEPTH
+      ): SparseMerkleTree[Depth] =
   ## Depth should be the same as
   ## verify_merkle_branch / is_valid_merkle_branch
 
-  # Note: Merkle Tree are indexed by 1 :/
-  # https://github.com/ethereum/eth2.0-specs/issues/1008
+  result.nnznodes[0] = @values
 
-  # A complete tree (all leaves used) with n leaves
-  # has n-1 internal nodes, for a total of 2N-1 nodes
-  # so we allocate enough to hold a complete tree
+  for depth in 1 .. Depth: # Inclusive range
+    let prev_depth_len = result.nnznodes[depth-1].len
+    let stop = round_step_down(prev_depth_len, 2)
+    for i in countup(0, stop-1, 2):
+      # hash by pair of previous nodes
+      let nodeHash = withEth2Hash:
+        h.update result.nnznodes[depth-1][i]
+        h.update result.nnznodes[depth-1][i+1]
+      result.nnznodes[depth].add nodeHash
 
-  # Tree relation in a flat sequence
-  #                  root at 1
-  # Left child        idx*2
-  # Right child       idx*2 + 1
-  # Parent            idx/2
+    if prev_depth_len != stop:
+      # If length is odd, the last one was skipped,
+      # we need to combine it
+      # with the zeroHash corresponding to the current depth
+      let nodeHash = withEth2Hash:
+        h.update result.nnznodes[depth-1][^1]
+        h.update ZeroHashes[depth-1]
+      result.nnznodes[depth].add nodeHash
 
-  let n = values.len.nextPowerOfTwo()
-  let leaves_offset = n # node0 is unused.
-  let size = leaves_offset + n
-  result = newSeq[Eth2Digest](size)
+proc get_merkle_proof*[Depth: static int](
+        tree: SparseMerkleTree[Depth],
+        index: int,
+      ): array[Depth, Eth2Digest] =
 
-  # The leaves contain the values
-  # the range [leaves_offset + values.len ..< leaves_offset + values.len.nextPowerOfTwo()]
-  # is wasted. Sparse merkle trees would help there
-  result[leaves_offset ..< leaves_offset + values.len] = values
+  # Descend down the tree according to the bit representation
+  # of the index:
+  #   - 0 --> go left
+  #   - 1 --> go right
+  let path = uint32(index)
+  for depth in 0 ..< Depth:
+    let nodeIdx = int((path shr depth) xor 1)
+    if nodeIdx < tree.nnznodes[depth].len:
+      result[depth] = tree.nnznodes[depth][nodeIdx]
+    else:
+      result[depth] = ZeroHashes[depth]
 
-  # Now we build the internal nodes
-  for i in countdown(leaves_offset, 1):
-    result[i] = withEth2Hash:
-      h.update(result[2*i])
-      h.update(result[2*i+1])
+when isMainModule:
+  # Check that round-tripping works with verify_merkle_branch (in beaconstate.nim)
+
+  proc toDigest[N: static int](x: array[N, byte]): Eth2Digest =
+    result.data[0 .. N-1] = x
+
+  block: # 2-depth round-trip
+    let a = [byte 0x01, 0x02, 0x03].toDigest
+    let b = [byte 0x04, 0x05, 0x06].toDigest
+    let c = [byte 0x07, 0x08, 0x09].toDigest
+
+    let tree = merkleTreeFromLeaves([a, b, c], Depth = 3)
+    echo "Tree: ", tree
+
+    let index = 2
+    let proof = get_merkle_proof(tree, index)
+    echo "Proof: ", proof
+
+    # TODO - need compliant implementation of SSZ hash_tree_root
+    let root = hash_tree_root([a, b, c])
+    echo root
+
+    # echo verify_merkle_branch(
+    #   a, get_merkle_proof(tree, index = 2),
+    #   depth = 3,
+    #   index = 2,
+    #   root = root
+    # )
