db173348262cf60e9524d884545077d4bb50663b
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-08-30 20:35:37 +0200

    Workaround https://github.com/status-im/nim-beacon-chain/issues/369

--- tests/official/test_fixture_epoch_crosslinks.nim
+++ tests/official/test_fixture_epoch_crosslinks.nim
@@ -9,15 +9,28 @@ import
   # Standard library
   os, unittest,
   # Beacon chain internals
-  ../../beacon_chain/spec/[beaconstate, datatypes, validator, helpers, state_transition_epoch],
+  ../../beacon_chain/spec/[datatypes, validator, state_transition_epoch],
   # Test utilities
   ../testutil,
   ./fixtures_utils
 
 const CrosslinksDir = SszTestsDir/const_preset/"phase0"/"epoch_processing"/"crosslinks"/"pyspec_tests"
 
+# TODO: parsing pre and post
+#       in the same scope crashes Nim with preset: mainnet
+#       https://github.com/status-im/nim-beacon-chain/issues/369
+proc parsePre(): BeaconState =
+  parseTest(CrosslinksDir/"no_attestations"/"pre.ssz", SSZ, BeaconState)
+
+proc parsePost(): BeaconState =
+  parseTest(CrosslinksDir/"no_attestations"/"post.ssz", SSZ, BeaconState)
+
 suite "Official - Epoch Processing - Crosslinks [Preset: " & preset():
   test "Crosslinks - no attestation" & preset():
-    let
-      preState = parseTest(CrosslinksDir/"no_attestations"/"pre.ssz", SSZ, BeaconState)
-      postState = parseTest(CrosslinksDir/"no_attestations"/"post.ssz", SSZ, BeaconState)
+    var state = parsePre()
+    let post = parsePost()
+
+    var cache = get_empty_per_epoch_cache()
+    process_crosslinks(state, cache)
+
+    check: state.hash_tree_root() == post.hash_tree_root()
