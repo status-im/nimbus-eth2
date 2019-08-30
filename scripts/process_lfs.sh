a344f1fbadf8417642e80575eb38b4592a46af88
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-08-30 12:02:25 +0200

    LFS caching: handle multiple versions of json_tests

--- scripts/process_lfs.sh
+++ scripts/process_lfs.sh
@@ -9,10 +9,14 @@
 
 set -e
 
-ARCHIVE_NAME="json_tests.tar.xz"
+ARCHIVE_NAME_v0_8_1="json_tests_v0.8.1.tar.xz"
+ARCHIVE_NAME_v0_8_3="json_tests_v0.8.3.tar.xz"
+
+LFS_DIR_v0_8_1="json_tests_v0.8.1"
+LFS_DIR_v0_8_3="json_tests_v0.8.3"
+
 TMP_CACHE_DIR="tmpcache"
 SUBREPO_DIR="tests/official/fixtures"
-LFS_DIR="json_tests"
 # verbosity level
 [[ -z "$V" ]] && V=0
 [[ -z "$BUILD_MSG" ]] && BUILD_MSG="Downloading LFS files"
@@ -42,20 +46,21 @@ download_lfs_files() {
 
 	pushd "${SUBREPO_DIR}"
 	git lfs install # redundant after running it once per repo, but fast enough not to worry about detecting whether it ran before
-	git lfs pull -I "${LFS_DIR}" # we just care about test fixtures converted from YAML to JSON
+	git lfs pull -I "${LFS_DIR_v0_8_1},${LFS_DIR_v0_8_3}" # we just care about test fixtures converted from YAML to JSON
 	popd
 }
 
-UPDATE_CACHE=0
+# TODO: Use a function
 if [[ -n "${CACHE_DIR}" ]]; then
-	if [[ -e "${CACHE_DIR}/${ARCHIVE_NAME}" ]]; then
+  UPDATE_CACHE=0 # v0.8.1 tests
+	if [[ -e "${CACHE_DIR}/${ARCHIVE_NAME_v0_8_1}" ]]; then
 		# compare the archive's mtime to the date of the last commit
-		if [[ $(stat ${STAT_FORMAT} "${CACHE_DIR}/${ARCHIVE_NAME}") -gt $(cd "${SUBREPO_DIR}"; git log --pretty=format:%cd -n 1 --date=unix "${LFS_DIR}") ]]; then
+		if [[ $(stat ${STAT_FORMAT} "${CACHE_DIR}/${ARCHIVE_NAME_v0_8_1}") -gt $(cd "${SUBREPO_DIR}"; git log --pretty=format:%cd -n 1 --date=unix "${LFS_DIR_v0_8_1}") ]]; then
 			# the cache is valid
-			echo "Copying cached files into ${SUBREPO_DIR}/${LFS_DIR}/"
+			echo "Copying cached files into ${SUBREPO_DIR}/${LFS_DIR_v0_8_1}/"
 			mkdir -p "${TMP_CACHE_DIR}"
-			${DECOMPRESS_XZ} "${CACHE_DIR}/${ARCHIVE_NAME}" | tar -x -C "${TMP_CACHE_DIR}" -f -
-			cp -a "${TMP_CACHE_DIR}/${LFS_DIR}"/* "${SUBREPO_DIR}/${LFS_DIR}/"
+			${DECOMPRESS_XZ} "${CACHE_DIR}/${ARCHIVE_NAME_v0_8_1}" | tar -x -C "${TMP_CACHE_DIR}" -f -
+			cp -a "${TMP_CACHE_DIR}/${LFS_DIR_v0_8_1}"/* "${SUBREPO_DIR}/${LFS_DIR_v0_8_1}/"
 			rm -rf "${TMP_CACHE_DIR}"
 		else
 			# old cache
@@ -74,13 +79,46 @@ if [[ -n "${CACHE_DIR}" ]]; then
 		download_lfs_files
 		echo "Updating the cache."
 		pushd "${SUBREPO_DIR}"
-		# the archive will contain ${LFS_DIR} as its top dir
-		git archive --format=tar HEAD "${LFS_DIR}" | ${COMPRESS_XZ} > "${ARCHIVE_NAME}"
+		# the archive will contain ${LFS_DIR_v0_8_1} as its top dir
+		git archive --format=tar HEAD "${LFS_DIR_v0_8_1}" | ${COMPRESS_XZ} > "${ARCHIVE_NAME_v0_8_1}"
 		popd
-		mv "${SUBREPO_DIR}/${ARCHIVE_NAME}" "${CACHE_DIR}/"
+		mv "${SUBREPO_DIR}/${ARCHIVE_NAME_v0_8_1}" "${CACHE_DIR}/"
 	fi
+
+  UPDATE_CACHE=0 # v0.8.3 tests
+	if [[ -e "${CACHE_DIR}/${ARCHIVE_NAME_v0_8_3}" ]]; then
+		# compare the archive's mtime to the date of the last commit
+		if [[ $(stat ${STAT_FORMAT} "${CACHE_DIR}/${ARCHIVE_NAME_v0_8_3}") -gt $(cd "${SUBREPO_DIR}"; git log --pretty=format:%cd -n 1 --date=unix "${LFS_DIR_v0_8_3}") ]]; then
+			# the cache is valid
+			echo "Copying cached files into ${SUBREPO_DIR}/${LFS_DIR_v0_8_3}/"
+			mkdir -p "${TMP_CACHE_DIR}"
+			${DECOMPRESS_XZ} "${CACHE_DIR}/${ARCHIVE_NAME_v0_8_3}" | tar -x -C "${TMP_CACHE_DIR}" -f -
+			cp -a "${TMP_CACHE_DIR}/${LFS_DIR_v0_8_3}"/* "${SUBREPO_DIR}/${LFS_DIR_v0_8_3}/"
+			rm -rf "${TMP_CACHE_DIR}"
+		else
+			# old cache
+			echo "Invalidating cache."
+			UPDATE_CACHE=1
+		fi
+	else
+		# creating the archive for the first time
+		mkdir -p "${CACHE_DIR}"
+		UPDATE_CACHE=1
+	fi
+	if [[ "${UPDATE_CACHE}" == "1" ]]; then
+		if [[ "${ON_MACOS}" == "1" ]]; then
+			brew install git-lfs # this takes almost 5 minutes on Travis, so only run it if needed
+		fi
+		download_lfs_files
+		echo "Updating the cache."
+		pushd "${SUBREPO_DIR}"
+		# the archive will contain ${LFS_DIR_v0_8_3} as its top dir
+		git archive --format=tar HEAD "${LFS_DIR_v0_8_3}" | ${COMPRESS_XZ} > "${ARCHIVE_NAME_v0_8_3}"
+		popd
+		mv "${SUBREPO_DIR}/${ARCHIVE_NAME_v0_8_3}" "${CACHE_DIR}/"
+	fi
+
 else
 	# no caching
 	download_lfs_files
 fi
-
