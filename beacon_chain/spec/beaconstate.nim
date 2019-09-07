8408b8dd595b9d36bb58fdb18a76ca82a56de53f
mratsim <mamy_github@numforge.co>
2019-09-07 12:57:35 -0400

    missing comma

--- beacon_chain/spec/beaconstate.nim
+++ beacon_chain/spec/beaconstate.nim
@@ -230,7 +230,7 @@ func initialize_beacon_state_from_eth1*(
       Eth1Data(block_hash: eth1_block_hash, deposit_count: uint64(len(deposits))),
     latest_block_header:
       BeaconBlockHeader(
-        body_root: hash_tree_root(BeaconBlockBody())
+        body_root: hash_tree_root(BeaconBlockBody()),
         # TODO - Pure BLSSig cannot be zero: https://github.com/status-im/nim-beacon-chain/issues/374
         signature: BlsValue[Signature](kind: OpaqueBlob)
       ),
