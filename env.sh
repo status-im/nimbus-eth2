ad50f26ff878d5ec816534d2fb347ea8c667303c
Zahary Karadjov <zahary@gmail.com>
2019-09-08 13:10:36 -0400

    Attempt to fix env.sh issues on macs Disable the debuginfo in interop/start.sh builds (this solved a problem for Danny on macOS)

--- env.sh
+++ env.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# We use ${BASH_SOURCE[0]} instead of $0 to allow sourcing this file
-# and we fall back to a Zsh-specific special var to also support Zsh.
-REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
-ABS_PATH="$(cd ${REL_PATH}; pwd)"
+if test -n "$BASH" ; then script=$BASH_SOURCE
+elif test -n "$TMOUT"; then script=${.sh.file}
+elif test -n "$ZSH_NAME" ; then script=${(%):-%x}
+elif test ${0##*/} = dash; then x=$(lsof -p $$ -Fn0 | tail -1); script=${x#n}
+else script=$0
+fi
+
+# https://unix.stackexchange.com/a/76518
+export REL_PATH=`dirname "$script"`
+export ABS_PATH=`exec 2>/dev/null;(cd -- "$REL_PATH") && cd -- "$REL_PATH"|| cd "$REL_PATH"; unset PWD; /usr/bin/pwd || /bin/pwd || pwd`
+
 source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
 
