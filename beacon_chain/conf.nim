Araq/nim-beacon-chain

Implement the quick start option (aka mocked start)
For more info, see here:
https://github.com/ethereum/eth2.0-pm/tree/master/interop/mocked_start

beacon_chain/conf.nim
@@ -18,6 +18,11 @@ type
     createTestnet
     updateTestnet
 
+  GenesisType* = enum
+    eth1
+    snapshot
+    quickStart
+
   BeaconNodeConf* = object
     logLevel* {.
       desc: "Sets the log level",
@@ -40,6 +45,11 @@ type
       defaultValue: noCommand.}: StartUpCommand
 
     of noCommand:
+      nodename* {.
+        desc: "A name for this node that will appear in the logs. " &
+              "If you set this to 'auto', a persistent automatically generated ID will be seleceted for each --dataDir folder"
+        defaultValue: ""}: string
+
       bootstrapNodes* {.
         desc: "Specifies one or more bootstrap nodes to use when connecting to the network."
         longform: "bootstrapNode"
@@ -62,28 +72,40 @@ type
         desc: "Specify method to use for determining public address. Must be one of: any, none, upnp, pmp, extip:<IP>"
         defaultValue: "any" .}: string
 
-      validators* {.
-        required
-        desc: "Path to a validator private key, as generated by validator_keygen"
-        longform: "validator"
-        shortform: "v".}: seq[ValidatorKeyPath]
+      case genesisType*: GenesisType
+      of eth1:
+        depositWeb3Url* {.
+          desc: "URL of the Web3 server to observe Eth1",
+          defaultValue: ""}: string
 
-      stateSnapshot* {.
-        desc: "Json file specifying a recent state snapshot"
-        shortform: "s".}: Option[TypedInputFile[BeaconState, Json, "json"]]
+        depositContractAddress* {.
+          desc: "Address of the deposit contract",
+          defaultValue: ""}: string
 
-      nodename* {.
-        desc: "A name for this node that will appear in the logs. " &
-              "If you set this to 'auto', a persistent automatically generated ID will be seleceted for each --dataDir folder"
-        defaultValue: ""}: string
+      of snapshot:
+        validators* {.
+          required
+          desc: "Path to a validator private key, as generated by validator_keygen"
+          longform: "validator"
+          shortform: "v".}: seq[ValidatorKeyPath]
 
-      depositWeb3Url* {.
-        desc: "URL of the Web3 server to observe Eth1",
-        defaultValue: ""}: string
+        stateSnapshot* {.
+          desc: "Json file specifying a recent state snapshot"
+          shortform: "s".}: Option[TypedInputFile[BeaconState, Json, "json"]]
 
-      depositContractAddress* {.
-        desc: "Address of the deposit contract",
-        defaultValue: ""}: string
+      of quickStart:
+        genesisTime* {.
+          desc: "A unix timestamp marking the genesis start".}: uint64
+
+        quickStartTotalValidators* {.
+          longform: "totalValidators"
+          desc: "Total number of validators in the network".}: uint64
+
+        firstValidatorIndex* {.
+          desc: "Index of the first attached validator".}: uint64
+
+        attachedValidatorsCount* {.
+          desc: "Number of validators to attach to this node".}: uint64
 
     of createTestnet:
       networkId* {.

status-im/nimbus-eth2

beacon_chain/conf.nim
@@ -19,6 +19,11 @@ type
     updateTestnet
     makeDeposits
 
+  GenesisType* = enum
+    eth1
+    snapshot
+    quickStart
+
   BeaconNodeConf* = object
     logLevel* {.
       desc: "Sets the log level",
@@ -36,19 +41,16 @@ type
       shortform: "d"
       defaultValue: config.defaultDataDir().}: OutDir
 
-    depositWeb3Url* {.
-      desc: "URL of the Web3 server to observe Eth1",
-      defaultValue: ""}: string
-
-    depositContractAddress* {.
-      desc: "Address of the deposit contract",
-      defaultValue: ""}: string
-
     case cmd* {.
       command
       defaultValue: noCommand.}: StartUpCommand
 
     of noCommand:
+      nodename* {.
+        desc: "A name for this node that will appear in the logs. " &
+              "If you set this to 'auto', a persistent automatically generated ID will be seleceted for each --dataDir folder"
+        defaultValue: ""}: string
+
       bootstrapNodes* {.
         desc: "Specifies one or more bootstrap nodes to use when connecting to the network."
         longform: "bootstrapNode"
@@ -71,21 +73,6 @@ type
         desc: "Specify method to use for determining public address. Must be one of: any, none, upnp, pmp, extip:<IP>"
         defaultValue: "any" .}: string
 
-      validators* {.
-        required
-        desc: "Path to a validator private key, as generated by makeDeposits"
-        longform: "validator"
-        shortform: "v".}: seq[ValidatorKeyPath]
-
-      stateSnapshot* {.
-        desc: "Json file specifying a recent state snapshot"
-        shortform: "s".}: Option[InputFile]
-
-      nodename* {.
-        desc: "A name for this node that will appear in the logs. " &
-              "If you set this to 'auto', a persistent automatically generated ID will be seleceted for each --dataDir folder"
-        defaultValue: ""}: string
-
       metricsServer* {.
         desc: "Enable the metrics server"
         defaultValue: false.}: bool
@@ -97,6 +84,41 @@ type
       metricsServerPort* {.
         desc: "Listening HTTP port of the metrics server"
         defaultValue: 8008 .}: uint16
+
+      case genesisType*: GenesisType
+      of eth1:
+        depositWeb3Url* {.
+          desc: "URL of the Web3 server to observe Eth1",
+          defaultValue: ""}: string
+
+        depositContractAddress* {.
+          desc: "Address of the deposit contract",
+          defaultValue: ""}: string
+
+      of quickStart:
+        genesisTime* {.
+          desc: "A unix timestamp marking the genesis start".}: uint64
+
+        quickStartTotalValidators* {.
+          longform: "totalValidators"
+          desc: "Total number of validators in the network".}: uint64
+
+        firstValidatorIndex* {.
+          desc: "Index of the first attached validator".}: uint64
+
+        attachedValidatorsCount* {.
+          desc: "Number of validators to attach to this node".}: uint64
+
+      of snapshot:
+        validators* {.
+          required
+          desc: "Path to a validator private key, as generated by makeDeposits"
+          longform: "validator"
+          shortform: "v".}: seq[ValidatorKeyPath]
+
+        stateSnapshot* {.
+          desc: "A SSZ or Json file specifying a recent state snapshot"
+          shortform: "s".}: InputFile
 
     of createTestnet:
       networkId* {.