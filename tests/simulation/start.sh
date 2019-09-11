75d8ef854713d9e8775b463c700239e980090947
Zahary Karadjov <zahary@gmail.com>
2019-09-11 11:01:36 -0400

    Tentative fix for the Hello packet issues

--- tests/simulation/start.sh
+++ tests/simulation/start.sh
@@ -62,7 +62,7 @@ if [ ! -f "${SNAPSHOT_FILE}" ]; then
     --validatorsDir="${VALIDATORS_DIR}" \
     --totalValidators="${NUM_VALIDATORS}" \
     --outputGenesis="${SNAPSHOT_FILE}" \
-    --outputNetwork="${NETWORK_METADATA_FILE}" \
+    --outputNetworkMetadata="${NETWORK_METADATA_FILE}" \
     --bootstrapAddress=127.0.0.1 \
     --bootstrapPort=50000 \
     --genesisOffset=10 # Delay in seconds
