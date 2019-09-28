Araq/nim-beacon-chain

log raw incoming gossip data

beacon_chain/eth2_network.nim
@@ -246,6 +246,8 @@ else:
     result = proc(api: DaemonAPI,
                   ticket: PubsubTicket,
                   msg: PubSubMessage): Future[bool] {.async.} =
+      debug "Incoming gossip bytes",
+        peer = msg.peer, len = msg.data.len, tops = msg.topics
       msgHandler SSZ.decode(msg.data, MsgType)
       return true
 

status-im/nimbus-eth2

beacon_chain/eth2_network.nim
@@ -248,6 +248,8 @@ else:
     result = proc(api: DaemonAPI,
                   ticket: PubsubTicket,
                   msg: PubSubMessage): Future[bool] {.async.} =
+      trace "Incoming gossip bytes",
+        peer = msg.peer, len = msg.data.len, tops = msg.topics
       msgHandler SSZ.decode(msg.data, MsgType)
       return true
 