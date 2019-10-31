39d01b5a3aeba9f49ec71495d6fa8e62f3f27133
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-10-31 16:38:07 +0100

    fix symlink order

--- scripts/setup_official_tests.sh
+++ scripts/setup_official_tests.sh
@@ -106,7 +106,7 @@ cached_test_vectors() {
   }
 
   # Symlink tarballs if it doesn't already exist
-  ln -s "${CACHE_DIR}/tarballs" "${SUBREPO_DIR}"
+  ln -s "${SUBREPO_DIR}" "${CACHE_DIR}/tarballs"
 
   # Now uncompress
   pushd "${SUBREPO_DIR}"
