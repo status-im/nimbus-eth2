a644839b7949894009dc9c78a0de1742dc209477
Zahary Karadjov <zahary@gmail.com>
2019-11-01 17:14:34 +0200

    Dynamic SSZ navigator

--- beacon_chain/ssz/navigator.nim
+++ beacon_chain/ssz/navigator.nim
@@ -3,9 +3,9 @@ import
   ./types, ./bytes_reader
 
 type
-  MemRange = object
-    startAddr: ptr byte
-    length: int
+  MemRange* = object
+    startAddr*: ptr byte
+    length*: int
 
   SszNavigator*[T] = object
     m: MemRange
@@ -14,6 +14,18 @@ func sszMount*(data: openarray[byte], T: type): SszNavigator[T] =
   let startAddr = unsafeAddr data[0]
   SszNavigator[T](m: MemRange(startAddr: startAddr, length: data.len))
 
+template sszMount*(data: MemRange, T: type): SszNavigator[T] =
+  SszNavigator[T](m: data)
+
+template getMemRange*(n: SszNavigator): MemRange =
+  # Please note that this accessor was created intentionally.
+  # We don't want to expose the `m` field, because the navigated
+  # type may have a field by that name. We wan't any dot field
+  # access to be redirected to the navigated type.
+  # For this reason, this template should always be used with
+  # the function call syntax `getMemRange(n)`.
+  n.m
+
 template checkBounds(m: MemRange, offset: int) =
   if offset > m.length:
     raise newException(MalformedSszError, "Malformed SSZ")
@@ -21,9 +33,9 @@ template checkBounds(m: MemRange, offset: int) =
 template toOpenArray(m: MemRange): auto =
   makeOpenArray(m.startAddr, m.length)
 
-func navigateToField[T](n: SszNavigator[T],
-                        fieldName: static string,
-                        FieldType: type): SszNavigator[FieldType] =
+func navigateToField*[T](n: SszNavigator[T],
+                         fieldName: static string,
+                         FieldType: type): SszNavigator[FieldType] =
   mixin toSszType
   type SszFieldType = type toSszType(default FieldType)
 
@@ -54,6 +66,47 @@ template `.`*[T](n: SszNavigator[T], field: untyped): auto =
   type RecType = T
   type FieldType = type(default(RecType).field)
   navigateToField(n, astToStr(field), FieldType)
+
+func indexVarSizeList(m: MemRange, idx: int): MemRange =
+  template readOffset(pos): int =
+    int fromSszBytes(uint32, makeOpenArray(shift(m.startAddr, pos), offsetSize))
+
+  let offsetPos = offsetSize * idx
+  checkBounds(m, offsetPos + offsetSize)
+
+  let firstOffset = readOffset 0
+  let listLen = firstOffset div offsetSize
+
+  if idx >= listLen:
+    # TODO: Use a RangeError here?
+    # This would require the user to check the `len` upfront
+    raise newException(MalformedSszError, "Indexing past the end")
+
+  let elemPos = readOffset offsetPos
+  checkBounds(m, elemPos)
+
+  let endPos = if idx < listLen - 1:
+    let nextOffsetPos = offsetPos + offsetSize
+    # TODO. Is there a way to remove this bounds check?
+    checkBounds(m, nextOffsetPos + offsetSize)
+    readOffset(offsetPos + nextOffsetPos)
+  else:
+    m.length
+
+  MemRange(startAddr: m.startAddr.shift(elemPos), length: endPos - elemPos)
+
+template `[]`*[T](n: SszNavigator[seq[T]], idx: int): SszNavigator[T] =
+  type R = T
+  mixin toSszType
+  type ElemType = type toSszType(default T)
+  when isFixedSize(ElemType):
+    const elemSize = fixedPortionSize(ElemType)
+    let elemPos = idx * elemSize
+    checkBounds(n.m, elemPos + elemSize)
+    SszNavigator[R](m: MemRange(startAddr: shift(n.m.startAddr, elemPos),
+                                length: elemSize))
+  else:
+    SszNavigator[R](m: indexVarSizeList(n.m, idx))
 
 func `[]`*[T](n: SszNavigator[T]): T =
   readSszValue(toOpenArray(n.m), T)
