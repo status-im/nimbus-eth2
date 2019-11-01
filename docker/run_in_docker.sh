8124dbb93cdb47325d0913c860d8e78e903b4db7
Zahary Karadjov <zahary@gmail.com>
2019-11-01 17:15:53 +0200

    Remove the --nat:none forcing in Docker

--- docker/run_in_docker.sh
+++ docker/run_in_docker.sh
@@ -9,13 +9,5 @@
 killall p2pd
 rm -rf /tmp/*
 
-if [[ "$2" == "" ]]; then
-  # TODO This is a normal execution of a long-running testnet node.
-  # If the nat is enabled at the moment, the node fails to start.
-  beacon_node --nat:none
-else
-  # This is a one-off command such as createTestnet.
-  # We cannot reuse the command above, because the --nat option
-  # is not compatible with most of the commands.
-  beacon_node "$@"
-fi
+beacon_node "$@"
+
