f5e9b9a922678a13bfaac38580b9f8a8c85bf7f5
Zahary Karadjov <zahary@gmail.com>
2019-11-11 01:28:13 +0000

    Allow running local nodes without attached validators
    The number of user nodes is now specified with `USER_NODES`.
    
    To make the instructions more stable, the "numeric id" of the user
    nodes will be starting from 0 (so you can always use `run_node.sh 0`
    to start a user node).
    
    If you specify a node index above the total number of nodes, you'll
    launch a node without any validators attached (this is useful for
    testing the sync for example).

--- tests/simulation/run_node.sh
+++ tests/simulation/run_node.sh
@@ -14,24 +14,24 @@ source "${SIM_ROOT}/../../env.sh"
 cd "$GIT_ROOT"
 
 DATA_DIR="${SIMULATION_DIR}/node-$NODE_ID"
-
-V_PREFIX="${VALIDATORS_DIR}/v$(printf '%06d' $NODE_ID)"
-PORT=$(printf '5%04d' $NODE_ID)
+PORT=$(( BASE_P2P_PORT + NODE_ID ))
 
 NAT_FLAG="--nat:none"
 if [ "${NAT:-}" == "1" ]; then
   NAT_FLAG="--nat:any"
 fi
 
-FIRST_VALIDATOR_IDX=$(( (NUM_VALIDATORS / ($NUM_NODES + $NUM_MISSING_NODES)) * $NODE_ID ))
-LAST_VALIDATOR_IDX=$(( (NUM_VALIDATORS / ($NUM_NODES + $NUM_MISSING_NODES)) * ($NODE_ID + 1) - 1 ))
-
 mkdir -p $DATA_DIR/validators
 rm -f $DATA_DIR/validators/*
 
-pushd $VALIDATORS_DIR >/dev/null
-  cp $(seq -s " " -f v%07g.privkey $FIRST_VALIDATOR_IDX $LAST_VALIDATOR_IDX) $DATA_DIR/validators
-popd >/dev/null
+if [[ $NODE_ID -lt $TOTAL_NODES ]]; then
+  FIRST_VALIDATOR_IDX=$(( (NUM_VALIDATORS / TOTAL_NODES) * NODE_ID ))
+  LAST_VALIDATOR_IDX=$(( (NUM_VALIDATORS / TOTAL_NODES) * (NODE_ID + 1) - 1 ))
+
+  pushd $VALIDATORS_DIR >/dev/null
+    cp $(seq -s " " -f v%07g.privkey $FIRST_VALIDATOR_IDX $LAST_VALIDATOR_IDX) $DATA_DIR/validators
+  popd >/dev/null
+fi
 
 $BEACON_NODE_BIN \
   --bootstrap-file=$NETWORK_BOOTSTRAP_FILE \
@@ -46,5 +46,5 @@ $BEACON_NODE_BIN \
   --metrics-server=on \
   --metrics-server-address="127.0.0.1" \
   --metrics-server-port="$(( $BASE_METRICS_PORT + $NODE_ID ))" \
-  "$@"
+  "$@" | tee "$DATA_DIR/node.log"
 
