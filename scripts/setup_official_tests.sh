2fdfba1f34a2fbf1e3a811ba36d1d2d464953fe0
Dustin Brody <tersec@users.noreply.github.com>
2019-11-21 09:38:27 +0100

    don't need git-lfs anymore, since test vector download moved to curl from github; remove some unused OS/xz detection code from test vector download script

--- scripts/setup_official_tests.sh
+++ scripts/setup_official_tests.sh
@@ -9,7 +9,6 @@
 
 set -e
 
-TMP_CACHE_DIR="tmpcache"
 SUBREPO_DIR="tests/official/fixtures"
 # verbosity level
 [[ -z "$V" ]] && V=0
@@ -17,21 +16,6 @@ SUBREPO_DIR="tests/official/fixtures"
 CACHE_DIR="$1" # optional parameter pointing to a CI cache dir. Without it, we just download the LFS files for a local `make test`.
 
 [[ -d "${SUBREPO_DIR}" ]] || { echo "This script should be run from the \"nim-beacon-chain\" repo top dir."; exit 1; }
-
-# macOS quirks
-if uname | grep -qi "darwin"; then
-	ON_MACOS=1
-	STAT_FORMAT="-f %m"
-else
-	ON_MACOS=0
-	STAT_FORMAT="-c %Y"
-fi
-
-# to and from stdout
-DECOMPRESS_XZ="false"
-COMPRESS_XZ="false"
-which 7z &>/dev/null && { DECOMPRESS_XZ="7z e -txz -bd -so"; COMPRESS_XZ="7z a -txz -an -bd -si -so"; }
-which xz &>/dev/null && { DECOMPRESS_XZ="xz -d -c -T 0"; COMPRESS_XZ="xz -c -T 0"; }
 
 # script output
 echo -e "$BUILD_MSG"
