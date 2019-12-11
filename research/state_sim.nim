jangko/nim-beacon-chain

add simple wasm page with state simulator

research/state_sim.nim
@@ -31,23 +31,34 @@ template withTimerRet(stats: var RunningStat, body: untyped): untyped =
 
   tmp
 
-proc writeJson*(prefix, slot, v: auto) =
+proc jsonName(prefix, slot: auto): string =
+  fmt"{prefix:04}-{humaneSlotNum(slot):08}.json"
+
+proc writeJson*(fn, v: auto) =
   var f: File
   defer: close(f)
-  let fileName = fmt"{prefix:04}-{humaneSlotNum(slot):08}.json"
-  Json.saveFile(fileName, v, pretty = true)
+  Json.saveFile(fn, v, pretty = true)
 
 cli do(slots = 448,
        validators = SLOTS_PER_EPOCH * 9, # One per shard is minimum
        json_interval = SLOTS_PER_EPOCH,
+       write_last_json = false,
        prefix = 0,
        attesterRatio {.desc: "ratio of validators that attest in each round"} = 0.9,
        validate = true):
+  echo "Preparing validators..."
   let
     flags = if validate: {} else: {skipValidation}
+    deposits = makeInitialDeposits(validators, flags)
+
+  echo "Generating Genesis..."
+
+  let
     genesisState = initialize_beacon_state_from_eth1(
-      makeInitialDeposits(validators, flags), 0, Eth1Data(), flags)
+      deposits, 0, Eth1Data(), flags)
     genesisBlock = get_initial_beacon_block(genesisState)
+
+  echo "Starting simulation..."
 
   var
     attestations = initTable[Slot, seq[Attestation]]()
@@ -57,17 +68,28 @@ cli do(slots = 448,
     attesters: RunningStat
     r: Rand
     blck: BeaconBlock
+
+  var
     cache = get_empty_per_epoch_cache()
 
-  proc maybeWrite() =
-    if state.slot mod json_interval.uint64 == 0:
-      writeJson(prefix, state.slot, state)
-      write(stdout, ":")
+  proc maybeWrite(last: bool) =
+    if write_last_json:
+      if state.slot mod json_interval.uint64 == 0:
+        write(stdout, ":")
+      else:
+        write(stdout, ".")
+
+      if last:
+        writeJson("state.json", state)
     else:
-      write(stdout, ".")
+      if state.slot mod json_interval.uint64 == 0:
+        writeJson(jsonName(prefix, state.slot), state)
+        write(stdout, ":")
+      else:
+        write(stdout, ".")
 
   for i in 0..<slots:
-    maybeWrite()
+    maybeWrite(false)
 
     let
       attestations_idx = state.slot
@@ -142,9 +164,10 @@ cli do(slots = 448,
       echo &" slot: {humaneSlotNum(state.slot)} ",
         &"epoch: {humaneEpochNum(state.slot.compute_epoch_of_slot)}"
 
-  maybeWrite() # catch that last state as well..
 
-  echo "done!"
+  maybeWrite(true) # catch that last state as well..
+
+  echo "Done!"
 
   echo "Validators: ", validators, ", epoch length: ", SLOTS_PER_EPOCH
   echo "Validators per attestation (mean): ", attesters.mean

status-im/nimbus-eth2

research/state_sim.nim
@@ -2,7 +2,7 @@ import
   confutils, stats, times, std/monotimes,
   strformat,
   options, sequtils, random, tables,
-  ../tests/[testutil, testblockutil],
+  ../tests/[testblockutil],
   ../beacon_chain/spec/[beaconstate, crypto, datatypes, digest, helpers, validator],
   ../beacon_chain/[attestation_pool, extras, ssz]
 
@@ -13,11 +13,31 @@ type Timers = enum
   tShuffle = "Retrieve committee once using get_beacon_committee"
   tAttest = "Combine committee attestations"
 
-proc writeJson*(prefix, slot, v: auto) =
+template withTimer(stats: var RunningStat, body: untyped) =
+  let start = cpuTime()
+
+  block:
+    body
+
+  let stop = cpuTime()
+  stats.push stop - start
+
+template withTimerRet(stats: var RunningStat, body: untyped): untyped =
+  let start = cpuTime()
+  let tmp = block:
+    body
+  let stop = cpuTime()
+  stats.push stop - start
+
+  tmp
+
+proc jsonName(prefix, slot: auto): string =
+  fmt"{prefix:04}-{shortLog(slot):08}.json"
+
+proc writeJson*(fn, v: auto) =
   var f: File
   defer: close(f)
-  let fileName = fmt"{prefix:04}-{shortLog(slot):08}.json"
-  Json.saveFile(fileName, v, pretty = true)
+  Json.saveFile(fn, v, pretty = true)
 
 func verifyConsensus(state: BeaconState, attesterRatio: auto) =
   if attesterRatio < 0.63:
@@ -38,15 +58,23 @@ func verifyConsensus(state: BeaconState, attesterRatio: auto) =
 cli do(slots = SLOTS_PER_EPOCH * 6,
        validators = SLOTS_PER_EPOCH * 30, # One per shard is minimum
        json_interval = SLOTS_PER_EPOCH,
+       write_last_json = false,
        prefix = 0,
        attesterRatio {.desc: "ratio of validators that attest in each round"} = 0.73,
        validate = true):
+  echo "Preparing validators..."
   let
     flags = if validate: {} else: {skipValidation}
-    genesisState = initialize_beacon_state_from_eth1(
-      Eth2Digest(), 0,
-      makeInitialDeposits(validators, flags), flags)
+    deposits = makeInitialDeposits(validators, flags)
+
+  echo "Generating Genesis..."
+
+  let
+    genesisState =
+      initialize_beacon_state_from_eth1(Eth2Digest(), 0, deposits, flags)
     genesisBlock = get_initial_beacon_block(genesisState)
+
+  echo "Starting simulation..."
 
   var
     attestations = initTable[Slot, seq[Attestation]]()
@@ -56,21 +84,32 @@ cli do(slots = SLOTS_PER_EPOCH * 6,
     attesters: RunningStat
     r: Rand
     blck: BeaconBlock
+
+  var
     cache = get_empty_per_epoch_cache()
 
-  proc maybeWrite() =
-    if state.slot mod json_interval.uint64 == 0:
-      writeJson(prefix, state.slot, state)
-      write(stdout, ":")
+  proc maybeWrite(last: bool) =
+    if write_last_json:
+      if state.slot mod json_interval.uint64 == 0:
+        write(stdout, ":")
+      else:
+        write(stdout, ".")
+
+      if last:
+        writeJson("state.json", state)
     else:
-      write(stdout, ".")
+      if state.slot mod json_interval.uint64 == 0:
+        writeJson(jsonName(prefix, state.slot), state)
+        write(stdout, ":")
+      else:
+        write(stdout, ".")
 
   # TODO doAssert against this up-front
   # indexed attestation: validator index beyond max validators per committee
   # len(indices) <= MAX_VALIDATORS_PER_COMMITTEE
 
   for i in 0..<slots:
-    maybeWrite()
+    maybeWrite(false)
     verifyConsensus(state, attesterRatio)
 
     let
@@ -143,9 +182,10 @@ cli do(slots = SLOTS_PER_EPOCH * 6,
       echo &" slot: {shortLog(state.slot)} ",
         &"epoch: {shortLog(state.slot.compute_epoch_at_slot)}"
 
-  maybeWrite() # catch that last state as well..
 
-  echo "done!"
+  maybeWrite(true) # catch that last state as well..
+
+  echo "Done!"
 
   echo "Validators: ", validators, ", epoch length: ", SLOTS_PER_EPOCH
   echo "Validators per attestation (mean): ", attesters.mean