8c041171200be3854bf9ff5720590f60f1600c2e
Zahary Karadjov <zahary@gmail.com>
2020-03-24 13:13:07 +0200

    Spec-compliant implementation of Eth1 monitoring; Eth1-enabled local sim
    BEWARE! This commit will trigger a stack overflow during local sim

--- scripts/connect_to_testnet.nims
+++ scripts/connect_to_testnet.nims
@@ -140,5 +140,6 @@ cli do (skipGoerliKey {.
     --web3-url={web3Url}
     {bootstrapFileOpt}
     {logLevelOpt}
+    {depositContractOpt}
     --state-snapshot="{testnetDir/genesisFile}" """ & depositContractOpt, "\n", " ")
 
---
+++ tests/test_mainchain_monitor.nim
@@ -0,0 +1,52 @@
+import
+  unittest,
+  chronos, web3/ethtypes,
+  ../beacon_chain/mainchain_monitor
+
+type
+  MockDataProvider = ref object of DataProvider
+    
+
+method getBlockByHash*(p: MockDataProvider, hash: BlockHash): Future[BlockObject] {.
+  async
+  gcsafe
+  # raises: [Defect]
+.} =
+  return BlockObject()
+
+method onDisconnect*(p: MockDataProvider, handler: DisconnectHandler) {.
+  async
+  gcsafe
+  # raises: []
+.} =
+  discard
+
+method onDepositEvent*(p: MockDataProvider,
+                       startBlock: Eth1BlockNumber,
+                       handler: DepositEventHandler): Future[void] {.
+  async
+  gcsafe
+  # raises: []
+.} =
+  discard
+
+method close*(p: MockDataProvider): Future[void] {.
+  async
+  gcsafe
+  # raises: [Defect]
+.} =
+  discard
+
+method fetchDepositData*(p: MockDataProvider,
+                         web3Block: BlockObject): Future[Eth1Block] {.
+  async
+  gcsafe
+  # raises: [Defect, CatchableError]
+.} =
+  return Eth1Block()
+
+suite "Eth1 Chain":
+  discard
+
+suite "Mainchain monitor":
+  discard
