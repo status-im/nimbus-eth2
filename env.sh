eb0989b9371785cc583f2d9d85c8d71561affb2d
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2020-05-02 15:58:32 +0200

    remove unused files

--- env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# We use ${BASH_SOURCE[0]} instead of $0 to allow sourcing this file
-# and we fall back to a Zsh-specific special var to also support Zsh.
-REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
-ABS_PATH="$(cd ${REL_PATH}; pwd)"
-source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
-
