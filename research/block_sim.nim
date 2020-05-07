2e664f6566cbb5e5d2c152873243d51a7137141c
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-05-07 18:33:43 +0200

    Fix blocksim blockpool init

--- research/block_sim.nim
+++ research/block_sim.nim
@@ -55,7 +55,7 @@ cli do(slots = SLOTS_PER_EPOCH * 6,
 
   var
     blockPool = BlockPool.init(db)
-    attPool = AttestationPool.init(blockPool, blockPool.finalizedHead)
+    attPool = AttestationPool.init(blockPool)
     timers: array[Timers, RunningStat]
     attesters: RunningStat
     r = initRand(1)
