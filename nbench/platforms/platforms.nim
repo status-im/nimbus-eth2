fff21f174caf41f4b6562ce44c8272f2c123f75a
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-05-14 21:13:58 +0200

    windows does not support inline ASM

--- nbench/platforms/platforms.nim
+++ nbench/platforms/platforms.nim
@@ -5,8 +5,19 @@
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
+const GccCompatible = defined(gcc) or defined(clang)
+const x86arch = defined(i386) or defined(amd64)
 
-when defined(i386) or defined(amd64):
+const supports_x86_inline_asm = block:
+  x86arch and (
+    (
+      GccCompatible and not defined(windows)
+    ) or (
+      defined(vcc)
+    )
+  )
+
+when supports_x86_inline_asm:
   import x86
   export getTicks, cpuName
 
