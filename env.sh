7d3160b0b52083c5109bbb1ea9010a48d936bb7c
Zahary Karadjov <zahary@gmail.com>
2020-05-25 21:55:07 +0300

    Local sim now includes a prometheus server, a Ganache instance and a simulation dashboard server (in Tmux mode)

--- env.sh
+++ env.sh
@@ -4,5 +4,22 @@
 # and we fall back to a Zsh-specific special var to also support Zsh.
 REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
 ABS_PATH="$(cd ${REL_PATH}; pwd)"
+
+# Activate nvm only when this file is sourced without arguments:
+if [ -z "$*" ]; then
+  if command -v nvm > /dev/null; then
+    nvm use
+    command -v ganache-cli > /dev/null || { npm install -g ganache-cli; }
+  else
+    echo <<EOF
+  In order to use Ganache (a development ETH1 chain), please install NVM with:
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+  For more info:
+  https://github.com/nvm-sh/nvm
+EOF
+  fi
+fi
+
 source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
 
