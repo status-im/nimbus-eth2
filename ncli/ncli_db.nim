e692aa426fe35efd7a279f7112821ee01224bab8
Jacek Sieka <jacek@status.im>
2020-06-01 16:48:24 +0200

    ncli_db: add validation flag, better ux

--- ncli/ncli_db.nim
+++ ncli/ncli_db.nim
@@ -1,7 +1,7 @@
-import
+  import
   confutils, stats, chronicles, strformat, tables,
   ../beacon_chain/block_pool,
-  ../beacon_chain/spec/[crypto, datatypes],
+  ../beacon_chain/spec/[crypto, datatypes, helpers],
   ../beacon_chain/[beacon_chain_db, extras, state_transition, ssz],
   ../research/simutils,
   eth/db/[kvstore, kvstore_sqlite3]
@@ -11,13 +11,35 @@ type Timers = enum
   tLoadBlock = "Load block from database"
   tLoadState = "Load state from database"
   tApplyBlock = "Apply block"
+  tApplyEpochBlock = "Apply epoch block"
 
-cli do(databaseDir: string, cmd: string):
+type
+  DbCmd* = enum
+    bench
+
+  DbConf = object
+    databaseDir* {.
+        defaultValue: ""
+        desc: "Directory where `nbc.sqlite` is stored"
+        name: "db" }: InputDir
+
+    case cmd* {.
+      command
+      desc: "Run benchmark by applying all blocks from database"
+      .}: DbCmd
+
+    of bench:
+      validate* {.
+        defaultValue: true
+        desc: "Enable BLS validation" }: bool
+
+proc cmdBench(conf: DbConf) =
   var timers: array[Timers, RunningStat]
 
   echo "Opening database..."
   let
-    db = BeaconChainDB.init(kvStore SqStoreRef.init(databaseDir, "nbc").tryGet())
+    db = BeaconChainDB.init(
+      kvStore SqStoreRef.init(conf.databaseDir.string, "nbc").tryGet())
 
   if not BlockPool.isInitialized(db):
     echo "Database not initialized"
@@ -29,30 +51,40 @@ cli do(databaseDir: string, cmd: string):
 
   echo &"Loaded {pool.blocks.len} blocks, head slot {pool.head.blck.slot}"
 
-  case cmd
-  of "bench":
-    var
-      blockRefs: seq[BlockRef]
-      blocks: seq[SignedBeaconBlock]
-      cur = pool.head.blck
+  var
+    blockRefs: seq[BlockRef]
+    blocks: seq[SignedBeaconBlock]
+    cur = pool.head.blck
 
-    while cur != nil:
-      blockRefs.add cur
-      cur = cur.parent
+  while cur != nil:
+    blockRefs.add cur
+    cur = cur.parent
 
-    for b in 1..<blockRefs.len: # Skip genesis block
-      withTimer(timers[tLoadBlock]):
-        blocks.add db.getBlock(blockRefs[blockRefs.len - b - 1].root).get()
+  for b in 1..<blockRefs.len: # Skip genesis block
+    withTimer(timers[tLoadBlock]):
+      blocks.add db.getBlock(blockRefs[blockRefs.len - b - 1].root).get()
 
-    let state = (ref HashedBeaconState)(
-      root: db.getBlock(blockRefs[^1].root).get().message.state_root
-    )
+  let state = (ref HashedBeaconState)(
+    root: db.getBlock(blockRefs[^1].root).get().message.state_root
+  )
 
-    withTimer(timers[tLoadState]):
-      discard db.getState(state[].root, state[].data, noRollback)
+  withTimer(timers[tLoadState]):
+    discard db.getState(state[].root, state[].data, noRollback)
 
-    for b in blocks:
-      withTimer(timers[tApplyBlock]):
-        discard state_transition(state[], b, {}, noRollback)
+  let flags = if conf.validate: {} else: {skipBlsValidation}
+  for b in blocks:
+    let
+      isEpoch = state[].data.slot.compute_epoch_at_slot !=
+        b.message.slot.compute_epoch_at_slot
+    withTimer(timers[if isEpoch: tApplyEpochBlock else: tApplyBlock]):
+      discard state_transition(state[], b, flags, noRollback)
 
-  printTimers(true, timers)
+  printTimers(conf.validate, timers)
+
+when isMainModule:
+  let
+    config = DbConf.load()
+
+  case config.cmd
+  of bench:
+    cmdBench(config)
