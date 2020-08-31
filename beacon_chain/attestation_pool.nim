4473ab98e91b307745be9f18804457b9cf895c04
Dustin Brody <tersec@users.noreply.github.com>
2020-08-31 12:01:29 +0200

    ignore sqlite WAL journal files in git; switch attestation resolved from info to debug

--- beacon_chain/attestation_pool.nim
+++ beacon_chain/attestation_pool.nim
@@ -152,7 +152,7 @@ proc addAttestation*(pool: var AttestationPool,
           attestation.data.slot, participants, attestation.data.beacon_block_root,
           wallSlot)
 
-        info "Attestation resolved",
+        debug "Attestation resolved",
           attestation = shortLog(attestation),
           validations = a.validations.len()
 
@@ -169,7 +169,7 @@ proc addAttestation*(pool: var AttestationPool,
       attestation.data.slot, participants, attestation.data.beacon_block_root,
       wallSlot)
 
-    info "Attestation resolved",
+    debug "Attestation resolved",
       attestation = shortLog(attestation),
       validations = 1
 
