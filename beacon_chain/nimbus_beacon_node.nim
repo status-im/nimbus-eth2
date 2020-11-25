be107df7f116163bf30f09bc28908fc7109b9465
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2020-11-25 22:56:52 +0100

    status bar: actually display it every second, after updating its data

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -1028,6 +1028,8 @@ when hasPrompt:
       proc statusBarUpdatesPollingLoop() {.async.} =
         while true:
           update statusBar
+          erase statusBar
+          render statusBar
           await sleepAsync(chronos.seconds(1))
 
       traceAsyncErrors statusBarUpdatesPollingLoop()
