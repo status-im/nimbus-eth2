dfd99ec94336b784d71845421ac2c15288f8cef1
tersec <tersec@users.noreply.github.com>
2021-03-22 14:44:45 +0000

    Altair (HF1/v1.1.0) minimal and mainnet presets/constants (#2444)
    * Altair mainnet & minimal presets
    
    * std/math not used

---
+++ beacon_chain/spec/presets/altair/mainnet.nim
@@ -0,0 +1,50 @@
+# beacon_chain
+# Copyright (c) 2021 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+# This file contains constants that are part of the spec and thus subject to
+# serialization and spec updates.
+
+const
+  # Updated penalty values
+  # ---------------------------------------------------------------
+  # https://github.com/ethereum/eth2.0-specs/blob/v1.1.0-alpha.1/configs/mainnet/altair.yaml#L5
+  CONFIG_NAME* = "mainnet"
+
+  INACTIVITY_PENALTY_QUOTIENT_ALTAIR* = 50331648 ##\
+  ## 3 * 2**24 (= 50,331,648)
+
+  MIN_SLASHING_PENALTY_QUOTIENT_ALTAIR* = 64
+  PROPORTIONAL_SLASHING_MULTIPLIER_ALTAIR* = 2
+
+  # Misc
+  # ---------------------------------------------------------------
+  # https://github.com/ethereum/eth2.0-specs/blob/v1.1.0-alpha.1/configs/mainnet/altair.yaml#L15
+  SYNC_COMMITTEE_SIZE* = 1024
+  SYNC_SUBCOMMITTEE_SIZE* = 64
+  INACTIVITY_SCORE_BIAS* = 4
+
+  # Time parameters
+  # ---------------------------------------------------------------
+  # https://github.com/ethereum/eth2.0-specs/blob/v1.1.0-alpha.1/configs/mainnet/altair.yaml#L25
+  EPOCHS_PER_SYNC_COMMITTEE_PERIOD* = 256
+
+  # Signature domains (DOMAIN_SYNC_COMMITTEE) in spec/datatypes/base
+
+  # Fork
+  # ---------------------------------------------------------------
+  # https://github.com/ethereum/eth2.0-specs/blob/v1.1.0-alpha.1/configs/mainnet/altair.yaml#L36
+
+  # ALTAIR_FORK_VERSION is a runtime preset
+
+  ALTAIR_FORK_SLOT* = 0 # TBD
+
+  # Sync protocol
+  # ---------------------------------------------------------------
+  # https://github.com/ethereum/eth2.0-specs/blob/v1.1.0-alpha.1/configs/mainnet/altair.yaml#L43
+  MIN_SYNC_COMMITTEE_PARTICIPANTS* = 1
+  MAX_VALID_LIGHT_CLIENT_UPDATES* = 8192
+  LIGHT_CLIENT_UPDATE_TIMEOUT* = 8192
