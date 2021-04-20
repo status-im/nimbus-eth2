865f2c884cbb786a554f7b07445c91842ed62a3b
Dustin Brody <tersec@users.noreply.github.com>
2021-04-20 13:09:47 +0200

    clean up rpc module imports

--- beacon_chain/eth2_merge_web3.nim
+++ beacon_chain/eth2_merge_web3.nim
@@ -2,12 +2,9 @@ import
   json_serialization/std/[options, sets, net], serialization/errors,
   "."/[
     beacon_clock, version],
-  #./networking/[eth2_discovery, network_metadata],
-  #./gossip_processing/eth2_processor,
   ./rpc/rest_utils,
   ./spec/[
-    datatypes, digest, crypto, eth2_apis/beacon_rpc_client,
-    helpers, presets],
+    datatypes, digest, crypto, eth2_apis/beacon_rpc_client],
   ./eth1/eth1_monitor
 
 # Keep changes in one repo
