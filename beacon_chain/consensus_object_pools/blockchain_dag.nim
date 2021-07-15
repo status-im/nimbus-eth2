status-im/nim-beacon-chain

Initial implementation of sync committees (wip)

beacon_chain/consensus_object_pools/blockchain_dag.nim
@@ -455,6 +455,9 @@ proc init*(T: type ChainDAGRef,
 
   dag
 
+template genesisValidatorsRoot*(dag: ChainDAGRef): Eth2Digest =
+  getStateField(dag.headState.data, genesis_validators_root)
+
 func getEpochRef*(
     dag: ChainDAGRef, state: StateData, cache: var StateCache): EpochRef =
   let
@@ -544,6 +547,12 @@ func stateCheckpoint*(bs: BlockSlot): BlockSlot =
   while not isStateCheckPoint(bs):
     bs = bs.parentOrSlot
   bs
+
+proc forkAtSlot*(dag: ChainDAGRef, slot: Slot): Fork =
+  if slot < dag.altairTransitionSlot:
+    genesisFork(dag.runtimePreset)
+  else:
+    altairFork(dag.runtimePreset, dag.altairTransitionSlot.epoch)
 
 proc forkDigestAtSlot*(dag: ChainDAGRef, slot: Slot): ForkDigest =
   if slot < dag.altairTransitionSlot:
@@ -952,6 +961,28 @@ proc pruneBlocksDAG(dag: ChainDAGRef) =
     currentCandidateHeads = dag.heads.len,
     prunedHeads = hlen - dag.heads.len,
     dagPruneDur = Moment.now() - startTick
+
+func isSyncCommitteeMember*(blockRef: BlockRef,
+                            committeeIdx: SubnetId,
+                            validatorIdx: uint64): bool =
+  # TODO
+  true
+
+template syncCommitteeParticipants*(blockRef: BlockRef): openarray[ValidatorIndex] =
+  newSeq[ValidatorIndex]()
+
+template syncCommitteeParticipants*(
+    blockRef: BlockRef,
+    committeeIdx: SubnetId): openarray[ValidatorIndex] =
+  newSeq[ValidatorIndex]()
+
+iterator syncCommitteeParticipants*(
+    blockRef: BlockRef,
+    committeeIdx: SubnetId,
+    aggregationBits: SyncCommitteeAggregationBits): ValidatorIndex =
+  for pos, valIdx in syncCommitteeParticipants(blockRef, committeeIdx):
+    if aggregationBits[pos]:
+      yield valIdx
 
 func needStateCachesAndForkChoicePruning*(dag: ChainDAGRef): bool =
   dag.lastPrunePoint != dag.finalizedHead

beacon_chain/spec/beaconstate.nim
@@ -268,6 +268,18 @@ proc slash_validator*(state: var SomeBeaconState, slashed_index: ValidatorIndex,
 func genesis_time_from_eth1_timestamp*(preset: RuntimePreset, eth1_timestamp: uint64): uint64 =
   eth1_timestamp + preset.GENESIS_DELAY
 
+func genesisFork*(preset: RuntimePreset): Fork =
+  Fork(
+    previous_version: preset.GENESIS_FORK_VERSION,
+    current_version: preset.GENESIS_FORK_VERSION,
+    epoch: GENESIS_EPOCH)
+
+func altairFork*(preset: RuntimePreset, epoch: Epoch): Fork =
+  Fork(
+    previous_version: preset.GENESIS_FORK_VERSION,
+    current_version: preset.ALTAIR_FORK_VERSION,
+    epoch: epoch)
+
 # https://github.com/ethereum/eth2.0-specs/blob/v1.0.1/specs/phase0/beacon-chain.md#genesis
 proc initialize_beacon_state_from_eth1*(
     preset: RuntimePreset,
@@ -292,10 +304,7 @@ proc initialize_beacon_state_from_eth1*(
   doAssert deposits.lenu64 >= SLOTS_PER_EPOCH
 
   var state = phase0.BeaconStateRef(
-    fork: Fork(
-      previous_version: preset.GENESIS_FORK_VERSION,
-      current_version: preset.GENESIS_FORK_VERSION,
-      epoch: GENESIS_EPOCH),
+    fork: genesisFork(preset),
     genesis_time: genesis_time_from_eth1_timestamp(preset, eth1_timestamp),
     eth1_data:
       Eth1Data(block_hash: eth1_block_hash, deposit_count: uint64(len(deposits))),
@@ -852,6 +861,7 @@ proc upgrade_to_altair*(pre: phase0.BeaconState): ref altair.BeaconState =
   let epoch = get_current_epoch(pre)
 
   # https://github.com/ethereum/eth2.0-specs/blob/v1.1.0-alpha.8/specs/altair/fork.md#configuration
+  # TODO: This should be read from a RuntimePreset
   const ALTAIR_FORK_VERSION = Version [byte 1, 0, 0, 0]
 
   var
@@ -869,6 +879,8 @@ proc upgrade_to_altair*(pre: phase0.BeaconState): ref altair.BeaconState =
     genesis_time: pre.genesis_time,
     genesis_validators_root: pre.genesis_validators_root,
     slot: pre.slot,
+    # TODO: Use the `altairFork` function
+    #      (this would require a RuntimePreset to reach here)
     fork: Fork(
       previous_version: pre.fork.current_version,
       current_version: ALTAIR_FORK_VERSION,

status-im/nimbus-eth2

beacon_chain/consensus_object_pools/blockchain_dag.nim
@@ -454,6 +454,9 @@ proc init*(T: type ChainDAGRef,
 
   dag
 
+template genesisValidatorsRoot*(dag: ChainDAGRef): Eth2Digest =
+  getStateField(dag.headState.data, genesis_validators_root)
+
 func getEpochRef*(
     dag: ChainDAGRef, state: StateData, cache: var StateCache): EpochRef =
   let
@@ -543,6 +546,12 @@ func stateCheckpoint*(bs: BlockSlot): BlockSlot =
   while not isStateCheckPoint(bs):
     bs = bs.parentOrSlot
   bs
+
+proc forkAtSlot*(dag: ChainDAGRef, slot: Slot): Fork =
+  if slot.epoch < dag.cfg.ALTAIR_FORK_EPOCH:
+    genesisFork(dag.cfg)
+  else:
+    altairFork(dag.cfg)
 
 proc forkDigestAtSlot*(dag: ChainDAGRef, slot: Slot): ForkDigest =
   if slot.epoch < dag.cfg.ALTAIR_FORK_EPOCH:
@@ -955,6 +964,28 @@ proc pruneBlocksDAG(dag: ChainDAGRef) =
     currentCandidateHeads = dag.heads.len,
     prunedHeads = hlen - dag.heads.len,
     dagPruneDur = Moment.now() - startTick
+
+func isSyncCommitteeMember*(blockRef: BlockRef,
+                            committeeIdx: SubnetId,
+                            validatorIdx: uint64): bool =
+  # TODO
+  true
+
+template syncCommitteeParticipants*(blockRef: BlockRef): openarray[ValidatorIndex] =
+  newSeq[ValidatorIndex]()
+
+template syncCommitteeParticipants*(
+    blockRef: BlockRef,
+    committeeIdx: SubnetId): openarray[ValidatorIndex] =
+  newSeq[ValidatorIndex]()
+
+iterator syncCommitteeParticipants*(
+    blockRef: BlockRef,
+    committeeIdx: SubnetId,
+    aggregationBits: SyncCommitteeAggregationBits): ValidatorIndex =
+  for pos, valIdx in syncCommitteeParticipants(blockRef, committeeIdx):
+    if aggregationBits[pos]:
+      yield valIdx
 
 func needStateCachesAndForkChoicePruning*(dag: ChainDAGRef): bool =
   dag.lastPrunePoint != dag.finalizedHead

beacon_chain/spec/beaconstate.nim
@@ -203,6 +203,18 @@ proc slash_validator*(
 func genesis_time_from_eth1_timestamp*(cfg: RuntimeConfig, eth1_timestamp: uint64): uint64 =
   eth1_timestamp + cfg.GENESIS_DELAY
 
+func genesisFork*(cfg: RuntimeConfig): Fork =
+  Fork(
+    previous_version: cfg.GENESIS_FORK_VERSION,
+    current_version: cfg.GENESIS_FORK_VERSION,
+    epoch: GENESIS_EPOCH)
+
+func altairFork*(cfg: RuntimeConfig): Fork =
+  Fork(
+    previous_version: cfg.GENESIS_FORK_VERSION,
+    current_version: cfg.ALTAIR_FORK_VERSION,
+    epoch: cfg.ALTAIR_FORK_EPOCH)
+
 # https://github.com/ethereum/eth2.0-specs/blob/v1.0.1/specs/phase0/beacon-chain.md#genesis
 proc initialize_beacon_state_from_eth1*(
     cfg: RuntimeConfig,
@@ -227,10 +239,7 @@ proc initialize_beacon_state_from_eth1*(
   doAssert deposits.lenu64 >= SLOTS_PER_EPOCH
 
   var state = phase0.BeaconStateRef(
-    fork: Fork(
-      previous_version: cfg.GENESIS_FORK_VERSION,
-      current_version: cfg.GENESIS_FORK_VERSION,
-      epoch: GENESIS_EPOCH),
+    fork: genesisFork(cfg),
     genesis_time: genesis_time_from_eth1_timestamp(cfg, eth1_timestamp),
     eth1_data:
       Eth1Data(block_hash: eth1_block_hash, deposit_count: uint64(len(deposits))),
@@ -783,12 +792,7 @@ func translate_participation(
         state.previous_epoch_participation[index] =
           add_flag(state.previous_epoch_participation[index], flag_index)
 
-proc upgrade_to_altair*(pre: phase0.BeaconState): ref altair.BeaconState =
-  let epoch = get_current_epoch(pre)
-
-  # https://github.com/ethereum/eth2.0-specs/blob/v1.1.0-alpha.8/specs/altair/fork.md#configuration
-  const ALTAIR_FORK_VERSION = Version [byte 1, 0, 0, 0]
-
+proc upgrade_to_altair*(cfg: RuntimeConfig, pre: phase0.BeaconState): ref altair.BeaconState =
   var
     empty_participation =
       HashList[ParticipationFlags, Limit VALIDATOR_REGISTRY_LIMIT]()
@@ -804,11 +808,7 @@ proc upgrade_to_altair*(pre: phase0.BeaconState): ref altair.BeaconState =
     genesis_time: pre.genesis_time,
     genesis_validators_root: pre.genesis_validators_root,
     slot: pre.slot,
-    fork: Fork(
-      previous_version: pre.fork.current_version,
-      current_version: ALTAIR_FORK_VERSION,
-      epoch: epoch
-    ),
+    fork: altairFork(cfg),
 
     # History
     latest_block_header: pre.latest_block_header,