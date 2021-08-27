6d47d96c8447dd55c9b1d5a6df17b8aad2b048f4
Jacek Sieka <jacek@status.im>
2021-08-27 18:54:51 +0200

    altair upgrade for prater (#2828)
    and a few import fixes for free

--- beacon_chain/validators/keystore_management.nim
+++ beacon_chain/validators/keystore_management.nim
@@ -9,7 +9,7 @@
 
 import
   std/[os, strutils, terminal, wordwrap, unicode],
-  chronicles, chronos, web3, stint, json_serialization, zxcvbn,
+  chronicles, chronos, json_serialization, zxcvbn,
   serialization, blscurve, eth/common/eth_types, eth/keys, confutils, bearssl,
   ../spec/[eth2_merkleization, keystore],
   ../spec/datatypes/base,
