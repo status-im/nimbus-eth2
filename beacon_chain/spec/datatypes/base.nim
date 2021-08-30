3689c68cbfb1123e605feccaf691117d00f516c0
zah <zahary@gmail.com>
2021-08-30 03:58:30 +0300

    Carry out the sync committee gossip duties
    Other changes:
    
    * Add server getBlockV2(), and produceBlockV2().
    * Add getBlockV2() to REST test suite.
    * Add client getBlockV2(), and produceBlockV2().
    * Fix URLs in comments.
    * Add some primitives and fix some issues in forks.nim.
    * Switch `validator_client` to V2 calls usage.
    * Bump `chronos` with imports fixes.
    * Bump `nim-json-serialization` for `requireAllFields`.

--- beacon_chain/spec/datatypes/base.nim
+++ beacon_chain/spec/datatypes/base.nim
@@ -56,7 +56,7 @@ export
 # Eventually, we could also differentiate between user/tainted data and
 # internal state that's gone through sanity checks already.
 
-const SPEC_VERSION* = "1.0.1"
+const SPEC_VERSION* = "1.1.0"
 ## Spec version we're aiming to be compatible with, right now
 
 const
