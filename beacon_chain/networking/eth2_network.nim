aade60fea5b3233bd03ab652ce8e1afc4cb89aea
Tanguy <tanguy@status.im>
2021-09-22 16:39:51 +0200

    Log score when kicking peer

--- beacon_chain/networking/eth2_network.nim
+++ beacon_chain/networking/eth2_network.nim
@@ -1005,7 +1005,7 @@ proc trimConnections(node: Eth2Node, count: int) {.async.} =
   for peerId in scores.keys:
     #TODO kill a single connection instead of the whole peer
     # Not possible with the current libp2p's conn management
-    debug "kicking peer", peerId
+    debug "kicking peer", peerId, score=scores[peerId]
     await node.switch.disconnect(peerId)
     dec toKick
     inc(nbc_cycling_kicked_peers)
