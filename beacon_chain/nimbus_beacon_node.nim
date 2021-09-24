21b5e212e65508bf7708714eedd2d541b5605f47
Dustin Brody <tersec@users.noreply.github.com>
2021-09-24 07:34:34 +0000

    no phase0 + no sync aggregates

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -425,20 +425,17 @@ proc init*(T: type BeaconNode,
             getProposerSlashingsTopic(network.forkDigests.phase0),
             getVoluntaryExitsTopic(network.forkDigests.phase0),
             getAggregateAndProofsTopic(network.forkDigests.phase0),
-            getSyncCommitteeContributionAndProofTopic(network.forkDigests.phase0),
 
             getBeaconBlocksTopic(dag.forkDigests.altair),
             getAttesterSlashingsTopic(network.forkDigests.altair),
             getProposerSlashingsTopic(network.forkDigests.altair),
             getVoluntaryExitsTopic(network.forkDigests.altair),
             getAggregateAndProofsTopic(network.forkDigests.altair),
-            getSyncCommitteeContributionAndProofTopic(network.forkDigests.altair)
           ]
       for subnet_id in 0'u64 ..< ATTESTATION_SUBNET_COUNT:
         topics &= getAttestationTopic(network.forkDigests.phase0, SubnetId(subnet_id))
         topics &= getAttestationTopic(network.forkDigests.altair, SubnetId(subnet_id))
       for subnet_id in 0'u64 ..< SYNC_COMMITTEE_SUBNET_COUNT:
-        topics &= getSyncCommitteeTopic(network.forkDigests.phase0, SyncCommitteeIndex(subnet_id))
         topics &= getSyncCommitteeTopic(network.forkDigests.altair, SyncCommitteeIndex(subnet_id))
       topics)
 
