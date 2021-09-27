71da90562919928940eb625af2376787f341691a
Eugene Kabanov <eugene.kabanov@status.im>
2021-09-27 11:14:43 +0300

    Fix sync_committee_bits serialization. (#2899)
    * Fix REST `sync_committee_bits` serialization issue.
    
    * Fix error value.

--- beacon_chain/spec/eth2_apis/eth2_rest_serialization.nim
+++ beacon_chain/spec/eth2_apis/eth2_rest_serialization.nim
@@ -489,6 +489,19 @@ proc writeValue*(writer: var JsonWriter[RestJson], value: BitList) {.
      raises: [IOError, Defect].} =
   writeValue(writer, BitSeq value)
 
+## BitArray
+proc readValue*(reader: var JsonReader[RestJson], value: var BitArray) {.
+     raises: [IOError, SerializationError, Defect].} =
+  try:
+    hexToByteArray(readValue(reader, string), value.bytes)
+  except ValueError:
+    raiseUnexpectedValue(reader,
+                         "A BitArray value should be a valid hex string")
+
+proc writeValue*(writer: var JsonWriter[RestJson], value: BitArray) {.
+     raises: [IOError, Defect].} =
+  writeValue(writer, hexOriginal(value.bytes))
+
 ## Eth2Digest
 proc readValue*(reader: var JsonReader[RestJson], value: var Eth2Digest) {.
      raises: [IOError, SerializationError, Defect].} =
