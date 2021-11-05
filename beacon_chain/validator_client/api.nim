4fb998981e554662bad4d8351581eb51f12b4cb5
cheatfate <eugene.kabanov@status.im>
2021-11-05 03:50:58 +0200

    Fix generic sandwich.

--- beacon_chain/validator_client/api.nim
+++ beacon_chain/validator_client/api.nim
@@ -1,7 +1,6 @@
-import
-  ../spec/eth2_apis/eth2_rest_serialization,
-  ../spec/datatypes/[phase0, altair],
-  common
+import chronicles,
+       ../spec/eth2_apis/eth2_rest_serialization,
+       ../spec/datatypes/[phase0, altair], common
 
 export eth2_rest_serialization, common
 
