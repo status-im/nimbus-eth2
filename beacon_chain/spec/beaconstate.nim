ca3de9adfb917cd271cb832b43fb658b1dcca433
Zahary Karadjov <zahary@gmail.com>
2021-11-07 23:03:23 +0200

    Correct implementation of the /validator/duties/sync/{epoch} API
    According to the spec, this call should return the positions of
    the specified validators within the sync committee. The existing
    code was instead returning the indices of the sync sub-committees
    where the validator is a member.

--- beacon_chain/spec/beaconstate.nim
+++ beacon_chain/spec/beaconstate.nim
@@ -19,6 +19,9 @@ import
 
 export extras, forks, validator
 
+type
+  SomeBeaconState* = phase0.BeaconState | altair.BeaconState | merge.BeaconState
+
 # https://github.com/ethereum/consensus-specs/blob/v1.1.4/specs/phase0/beacon-chain.md#increase_balance
 func increase_balance*(balance: var Gwei, delta: Gwei) =
   balance += delta
@@ -855,3 +858,7 @@ func upgrade_to_merge*(cfg: RuntimeConfig, pre: altair.BeaconState):
     # Execution-layer
     latest_execution_payload_header: ExecutionPayloadHeader()
   )
+
+template isValidInState*(idx: ValidatorIndex, state: SomeBeaconState): bool =
+  idx.int < state.validators.len
+
--- beacon_chain/spec/eth2_apis/eth2_rest_serialization.nim
+++ beacon_chain/spec/eth2_apis/eth2_rest_serialization.nim
@@ -412,12 +412,12 @@ proc readValue*(reader: var JsonReader[RestJson], value: var Epoch) {.
     reader.raiseUnexpectedValue($res.error())
 
 ## ValidatorIndex
-proc writeValue*(writer: var JsonWriter[RestJson], value: ValidatorIndex) {.
-     raises: [IOError, Defect].} =
+proc writeValue*(writer: var JsonWriter[RestJson], value: ValidatorIndex)
+                {.raises: [IOError, Defect].} =
   writeValue(writer, Base10.toString(uint64(value)))
 
-proc readValue*(reader: var JsonReader[RestJson], value: var ValidatorIndex) {.
-     raises: [IOError, SerializationError, Defect].} =
+proc readValue*(reader: var JsonReader[RestJson], value: var ValidatorIndex)
+               {.raises: [IOError, SerializationError, Defect].} =
   let svalue = reader.readValue(string)
   let res = Base10.decode(uint64, svalue)
   if res.isOk():
@@ -427,6 +427,24 @@ proc readValue*(reader: var JsonReader[RestJson], value: var ValidatorIndex) {.
     else:
       reader.raiseUnexpectedValue(
         "Validator index is bigger then VALIDATOR_REGISTRY_LIMIT")
+  else:
+    reader.raiseUnexpectedValue($res.error())
+
+proc writeValue*(writer: var JsonWriter[RestJson], value: ValidatorIndexInSyncCommittee)
+                {.raises: [IOError, Defect].} =
+  writeValue(writer, Base10.toString(distinctBase(value)))
+
+proc readValue*(reader: var JsonReader[RestJson], value: var ValidatorIndexInSyncCommittee)
+               {.raises: [IOError, SerializationError, Defect].} =
+  let svalue = reader.readValue(string)
+  let res = Base10.decode(uint64, svalue)
+  if res.isOk():
+    let v = res.get()
+    if v < SYNC_COMMITTEE_SIZE:
+      value = ValidatorIndexInSyncCommittee(v)
+    else:
+      reader.raiseUnexpectedValue(
+        "Validator index is bigger then SYNC_COMMITTEE_SIZE")
   else:
     reader.raiseUnexpectedValue($res.error())
 
