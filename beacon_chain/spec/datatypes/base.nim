96b1500247c75d7ea0219072d1d6a36e53c7996c
Jacek Sieka <jacek@status.im>
2021-11-11 16:52:25 +0100

    work around compiler bug

--- beacon_chain/spec/datatypes/base.nim
+++ beacon_chain/spec/datatypes/base.nim
@@ -623,13 +623,10 @@ proc readValue*(reader: var JsonReader, value: var ForkDigest)
 static: doAssert high(int) >= high(int32)
 
 # `ValidatorIndex` seq handling.
-template `[]`*[T](a: var seq[T], b: ValidatorIndex): var T =
-  a[b.int]
-
 template `[]=`*[T](a: var seq[T], b: ValidatorIndex, c: T) =
   a[b.int] = c
 
-template `[]`*[T](a: seq[T], b: ValidatorIndex): auto =
+template `[]`*[T](a: seq[T], b: ValidatorIndex): auto = # Also var seq (!)
   a[b.int]
 
 # `ValidatorIndex` Nim integration
