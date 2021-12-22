6b4f32ae236b6632e78576da850aaebbab8ee9ba
Zahary Karadjov <zahary@gmail.com>
2021-12-22 17:59:24 +0200

    Replicate a recent fix from the launch_local_testnet script due to a wide-spread code duplication

--- tests/simulation/restapi.sh
+++ tests/simulation/restapi.sh
@@ -17,7 +17,7 @@ KILL_OLD_PROCESSES="0"
 GETOPT_BINARY="getopt"
 if uname | grep -qi darwin; then
   # macOS
-  GETOPT_BINARY="/usr/local/opt/gnu-getopt/bin/getopt"
+  GETOPT_BINARY=$(find /opt/homebrew/opt/gnu-getopt/bin/getopt /usr/local/opt/gnu-getopt/bin/getopt 2> /dev/null || true)
   [[ -f "$GETOPT_BINARY" ]] || { echo "GNU getopt not installed. Please run 'brew install gnu-getopt'. Aborting."; exit 1; }
 fi
 
