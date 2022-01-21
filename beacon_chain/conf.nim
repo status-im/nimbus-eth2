095f12b3179ee93a6805fde41d0f6c377611b047
cheatfate <eugene.kabanov@status.im>
2022-01-21 09:35:06 +0200

    Fix comments.

--- beacon_chain/conf.nim
+++ beacon_chain/conf.nim
@@ -341,19 +341,18 @@ type
 
       restRequestTimeout* {.
         defaultValue: 0
-        desc: "The number of seconds to wait until complete REST request will be received"
+        desc: "The number of seconds to wait until complete REST request " &
+              "will be received"
         name: "rest-request-timeout".}: Natural
 
       restMaxRequestBodySize* {.
         defaultValue: 16_384
-        desc: "Maximum size of REST's request body in kilobytes. Increase " &
-              "this value, if you going to want to manage thousands of " &
-              "validators using REST"
+        desc: "Maximum size of REST request body (kilobytes)"
         name: "rest-max-body-size" .}: Natural
 
       restMaxRequestHeadersSize* {.
         defaultValue: 64
-        desc: "Maximum size of REST's request headers in kilobytes"
+        desc: "Maximum size of REST request headers (kilobytes)"
         name: "rest-max-headers-size" .}: Natural
 
       keymanagerEnabled* {.
