496d0266ec580b98c23352935d565382df9a43af
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2022-02-14 21:57:06 +0100

    bump nim-metrics (#3392)

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -1115,7 +1115,6 @@ proc onSecond(node: BeaconNode) =
     node.handleMissingBlocks()
 
   # Nim GC metrics (for the main thread)
-  updateSystemMetrics()
   updateThreadMetrics()
 
 proc runOnSecondLoop(node: BeaconNode) {.async.} =
