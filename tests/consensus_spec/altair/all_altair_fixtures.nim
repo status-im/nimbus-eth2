5a3ba5d96831599338b30239674285f89cb5954a
Etan Kissling <etan@status.im>
2022-03-08 13:21:56 +0100

    update to pre-release light client sync protocol (#3465)
    This adopts the spec sections of the pre-release proposal of the libp2p
    based light client sync protocol, and also adds a test runner for the
    new accompanying tests. While the release version of the light client
    sync protocol contains conflicting definitions, it is currently unused,
    and the code specific to the pre-release proposal is marked as such.
    See https://github.com/ethereum/consensus-specs/pull/2802

--- tests/consensus_spec/altair/all_altair_fixtures.nim
+++ tests/consensus_spec/altair/all_altair_fixtures.nim
@@ -18,5 +18,6 @@ import
   ./test_fixture_sanity_slots,
   ./test_fixture_ssz_consensus_objects,
   ./test_fixture_state_transition_epoch,
+  ./test_fixture_sync_protocol_light_client_sync,
   ./test_fixture_sync_protocol,
   ./test_fixture_transition
