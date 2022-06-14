status-im/nimbus-eth2

commit c20309ba394abb7ee57ad9a16efa02ff3edbb3b5

don't use slow-to-compress bzip2 with larger Jenkins artifacts

Jenkinsfile
@@ -74,7 +74,7 @@ def runStages(nodeDir) {
 			// archive testnet logs
 			sh """#!/bin/bash
 			for D in local_testnet0_data local_testnet1_data resttest0_data; do
-				[[ -d "\$D" ]] && tar cjf "\${D}-\${NODE_NAME}.tar.bz2" "\${D}"/*.txt || true
+				[[ -d "\$D" ]] && tar czf "\${D}-\${NODE_NAME}.tar.gz "\${D}"/*.txt || true
 			done
 			"""
 			try {

commit fe74b0f14f13dc0eac5c3efe0c4f701ac65acac5

fix CI archive step
c20309ba394abb7ee57ad9a16efa02ff3edbb3b5 accidentally deleted a `"`.
This is now re-added to avoid errors during artifact CI archiving step.

Jenkinsfile
@@ -74,7 +74,7 @@ def runStages(nodeDir) {
 			// archive testnet logs
 			sh """#!/bin/bash
 			for D in local_testnet0_data local_testnet1_data resttest0_data; do
-				[[ -d "\$D" ]] && tar czf "\${D}-\${NODE_NAME}.tar.gz "\${D}"/*.txt || true
+				[[ -d "\$D" ]] && tar czf "\${D}-\${NODE_NAME}.tar.gz" "\${D}"/*.txt || true
 			done
 			"""
 			try {