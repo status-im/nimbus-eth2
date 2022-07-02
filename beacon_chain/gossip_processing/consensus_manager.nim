66acf797bcfd0288c38cb6418b69483f4c469f1e
Dustin Brody <tersec@users.noreply.github.com>
2022-07-02 20:22:54 +0000

    fix is_optimistic_candidate_block per spec; don't crash with nil future

--- beacon_chain/gossip_processing/consensus_manager.nim
+++ beacon_chain/gossip_processing/consensus_manager.nim
@@ -132,9 +132,14 @@ proc updateExecutionClientHead(self: ref ConsensusManager, newHead: BlockRef)
     return
 
   # Can't use dag.head here because it hasn't been updated yet
+  let executionHeadRoot = self.dag.loadExecutionBlockRoot(newHead)
+
+  if executionHeadRoot.isZero:
+    # Nothing to call forkchoiceUpdated with; also effectively VALID.
+    self.dag.markBlockVerified(self.quarantine[], newHead.root)
+    return
+
   let
-    consensusHeadRoot = newHead.root
-    executionHeadRoot = self.dag.loadExecutionBlockRoot(newHead)
     executionFinalizedRoot =
       self.dag.loadExecutionBlockRoot(self.dag.finalizedHead.blck)
 
@@ -143,12 +148,12 @@ proc updateExecutionClientHead(self: ref ConsensusManager, newHead: BlockRef)
 
   case payloadExecutionStatus
   of PayloadExecutionStatus.valid:
-    self.dag.markBlockVerified(self.quarantine[], consensusHeadRoot)
+    self.dag.markBlockVerified(self.quarantine[], newHead.root)
   of PayloadExecutionStatus.invalid, PayloadExecutionStatus.invalid_block_hash:
-    self.dag.markBlockInvalid(consensusHeadRoot)
-    self.quarantine[].addUnviable(consensusHeadRoot)
+    self.dag.markBlockInvalid(newHead.root)
+    self.quarantine[].addUnviable(newHead.root)
   of PayloadExecutionStatus.accepted, PayloadExecutionStatus.syncing:
-    self.dag.optimisticRoots.incl consensusHeadRoot
+    self.dag.optimisticRoots.incl newHead.root
 
 proc updateHead*(self: var ConsensusManager, wallSlot: Slot) =
   ## Trigger fork choice and update the DAG with the new head block
@@ -160,6 +165,11 @@ proc updateHead*(self: var ConsensusManager, wallSlot: Slot) =
       wallSlot.start_beacon_time).valueOr:
     warn "Head selection failed, using previous head",
       head = shortLog(self.dag.head), wallSlot
+    return
+
+  if self.dag.loadExecutionBlockRoot(newHead).isZero:
+    # Nothing to call forkchoiceUpdated with; also effectively VALID.
+    self.dag.markBlockVerified(self.quarantine[], newHead.root)
     return
 
   # Store the new head in the chain DAG - this may cause epochs to be
