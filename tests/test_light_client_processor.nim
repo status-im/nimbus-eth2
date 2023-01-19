d307a2531b2fbeb88627593a70c56fb9b4b0a2b8
Etan Kissling <etan@status.im>
2023-01-19 16:01:17 +0100

    fix

--- tests/test_light_client_processor.nim
+++ tests/test_light_client_processor.nim
@@ -152,14 +152,16 @@ suite "Light client processor" & preset():
 
       # Reduce stack size by making this a `proc`
       proc applyPeriodWithoutSupermajority(
-          period: SyncCommitteePeriod, update: ref ForkedLightClientUpdate) =
+          period: SyncCommitteePeriod,
+          update: ref ForkedLightClientUpdate,
+          isFirstRun: bool) =
         res = processor[].storeObject(
           MsgSource.gossip, getBeaconTime(), update[])
         check update[].kind <= store[].kind
         if finalizationMode == LightClientFinalizationMode.Optimistic or
             period == lastPeriodWithSupermajority + 1:
           if finalizationMode == LightClientFinalizationMode.Optimistic or
-              i == 0:
+              isFirstRun:
             withForkyStore(store[]):
               when lcDataFork > LightClientDataFork.None:
                 let upgraded = newClone(
@@ -276,7 +278,7 @@ suite "Light client processor" & preset():
             setTimeToSlot(forkyUpdate.signature_slot)
 
         for i in 0 ..< 2:
-          applyPeriodWithoutSupermajority(period, update)
+          applyPeriodWithoutSupermajority(period, update, isFirstRun = (i == 0))
 
         if finalizationMode == LightClientFinalizationMode.Optimistic:
           withForkyStore(store[]):
