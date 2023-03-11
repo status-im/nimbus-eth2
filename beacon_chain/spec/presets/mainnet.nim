7a899b3538ba0855bfd6dd421217897e196ecd69
Etan Kissling <etan@status.im>
2023-03-11 12:53:15 +0100

    rename `eip4844_preset.nim` > `deneb_preset.nim`

--- beacon_chain/spec/presets/mainnet.nim
+++ beacon_chain/spec/presets/mainnet.nim
@@ -1,4 +1,15 @@
-import
-  ./mainnet/[altair_preset, bellatrix_preset, capella_preset, eip4844_preset, phase0_preset]
+# beacon_chain
+# Copyright (c) 2019-2023 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-export altair_preset, bellatrix_preset, capella_preset, eip4844_preset, phase0_preset
+import
+  ./mainnet/[
+    phase0_preset, altair_preset, bellatrix_preset, capella_preset,
+    deneb_preset]
+
+export
+  phase0_preset, altair_preset, bellatrix_preset, capella_preset,
+  deneb_preset
