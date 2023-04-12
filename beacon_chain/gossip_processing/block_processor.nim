5dfbf6db577ada581284830bc749653d60c12a62
tersec <tersec@users.noreply.github.com>
2023-04-12 15:55:48 +0000

    include small dedup in block processor to handle blockByRoot blocks

--- beacon_chain/gossip_processing/block_processor.nim
+++ beacon_chain/gossip_processing/block_processor.nim
@@ -13,6 +13,7 @@ import
   ../spec/signatures_batch,
   ../sszdump
 
+from std/deques import Deque, addLast, contains, initDeque, items, len, shrink
 from ../consensus_object_pools/consensus_manager import
   ConsensusManager, checkNextProposer, optimisticExecutionPayloadHash,
   runProposalForkchoiceUpdated, shouldSyncOptimistically, updateHead,
@@ -42,6 +43,10 @@ const
     ## syncing the finalized part of the chain
   PAYLOAD_PRE_WALL_SLOTS = SLOTS_PER_EPOCH * 2
     ## Number of slots from wall time that we start processing every payload
+  MAX_DEDUP_QUEUE_LEN = 16
+    ## Number of blocks, with FIFO discipline, against which to check queued
+    ## blocks before being processed to avoid spamming ELs. This should stay
+    ## small enough that even O(n) algorithms are reasonable.
 
 type
   BlobSidecars* = seq[ref BlobSidecar]
@@ -97,6 +102,9 @@ type
       ## The slot at which we sent a payload to the execution client the last
       ## time
 
+    dupBlckBuf: Deque[(Eth2Digest, ValidatorSig)]
+      # Small buffer to allow for filtering of duplicate blocks in block queue
+
   NewPayloadStatus {.pure.} = enum
     valid
     notValid
@@ -132,7 +140,9 @@ proc new*(T: type BlockProcessor,
     consensusManager: consensusManager,
     validatorMonitor: validatorMonitor,
     getBeaconTime: getBeaconTime,
-    verifier: BatchVerifier(rng: rng, taskpool: taskpool)
+    verifier: BatchVerifier(rng: rng, taskpool: taskpool),
+    dupBlckBuf: initDeque[(Eth2Digest, ValidatorSig)](
+      initialSize = MAX_DEDUP_QUEUE_LEN)
   )
 
 # Sync callbacks
@@ -206,7 +216,6 @@ proc storeBackfillBlock(
   # TODO: store blobs in db
 
   res
-
 
 from web3/engine_api_types import PayloadExecutionStatus, PayloadStatusV1
 from ../eth1/eth1_monitor import
@@ -606,6 +615,19 @@ proc addBlock*(
   except AsyncQueueFullError:
     raiseAssert "unbounded queue"
 
+# Dedup
+# ------------------------------------------------------------------------------
+
+func checkDuplicateBlocks(self: ref BlockProcessor, entry: BlockEntry): bool =
+  let key = (entry.blck.root, entry.blck.signature)
+  if self.dupBlckBuf.contains key:
+    return true
+  doAssert self.dupBlckBuf.len <= MAX_DEDUP_QUEUE_LEN
+  if self.dupBlckBuf.len >= MAX_DEDUP_QUEUE_LEN:
+    self.dupBlckBuf.shrink(fromFirst = 1)
+  self.dupBlckBuf.addLast key
+  false
+
 # Event Loop
 # ------------------------------------------------------------------------------
 
@@ -621,6 +643,9 @@ proc processBlock(
   if not afterGenesis:
     error "Processing block before genesis, clock turned back?"
     quit 1
+
+  if self.checkDuplicateBlocks(entry):
+    return
 
   let res = withBlck(entry.blck):
     await self.storeBlock(
