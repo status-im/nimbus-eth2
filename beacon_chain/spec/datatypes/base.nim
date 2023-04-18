f07b30fd02feb0450afc04a8587f4dcabec62d71
tersec <tersec@users.noreply.github.com>
2023-04-18 15:50:27 +0000

    use consensus specs v1.3.0 test vectors

--- beacon_chain/spec/datatypes/base.nim
+++ beacon_chain/spec/datatypes/base.nim
@@ -74,7 +74,7 @@ export
   tables, results, json_serialization, timer, sszTypes, beacon_time, crypto,
   digest, presets
 
-const SPEC_VERSION* = "1.3.0-rc.5"
+const SPEC_VERSION* = "1.3.0"
 ## Spec version we're aiming to be compatible with, right now
 
 const
