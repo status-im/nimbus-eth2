c6186df62223da183f645027e1d0459a0be5d697
Henri DF <henridf@gmail.com>
2023-05-11 10:43:11 +0200

    Add comment explaining why build with -f

--- scripts/compile_nim_program.sh
+++ scripts/compile_nim_program.sh
@@ -39,6 +39,12 @@ fi
 # parallel.
 # We can't use '--nimcache:...' here, because the same path is being used by
 # LTO on macOS, in "config.nims"
+# We have to use the `-f` flag here because running nim compile with
+# different -d defines (as we do when for mainnet/minimal in CI) does
+# not lead to a rebuild of changed deps - nim uses the cached
+# version. The specific instance here is `-d:FIELD_ELEMENTS_PER_BLOB`
+# that is used in the nim-kzg library and its dependency.
+# TODO find a way not to have to -f here.
 "$NIMC" c -f --compileOnly -o:build/${BINARY} "$@" -d:nimCachePathOverride=nimcache/release/${BINARY} "${SOURCE}"
 build/generate_makefile "nimcache/release/${BINARY}/${PROJECT_NAME}.json" "nimcache/release/${BINARY}/${BINARY}.makefile"
 # Don't swallow stderr, in case it's important.
