50730a7920866752613632c6c842b7044b685eed
Etan Kissling <etan@status.im>
2023-05-15 01:10:26 +0200

    lint

--- beacon_chain/rpc/rest_nimbus_api.nim
+++ beacon_chain/rpc/rest_nimbus_api.nim
@@ -1,4 +1,5 @@
-# Copyright (c) 2018-2021 Status Research & Development GmbH
+# beacon_chain
+# Copyright (c) 2018-2023 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
