10569ff2e92e7354acef71fee6343b13724187ee
tersec <tersec@users.noreply.github.com>
2023-05-25 07:55:00 +0000

    use v1.4.0-alpha.0 consensus spec test vectors (#4990)

--- tests/consensus_spec/test_fixture_fork_choice.nim
+++ tests/consensus_spec/test_fixture_fork_choice.nim
@@ -8,8 +8,6 @@
 {.used.}
 
 import
-  # Standard library
-  std/[json, sequtils, strutils, tables],
   # Status libraries
   stew/results, chronicles,
   eth/keys, taskpools,
@@ -27,6 +25,11 @@ import
   # Test
   ../testutil, ../testdbutil,
   ./fixtures_utils, ./os_ops
+
+from std/json import
+  JsonNode, getBool, getInt, getStr, hasKey, items, len, pairs, `$`, `[]`
+from std/sequtils import toSeq
+from std/strutils import contains
 
 # Test format described at https://github.com/ethereum/consensus-specs/tree/v1.3.0/tests/formats/fork_choice
 # Note that our implementation has been optimized with "ProtoArray"
@@ -419,6 +422,8 @@ template fcSuite(suiteName: static[string], testPathElem: static[string]) =
     for kind, path in walkDir(presetPath, relative = true, checkDir = true):
       let testsPath = presetPath/path/testPathElem
       if kind != pcDir or not os_ops.dirExists(testsPath):
+        continue
+      if testsPath.contains("/eip6110/") or testsPath.contains("\\eip6110\\"):
         continue
       let fork = forkForPathComponent(path).valueOr:
         raiseAssert "Unknown test fork: " & testsPath
