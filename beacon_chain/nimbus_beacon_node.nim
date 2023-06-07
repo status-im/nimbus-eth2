e8c6af0636f6a7e8eb0e6146e53dc8705948ed88
tersec <tersec@users.noreply.github.com>
2023-06-07 21:27:15 +0000

    add getNextWithdrawals Beacon API (#5021)

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -1418,6 +1418,7 @@ func connectedPeersCount(node: BeaconNode): int =
 
 proc installRestHandlers(restServer: RestServerRef, node: BeaconNode) =
   restServer.router.installBeaconApiHandlers(node)
+  restServer.router.installBuilderApiHandlers(node)
   restServer.router.installConfigApiHandlers(node)
   restServer.router.installDebugApiHandlers(node)
   restServer.router.installEventApiHandlers(node)
