d0f8b4e03d7401e017e1229e67576d33c3b66fea
Henri DF <henridf@gmail.com>
2023-06-28 10:47:33 +0200

    Implement local validator block proposals

--- beacon_chain/spec/forks.nim
+++ beacon_chain/spec/forks.nim
@@ -17,12 +17,12 @@ import
     block_id, eth2_merkleization, eth2_ssz_serialization,
     forks_light_client, presets],
   ./datatypes/[phase0, altair, bellatrix, capella, deneb],
-  ./mev/bellatrix_mev, ./mev/capella_mev
+  ./mev/bellatrix_mev, ./mev/capella_mev, ./mev/deneb_mev
 
 export
   extras, block_id, phase0, altair, bellatrix, capella, deneb,
   eth2_merkleization, eth2_ssz_serialization, forks_light_client,
-  presets, bellatrix_mev, capella_mev
+  presets, bellatrix_mev, capella_mev, deneb_mev
 
 # This file contains helpers for dealing with forks - we have two ways we can
 # deal with forks:
@@ -851,7 +851,8 @@ template withStateAndBlck*(
 
 func toBeaconBlockHeader*(
     blck: SomeForkyBeaconBlock | bellatrix_mev.BlindedBeaconBlock |
-          capella_mev.BlindedBeaconBlock): BeaconBlockHeader =
+          capella_mev.BlindedBeaconBlock | deneb_mev.BlindedBeaconBlock):
+            BeaconBlockHeader =
   ## Reduce a given `BeaconBlock` to its `BeaconBlockHeader`.
   BeaconBlockHeader(
     slot: blck.slot,
---
+++ beacon_chain/spec/mev/rest_deneb_mev_calls.nim
@@ -0,0 +1,27 @@
+# Copyright (c) 2023 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+{.push raises: [].}
+
+import
+  chronos, presto/client,
+  ".."/eth2_apis/[rest_types, eth2_rest_serialization]
+
+export chronos, client, rest_types, eth2_rest_serialization
+
+proc getHeaderCapella*(slot: Slot,
+                       parent_hash: Eth2Digest,
+                       pubkey: ValidatorPubKey
+                      ): RestResponse[GetHeaderResponseCapella] {.
+     rest, endpoint: "/eth/v1/builder/header/{slot}/{parent_hash}/{pubkey}",
+     meth: MethodGet, connection: {Dedicated, Close}.}
+  ## https://github.com/ethereum/builder-specs/blob/v0.3.0/apis/builder/header.yaml
+
+proc submitBlindedBlock*(body: capella_mev.SignedBlindedBeaconBlock
+                        ): RestResponse[SubmitBlindedBlockResponseCapella] {.
+     rest, endpoint: "/eth/v1/builder/blinded_blocks",
+     meth: MethodPost, connection: {Dedicated, Close}.}
+  ## https://github.com/ethereum/builder-specs/blob/v0.3.0/apis/builder/blinded_blocks.yaml
