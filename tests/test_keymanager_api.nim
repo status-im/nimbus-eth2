a7efddfc8465196183b8958ed3219427e96dd2cf
Etan Kissling <etan@status.im>
2023-07-31 23:05:27 +0200

    different attempt to set log level

--- tests/test_keymanager_api.nim
+++ tests/test_keymanager_api.nim
@@ -10,7 +10,7 @@
 import
   std/[typetraits, os, options, json, sequtils, uri, algorithm],
   testutils/unittests, chronicles, stint, json_serialization, confutils,
-  chronos, blscurve, libp2p/crypto/crypto as lcrypto,
+  chronicles, chronos, blscurve, libp2p/crypto/crypto as lcrypto,
   stew/[byteutils, io2], stew/shims/net,
 
   ../beacon_chain/spec/[crypto, keystore, eth2_merkleization],
@@ -276,8 +276,10 @@ proc startBeaconNode(basePort: int) {.raises: [Defect, CatchableError].} =
   copyHalfValidators(nodeDataDir, true)
   addPreTestRemoteKeystores(nodeValidatorsDir)
 
+  for topicName in ["libp2p", "gossipsub"]:
+    setTopicState(topicName, Disabled)
+
   let runNodeConf = try: BeaconNodeConf.load(cmdLine = mapIt([
-    "--log-level=TRACE;DEBUG:libp2p,gossipsub",
     "--tcp-port=" & $(basePort + PortKind.PeerToPeer.ord),
     "--udp-port=" & $(basePort + PortKind.PeerToPeer.ord),
     "--discv5=off",
