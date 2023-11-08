8a7b1b4506896ed543a3b0bdcf0da832bc8a0ce9
Etan Kissling <etan@status.im>
2023-11-08 16:14:29 +0100

    bump `gnosis-chain-configs` to `b8ae3091439131949a994d638d730b5a5cb60f7a`
    - fix(ci): specify enr-cli version
    - Update deneb config vars

--- beacon_chain/spec/presets/gnosis/deneb_preset.nim
+++ beacon_chain/spec/presets/gnosis/deneb_preset.nim
@@ -6,13 +6,13 @@
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 # Gnosis preset - Deneb
-# https://github.com/gnosischain/configs/blob/b90374a1c63703db8235fcdb65aff2e909bc42b5/presets/gnosis/deneb.yaml
+# https://github.com/gnosischain/configs/blob/b8ae3091439131949a994d638d730b5a5cb60f7a/presets/gnosis/deneb.yaml
 const
   # `uint64(4096)`
   FIELD_ELEMENTS_PER_BLOB*: uint64 = 4096
   # `uint64(2**12)` (= 4096)
   MAX_BLOB_COMMITMENTS_PER_BLOCK*: uint64 = 4096
-  # `uint64(2**2)` (= 4)
-  MAX_BLOBS_PER_BLOCK*: uint64 = 4
+  # `uint64(6)`
+  MAX_BLOBS_PER_BLOCK*: uint64 = 6
   # `floorlog2(get_generalized_index(BeaconBlockBody, 'blob_kzg_commitments')) + 1 + ceillog2(MAX_BLOB_COMMITMENTS_PER_BLOCK)` = 4 + 1 + 12 = 17
   KZG_COMMITMENT_INCLUSION_PROOF_DEPTH* = 17
