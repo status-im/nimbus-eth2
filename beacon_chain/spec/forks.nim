e2cd7227ce2ac435e76eb436f07e74cee99e9aef
Etan Kissling <etan@status.im>
2023-12-08 09:58:15 +0900

    re-add comment for `PayloadAttributesV2` usage
    As followup from #5654, ensure that we still keep the comment around
    referring to the correct `forkchoiceUpdated` to use being driven by
    the fork schedule.

--- beacon_chain/spec/forks.nim
+++ beacon_chain/spec/forks.nim
@@ -533,9 +533,15 @@ template BlindedBlockContents*(
 
 template PayloadAttributes*(
     kind: static ConsensusFork): auto =
+  # This also determines what `engine_forkchoiceUpdated` version will be used.
   when kind >= ConsensusFork.Deneb:
     typedesc[PayloadAttributesV3]
   elif kind >= ConsensusFork.Capella:
+    # https://github.com/ethereum/execution-apis/blob/v1.0.0-beta.3/src/engine/shanghai.md#specification-1
+    # Consensus layer client MUST call this method instead of
+    # `engine_forkchoiceUpdatedV1` under any of the following conditions:
+    # `headBlockHash` references a block which `timestamp` is greater or
+    # equal to the Shanghai timestamp
     typedesc[PayloadAttributesV2]
   elif kind >= ConsensusFork.Bellatrix:
     typedesc[PayloadAttributesV1]
