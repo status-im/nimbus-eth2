7c9820ccbf23b45150d48840770561b3c7adba91
jangko <jangko128@gmail.com>
2023-12-12 17:56:43 +0700

    Fix libnimbus_lc import

--- beacon_chain/libnimbus_lc/libnimbus_lc.nim
+++ beacon_chain/libnimbus_lc/libnimbus_lc.nim
@@ -17,7 +17,7 @@ import
   eth/trie/[db, hexary],
   json_rpc/jsonmarshal,
   secp256k1,
-  web3/primitives,
+  web3/eth_api_types,
   ../el/el_manager,
   ../spec/eth2_apis/[eth2_rest_serialization, rest_light_client_calls],
   ../spec/[helpers, light_client_sync],
