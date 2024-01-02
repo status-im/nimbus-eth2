3d8b175d25e96a7c20a8f445d58c8af72bbc5248
tersec <tersec@users.noreply.github.com>
2024-01-02 04:51:08 +0000

    rm unused RPC signatures replaced by Plain versions

--- beacon_chain/spec/eth2_apis/rest_config_calls.nim
+++ beacon_chain/spec/eth2_apis/rest_config_calls.nim
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2023 Status Research & Development GmbH
+# Copyright (c) 2018-2024 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -11,10 +11,6 @@ import
   "."/[rest_types, eth2_rest_serialization]
 
 export chronos, client, rest_types, eth2_rest_serialization
-
-proc getForkSchedule*(): RestResponse[GetForkScheduleResponse] {.
-     rest, endpoint: "/eth/v1/config/fork_schedule", meth: MethodGet.}
-  ## https://ethereum.github.io/beacon-APIs/#/Config/getForkSchedule
 
 proc getForkSchedulePlain*(): RestPlainResponse {.
      rest, endpoint: "/eth/v1/config/fork_schedule", meth: MethodGet.}
--- beacon_chain/validator_client/fallback_service.nim
+++ beacon_chain/validator_client/fallback_service.nim
@@ -1,5 +1,5 @@
 # beacon_chain
-# Copyright (c) 2021-2023 Status Research & Development GmbH
+# Copyright (c) 2021-2024 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -376,7 +376,7 @@ proc checkOffsetStatus(node: BeaconNodeServerRef, offset: TimeOffset) =
 
 proc runTimeMonitor(service: FallbackServiceRef,
                     node: BeaconNodeServerRef) {.async.} =
-  const NimbusExtensionsLog = "Beacon node do not support nimbus extensions"
+  const NimbusExtensionsLog = "Beacon node does not support Nimbus extensions"
   let
     vc = service.client
     roles = AllBeaconNodeRoles
