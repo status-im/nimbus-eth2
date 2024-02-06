96df07dffaa5dadb4b39d48ef64609a470db809e
Etan Kissling <etan@status.im>
2024-02-06 15:44:48 +0100

    prevent accidentally hashing `BeaconState`/`BeaconBlock` in Deneb
    Extend protection against accidentally calling computationally expensive
    functions when a cache is available to Deneb, as done for earlier forks.

--- beacon_chain/spec/eth2_merkleization.nim
+++ beacon_chain/spec/eth2_merkleization.nim
@@ -18,6 +18,7 @@ from ./datatypes/phase0 import HashedBeaconState, SignedBeaconBlock
 from ./datatypes/altair import HashedBeaconState, SignedBeaconBlock
 from ./datatypes/bellatrix import HashedBeaconState, SignedBeaconBlock
 from ./datatypes/capella import HashedBeaconState, SignedBeaconBlock
+from ./datatypes/deneb import HashedBeaconState, SignedBeaconBlock
 
 export ssz_codec, merkleization, proofs
 
@@ -28,13 +29,17 @@ type
 # creating recursive module dependency through `forks`.
 func hash_tree_root*(
     x: phase0.HashedBeaconState | altair.HashedBeaconState |
-       bellatrix.HashedBeaconState | capella.HashedBeaconState) {.
+       bellatrix.HashedBeaconState | capella.HashedBeaconState |
+       deneb.HashedBeaconState) {.
   error: "HashedBeaconState should not be hashed".}
 
 func hash_tree_root*(
     x: phase0.SignedBeaconBlock | altair.SignedBeaconBlock |
-       bellatrix.SignedBeaconBlock | capella.SignedBeaconBlock) {.
+       bellatrix.SignedBeaconBlock | capella.SignedBeaconBlock |
+       deneb.SignedBeaconBlock) {.
   error: "SignedBeaconBlock should not be hashed".}
+
+static: doAssert ConsensusFork.high == ConsensusFork.Deneb
 
 func depositCountBytes*(x: uint64): array[32, byte] =
   doAssert(x <= 4294967295'u64)
