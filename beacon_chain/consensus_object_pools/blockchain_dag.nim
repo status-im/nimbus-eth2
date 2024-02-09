65e6f892deb5d9ff4399a0840a90788726024008
Etan Kissling <etan@status.im>
2024-02-09 11:20:36 +0100

    fix checkpoint block potentially not getting backfilled into DB (#5863)
    When using checkpoint sync, only checkpoint state is available, block is
    not downloaded and backfilled later.
    
    `dag.backfill` tracks latest filled `slot`, and latest `parent_root` for
    which no block has been synced yet.
    
    In checkpoint sync, this assumption is broken, because there, the start
    `dag.backfill.slot` is set based on checkpoint state slot, and the block
    is also not available.
    
    However, sync manager in backward mode also requests `dag.backfill.slot`
    and `block_clearance` then backfills the checkpoint block once it is
    synced. But, there is no guarantee that a peer ever sends us that block.
    They could send us all parent blocks and solely omit the checkpoint
    block itself. In that situation, we would accept the parent blocks and
    advance `dag.backfill`, and subsequently never request the checkpoint
    block again, resulting in gap inside blocks DB that is never filled.
    
    To mitigate that, the assumption is restored that `dag.backfill.slot`
    is the latest filled `slot`, and `dag.backfill.parent_root` is the next
    block that needs to be synced. By setting `slot` to `tail.slot + 1` and
    `parent_root` to `tail.root`, we put a fake summary into `dag.backfill`
    so that `block_clearance` only proceeds once checkpoint block exists.

--- beacon_chain/consensus_object_pools/blockchain_dag.nim
+++ beacon_chain/consensus_object_pools/blockchain_dag.nim
@@ -117,7 +117,7 @@ proc updateFrontfillBlocks*(dag: ChainDAGRef) =
   # era files match the chain
   if dag.db.db.readOnly: return # TODO abstraction leak - where to put this?
 
-  if dag.frontfillBlocks.len == 0 or dag.backfill.slot > 0:
+  if dag.frontfillBlocks.len == 0 or dag.backfill.slot > GENESIS_SLOT:
     return
 
   info "Writing frontfill index", slots = dag.frontfillBlocks.len
@@ -258,6 +258,9 @@ func getBlockIdAtSlot*(dag: ChainDAGRef, slot: Slot): Opt[BlockSlotId] =
 proc containsBlock(
     cfg: RuntimeConfig, db: BeaconChainDB, slot: Slot, root: Eth2Digest): bool =
   db.containsBlock(root, cfg.consensusForkAtEpoch(slot.epoch))
+
+proc containsBlock*(dag: ChainDAGRef, bid: BlockId): bool =
+  dag.cfg.containsBlock(dag.db, bid.slot, bid.root)
 
 proc getForkedBlock*(db: BeaconChainDB, root: Eth2Digest):
     Opt[ForkedTrustedSignedBeaconBlock] =
@@ -1227,9 +1230,14 @@ proc init*(T: type ChainDAGRef, cfg: RuntimeConfig, db: BeaconChainDB,
 
       db.getBeaconBlockSummary(backfillRoot).expect(
         "Backfill block must have a summary: " & $backfillRoot)
-    else:
+    elif dag.containsBlock(dag.tail):
       db.getBeaconBlockSummary(dag.tail.root).expect(
         "Tail block must have a summary: " & $dag.tail.root)
+    else:
+      # Checkpoint sync, checkpoint block unavailable
+      BeaconBlockSummary(
+        slot: dag.tail.slot + 1,
+        parent_root: dag.tail.root)
 
   dag.forkDigests = newClone ForkDigests.init(
     cfg, getStateField(dag.headState, genesis_validators_root))
@@ -1241,8 +1249,9 @@ proc init*(T: type ChainDAGRef, cfg: RuntimeConfig, db: BeaconChainDB,
 
   let finalizedTick = Moment.now()
 
-  if dag.backfill.slot > 0: # See if we can frontfill blocks from era files
-    dag.frontfillBlocks = newSeqOfCap[Eth2Digest](dag.backfill.slot.int)
+  if dag.backfill.slot > GENESIS_SLOT:  # Try frontfill from era files
+    let backfillSlot = dag.backfill.slot - 1
+    dag.frontfillBlocks = newSeqOfCap[Eth2Digest](backfillSlot.int)
 
     let
       historical_roots = getStateField(dag.headState, historical_roots).asSeq()
@@ -1255,7 +1264,7 @@ proc init*(T: type ChainDAGRef, cfg: RuntimeConfig, db: BeaconChainDB,
     # blocks from genesis to backfill, if possible.
     for bid in dag.era.getBlockIds(
         historical_roots, historical_summaries, Slot(0), Eth2Digest()):
-      if bid.slot >= dag.backfill.slot:
+      if bid.slot >= backfillSlot:
         # If we end up in here, we failed the root comparison just below in
         # an earlier iteration
         fatal "Era summaries don't lead up to backfill, database or era files corrupt?",
@@ -1304,7 +1313,7 @@ proc init*(T: type ChainDAGRef, cfg: RuntimeConfig, db: BeaconChainDB,
     head = shortLog(dag.head),
     finalizedHead = shortLog(dag.finalizedHead),
     tail = shortLog(dag.tail),
-    backfill = (dag.backfill.slot, shortLog(dag.backfill.parent_root)),
+    backfill = shortLog(dag.backfill),
 
     loadDur = loadTick - startTick,
     summariesDur = summariesTick - loadTick,
