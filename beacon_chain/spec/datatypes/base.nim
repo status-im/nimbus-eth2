7d6154d6c05ffc2c01c75b27efacfe04ac6c5ccd
tersec <tersec@users.noreply.github.com>
2024-02-16 02:16:54 +0000

    use 1.4.0-beta.7-hotfix consensus spec test vectors

--- beacon_chain/spec/datatypes/base.nim
+++ beacon_chain/spec/datatypes/base.nim
@@ -74,7 +74,7 @@ export
   tables, results, endians2, json_serialization, sszTypes, beacon_time, crypto,
   digest, presets
 
-const SPEC_VERSION* = "1.4.0-beta.6"
+const SPEC_VERSION* = "1.4.0-beta.7-hotfix"
 ## Spec version we're aiming to be compatible with, right now
 
 const
