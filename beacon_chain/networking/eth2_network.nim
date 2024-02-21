140872ff061904fde4dd92934be508bc70dd4cbe
Etan Kissling <etan@status.im>
2024-02-21 11:26:13 +0100

    Revert "update imports as `stew/shims/net` is no longer exported by json-ser"
    This reverts commit f5374926139ea5778e575028ea8257cae4a3611a.

--- beacon_chain/networking/eth2_network.nim
+++ beacon_chain/networking/eth2_network.nim
@@ -14,7 +14,7 @@ import
   # Status libs
   results,
   stew/[leb128, endians2, byteutils, io2, bitops2],
-  stew/shims/[net, macros],
+  stew/shims/macros,
   snappy,
   json_serialization, json_serialization/std/[net, sets, options],
   chronos, chronos/ratelimit, chronicles, metrics,
