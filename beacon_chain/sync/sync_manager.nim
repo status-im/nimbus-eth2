d5110a0c99c677892d24cadc04665c4f2d3a664b
Etan Kissling <etan@status.im>
2024-03-22 01:22:14 +0100

    only request blobs if a sync response actually provided blocks
    During sync, we can skip the `blobSidecarsByRange` request when there
    are no blocks with `kzg_commitments` in the blocks data. Avoids running
    into throttling from peers during long periods of non-finality.

--- beacon_chain/sync/sync_manager.nim
+++ beacon_chain/sync/sync_manager.nim
@@ -408,6 +408,19 @@ proc syncStep[A, B](man: SyncManager[A, B], index: int, peer: A)
           request = req
     return
 
+  let shouldGetBlobs =
+    if not man.shouldGetBlobs(req.slot.epoch):
+      false
+    else:
+      var hasBlobs = false
+      for blck in blockData:
+        withBlck(blck[]):
+          when consensusFork >= ConsensusForkDeneb:
+            if forkyBlck.message.body.blob_kzg_commitments.len > 0:
+              hasBlobs = true
+              break
+      hasBlobs
+
   func combine(acc: seq[Slot], cur: Slot): seq[Slot] =
     var copy = acc
     if copy[copy.len-1] != cur:
@@ -415,7 +428,7 @@ proc syncStep[A, B](man: SyncManager[A, B], index: int, peer: A)
     copy
 
   let blobData =
-    if man.shouldGetBlobs(req.slot.epoch):
+    if shouldGetBlobs:
       let blobs = await man.getBlobSidecars(peer, req)
       if blobs.isErr():
         peer.updateScore(PeerScoreNoValues)
