c4a5bca629c8ceaaca8b45ad5a6d62cc30971c88
Etan Kissling <etan@status.im>
2024-03-24 06:03:51 +0100

    update block quarantine eviction order to FIFO (#6129)
    Use the same eviction policy for blocks as already the case for blobs.
    FIFO makes more sense, because it favors keeping ancestors of blocks
    which need to be applied to the DAG before their children get eligible.

--- beacon_chain/consensus_object_pools/blob_quarantine.nim
+++ beacon_chain/consensus_object_pools/blob_quarantine.nim
@@ -38,7 +38,7 @@ func shortLog*(x: seq[BlobFetchRecord]): string =
   "[" & x.mapIt(shortLog(it.block_root) & shortLog(it.indices)).join(", ") & "]"
 
 func put*(quarantine: var BlobQuarantine, blobSidecar: ref BlobSidecar) =
-  if quarantine.blobs.lenu64 > MaxBlobs:
+  if quarantine.blobs.lenu64 >= MaxBlobs:
     # FIFO if full. For example, sync manager and request manager can race to
     # put blobs in at the same time, so one gets blob insert -> block resolve
     # -> blob insert sequence, which leaves garbage blobs.
