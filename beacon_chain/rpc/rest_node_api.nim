099fd7de8a1dcf2ed7dfed300a9b6d1f1cce3386
cheatfate <eugene.kabanov@status.im>
2024-03-26 16:46:18 +0200

    Initial commit.

--- beacon_chain/rpc/rest_node_api.nim
+++ beacon_chain/rpc/rest_node_api.nim
@@ -5,8 +5,9 @@
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 import
-  stew/[byteutils, results],
+  stew/[byteutils, results, endians2],
   chronicles,
+  chronos, chronos/transports/[osnet, ipnet],
   eth/p2p/discoveryv5/enr,
   libp2p/[multiaddress, multicodec, peerstore],
   ../version, ../beacon_node, ../sync/sync_manager,
@@ -104,35 +105,161 @@ proc getLastSeenAddress(node: BeaconNode, id: PeerId): string =
     $addrs[len(addrs) - 1]
   else:
     ""
-proc getDiscoveryAddresses(node: BeaconNode): Option[seq[string]] =
+proc getDiscoveryAddresses(node: BeaconNode): Opt[seq[string]] =
   let restr = node.network.enrRecord().toTypedRecord()
   if restr.isErr():
-    return none[seq[string]]()
+    return Opt.none(seq[string])
   let respa = restr.get().toPeerAddr(udpProtocol)
   if respa.isErr():
-    return none[seq[string]]()
+    return Opt.none(seq[string])
   let pa = respa.get()
   let mpa = MultiAddress.init(multiCodec("p2p"), pa.peerId)
   if mpa.isErr():
-    return none[seq[string]]()
+    return Opt.none(seq[string])
   var addresses = newSeqOfCap[string](len(pa.addrs))
   for item in pa.addrs:
     let resa = concat(item, mpa.get())
     if resa.isOk():
       addresses.add($(resa.get()))
-  return some(addresses)
+  return Opt.some(addresses)
 
-proc getP2PAddresses(node: BeaconNode): Option[seq[string]] =
-  let pinfo = node.network.switch.peerInfo
-  let mpa = MultiAddress.init(multiCodec("p2p"), pinfo.peerId)
-  if mpa.isErr():
-    return none[seq[string]]()
-  var addresses = newSeqOfCap[string](len(pinfo.addrs))
-  for item in pinfo.addrs:
-    let resa = concat(item, mpa.get())
-    if resa.isOk():
-      addresses.add($(resa.get()))
-  return some(addresses)
+proc getProtocolArgument(ma: MultiAddress,
+                         codec: MultiCodec): MaResult[seq[byte]] =
+  var buffer: seq[byte]
+  for item in ma:
+    let
+      ritem = ? item
+      code = ? ritem.protoCode()
+    if code == codec:
+      let arg = ? ritem.protoArgument(buffer)
+      if arg > 0:
+        return ok(buffer)
+  err("Multiaddress codec has not been found")
+
+proc checkInterface(iface: NetworkInterface): bool =
+  if (iface.ifType == IfSoftwareLoopback) or (iface.state == StatusUp):
+    true
+  else:
+    false
+
+proc init(t: typedesc[MultiAddress], family: AddressFamily,
+          iface: NetworkInterface,
+          port: Port, suffix: MultiAddress): MaResult[seq[MultiAddress]] =
+  var res: seq[MultiAddress]
+  if not(iface.checkInterface()):
+    return err("Unsupported interface")
+  for ifaddr in iface.addresses:
+    if ifaddr.host.family != family:
+      continue
+    var address = ifaddr.host
+    address.port = port
+    let
+      maddress = ? MultiAddress.init(address)
+      suffixed = ? maddress.concat(suffix)
+    res.add(suffixed)
+  ok(res)
+
+proc getP2PAddresses(node: BeaconNode): Opt[seq[string]] =
+  let
+    interfaces = getInterfaces()
+    pinfo = node.network.switch.peerInfo
+    suffix = MultiAddress.init(multiCodec("p2p"), pinfo.peerId).valueOr:
+      return Opt.none(seq[string])
+
+  # In this loop we expand bounded addresses like `0.0.0.0` and `::` to list of
+  # interface addresses.
+  var addresses = newSeq[string]()
+  for maddress in pinfo.addrs:
+    if TCP_IP.matchPartial(maddress):
+      let
+        portArg = maddress.getProtocolArgument(multiCodec("tcp")).valueOr:
+          continue
+        port =
+          block:
+            if len(portArg) != sizeof(uint16):
+              continue
+            Port(uint16.fromBytesLE(portArg))
+
+      if IP4.matchPartial(maddress):
+        let address4 = maddress.getProtocolArgument(multiCodec("ip4")).valueOr:
+          continue
+        if address4 != AnyAddress.address_v4:
+          let suffixed = maddress.concat(suffix).valueOr:
+            continue
+          addresses.add($suffixed)
+          continue
+        for iface in interfaces:
+          let iaddrs = MultiAddress.init(AddressFamily.IPv4, iface, port,
+                                         suffix).valueOr:
+            continue
+          for item in iaddrs:
+            addresses.add($item)
+      elif IP6.matchPartial(maddress):
+        let address6 = maddress.getProtocolArgument(multiCodec("ip6")).valueOr:
+          continue
+        if address6 != AnyAddress6.address_v6:
+          let suffixed = maddress.concat(suffix).valueOr:
+            continue
+          addresses.add($suffixed)
+          continue
+        for iface in interfaces:
+          let iaddrs = MultiAddress.init(AddressFamily.IPv6, iface, port,
+                                         suffix).valueOr:
+            continue
+          for item in iaddrs:
+            addresses.add($item)
+      else:
+        let suffixed = maddress.concat(suffix).valueOr:
+          continue
+        addresses.add($suffixed)
+    else:
+      let suffixed = maddress.concat(suffix).valueOr:
+        continue
+      addresses.add($suffixed)
+
+  # We assume that an address that appears more then once in list of observed
+  # addresses by libp2p identify protocol, can be used as address which can
+  # be dialled (out public address in case when we are behind NAT).
+  var addrCounts = initCountTable[TransportAddress]()
+  let observed = node.network.switch.peerStore.getMostObservedProtosAndPorts()
+  for maddress in observed:
+    if TCP_IP.matchPartial(maddress):
+      let
+        portArg = maddress.getProtocolArgument(multiCodec("tcp")).valueOr:
+          continue
+        port =
+          block:
+            if len(portArg) != sizeof(uint16):
+              continue
+            Port(uint16.fromBytesLE(portArg))
+
+      if IP4.matchPartial(maddress):
+        let
+          address4 = maddress.getProtocolArgument(multiCodec("ip4")).valueOr:
+            continue
+        var ta4 = TransportAddress(family: AddressFamily.IPv4, port: port)
+        ta4.address_v4[0 .. 3] = address4[0 .. 3]
+        addrCounts.inc(ta4)
+      elif IP6.matchPartial(maddress):
+        let
+          address6 = maddress.getProtocolArgument(multiCodec("ip6")).valueOr:
+            continue
+        var ta6 = TransportAddress(family: AddressFamily.IPv6, port: port)
+        ta6.address_v6[0 .. 15] = address6[0 .. 15]
+        addrCounts.inc(ta6)
+      else:
+        discard
+
+  for taddr, count in addrCounts.pairs():
+    if count > 1:
+      let
+        maddr = MultiAddress.init(taddr).valueOr:
+          continue
+        suffixed = maddr.concat(suffix).valueOr:
+          continue
+      addresses.add($suffixed)
+
+  return Opt.some(addresses)
 
 proc installNodeApiHandlers*(router: var RestRouter, node: BeaconNode) =
   let
@@ -141,21 +268,11 @@ proc installNodeApiHandlers*(router: var RestRouter, node: BeaconNode) =
 
   # https://ethereum.github.io/beacon-APIs/#/Node/getNetworkIdentity
   router.api2(MethodGet, "/eth/v1/node/identity") do () -> RestApiResponse:
-    let discoveryAddresses =
-      block:
-        let res = node.getDiscoveryAddresses()
-        if res.isSome():
-          res.get()
-        else:
-          newSeq[string](0)
-
-    let p2pAddresses =
-      block:
-        let res = node.getP2PAddresses()
-        if res.isSome():
-          res.get()
-        else:
-          newSeq[string]()
+    let
+      discoveryAddresses = node.getDiscoveryAddresses().valueOr:
+        default(seq[string])
+      p2pAddresses = node.getP2PAddresses().valueOr:
+        default(seq[string])
 
     RestApiResponse.jsonResponse(
       (
