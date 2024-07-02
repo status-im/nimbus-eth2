ad64b22485a81fed085f135385a16890a42afff8
Agnish Ghosh <iamagnix@gmail.com>
2024-07-02 22:26:31 +0530

    shortLog for dc

--- beacon_chain/spec/datatypes/eip7594.nim
+++ beacon_chain/spec/datatypes/eip7594.nim
@@ -78,7 +78,6 @@ type
 func shortLog*(v: DataColumnSidecar): auto =
   (
     index: v.index,
-    column: v.column,
     kzg_commitments: v.kzg_commitments.len,
     kzg_proofs: v.kzg_proofs.len,
     block_header: shortLog(v.signed_block_header.message),
