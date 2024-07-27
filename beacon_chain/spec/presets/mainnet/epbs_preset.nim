c8afe5893a5c2474c965892744c62747973ae847
tomi-3-0 <calebomoniyitomiwa@gmail.com>
2024-07-27 16:33:37 +0100

    Add Constants and Presets for epbsX

---
+++ beacon_chain/spec/presets/mainnet/epbs_preset.nim
@@ -0,0 +1,17 @@
+# beacon_chain
+# Copyright (c) 2023-2024 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+{.push raises: [].}
+
+# Mainnet preset - epbs
+# https://github.com/ethereum/consensus-specs/blob/1508f51b80df5488a515bfedf486f98435200e02/specs/_features/eipxxxx/beacon-chain.md#preset
+const
+  # `2**2` (= 4)
+  MAX_PAYLOAD_ATTESTATIONS*: uint8 = 4
+  # `uint64(2**9)` (= 512)
+  PTC_SIZE*: uint64 = 512
+ 
---
+++ beacon_chain/spec/ptc_status.nim
@@ -0,0 +1,23 @@
+# beacon_chain
+# Copyright (c) 2018-2024 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+{.push raises: [].}
+
+type
+  PTCStatus* = distinct uint64
+
+# PTCStatus represents a single payload status. These values represent the
+# possible votes that the Payload Timeliness Committee(PTC) can cast
+# in ePBS when attesting for an execution payload.
+# https://github.com/ethereum/consensus-specs/blob/1508f51b80df5488a515bfedf486f98435200e02/specs/_features/eipxxxx/beacon-chain.md#constants
+const
+  PAYLOAD_ABSENT* = PTCStatus(0)
+  PAYLOAD_PRESENT* = PTCStatus(1)
+  PAYLOAD_WITHHELD* = PTCStatus(2)
+  PAYLOAD_INVALID_STATUS* = PTCStatus(3)
+
+
