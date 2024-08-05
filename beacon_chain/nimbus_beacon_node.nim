5f5532be5be30d3b297c936ecdd4c1fe9b5ece9b
cheatfate <eugene.kabanov@status.im>
2024-08-06 00:37:15 +0300

    Fix assertion.

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -562,6 +562,7 @@ proc initFullNode(
   dag.setReorgCb(onChainReorg)
 
   node.dag = dag
+  node.list = clist
   node.blobQuarantine = blobQuarantine
   node.quarantine = quarantine
   node.attestationPool = attestationPool
