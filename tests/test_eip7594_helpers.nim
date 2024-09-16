b3c8c717765e6054346a864d61e399532c4cc242
Agnish Ghosh <80243668+agnxsh@users.noreply.github.com>
2024-09-16 16:49:34 +0530

    add: compute matrix (#6550)
    * add: compute matrix
    
    * rm unsused imports
    
    * rm unused imports 2
    
    * rm bin
    
    * rm unused line
    
    * improve doc
    
    * rid space on proc sig
    
    * review 1
    
    * added seeded PRNG
    
    * randomize blob count as well, with smaller seed
    
    * review 2

--- tests/test_eip7594_helpers.nim
+++ tests/test_eip7594_helpers.nim
@@ -9,8 +9,62 @@
 {.used.}
 
 import
+  std/random,
   unittest2,
-  ../beacon_chain/spec/[helpers, eip7594_helpers]
+  results,
+  kzg4844/[kzg_abi, kzg],
+  ./consensus_spec/[os_ops, fixtures_utils],
+  ../beacon_chain/spec/[helpers, eip7594_helpers],
+  ../beacon_chain/spec/datatypes/[eip7594, deneb]
+
+from std/strutils import rsplit
+
+block:
+  template sourceDir: string = currentSourcePath.rsplit(DirSep, 1)[0]
+  doAssert loadTrustedSetup(
+    sourceDir &
+      "/../vendor/nim-kzg4844/kzg4844/csources/src/trusted_setup.txt", 0).isOk
+
+# 114 is the MSB (most/max significant byte)
+# such that BLS modulus does not overflow
+const MAX_TOP_BYTE = 114
+
+proc createSampleKzgBlobs(n: int, seed: int): seq[KzgBlob] =
+  var
+    blobs: seq[KzgBlob] = @[]
+    # Initialize the PRNG with the given seed
+    rng = initRand(seed)
+  for blobIndex in 0..<n:
+    var blob: array[int(BYTES_PER_BLOB), byte]
+    # Fill the blob with random bytes using the seeded PRNG
+    for byteIndex in 0..<int(BYTES_PER_BLOB):
+      blob[byteIndex] = rng.rand(byte)
+    # Adjust bytes according to the given condition
+    for byteIndex in 0..<int(BYTES_PER_BLOB):
+      if blob[byteIndex] > MAX_TOP_BYTE and
+         byteIndex mod kzg_abi.BYTES_PER_FIELD_ELEMENT == 0:
+        blob[byteIndex] = MAX_TOP_BYTE
+    blobs.add(KzgBlob(bytes: blob))
+  blobs
+
+iterator chunks[T](lst: seq[T], n: int): seq[T] =
+  ## Iterator that yields N-sized chunks from the list.
+  for i in countup(0, len(lst) - 1, n):
+    yield lst[i..min(i + n - 1, len(lst) - 1)]
+
+suite "EIP-7594 Unit Tests":
+  test "EIP-7594: Compute Matrix":
+    proc testComputeExtendedMatrix() =
+      var
+        rng = initRand(126)
+        blob_count = rng.rand(1..(deneb.MAX_BLOB_COMMITMENTS_PER_BLOCK.int))
+      let
+        input_blobs = createSampleKzgBlobs(blob_count, rng.rand(int))
+        extended_matrix = compute_matrix(input_blobs)
+      doAssert extended_matrix.get.len == kzg_abi.CELLS_PER_EXT_BLOB * blob_count
+      for row in chunks(extended_matrix.get, kzg_abi.CELLS_PER_EXT_BLOB):
+        doAssert len(row) == kzg_abi.CELLS_PER_EXT_BLOB
+    testComputeExtendedMatrix()
 
 suite "EIP-7594 Sampling Tests":
   test "EIP7594: Extended Sample Count":
@@ -44,3 +98,5 @@ suite "EIP-7594 Sampling Tests":
         check: get_extended_sample_count(
             samplesPerSlot, allowed_failures) == extendedSampleCount
     testExtendedSampleCount()
+
+doAssert freeTrustedSetup().isOk
