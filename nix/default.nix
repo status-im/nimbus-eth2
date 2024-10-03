61c5191c6c630633b96f5b420f6e643900ac4ff2
tersec <tersec@users.noreply.github.com>
2024-10-03 11:56:47 +0000

    maybe work around permissions issue

--- nix/default.nix
+++ nix/default.nix
@@ -59,6 +59,7 @@ in stdenv.mkDerivation rec {
     pushd vendor/nimbus-build-system/vendor/Nim
     mkdir dist
     cp -r ${callPackage ./nimble.nix {}}    dist/nimble
+    chmod 777 -R dist/nimble
     mkdir -p dist/nimble/dist
     cp -r ${callPackage ./sat.nix {}}       dist/nimble/dist
     cp -r ${callPackage ./checksums.nix {}} dist/checksums
