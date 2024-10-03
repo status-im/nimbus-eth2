92905bd7eec2477b60337aeb897857b51cc0368c
tersec <tersec@users.noreply.github.com>
2024-10-03 11:00:54 +0000

    koch puts sat in the nimble directory

--- nix/default.nix
+++ nix/default.nix
@@ -58,9 +58,9 @@ in stdenv.mkDerivation rec {
   preBuild = ''
     pushd vendor/nimbus-build-system/vendor/Nim
     mkdir dist
+    cp -r ${callPackage ./sat.nix {}}       dist/nimble
     cp -r ${callPackage ./nimble.nix {}}    dist/nimble
     cp -r ${callPackage ./checksums.nix {}} dist/checksums
-    cp -r ${callPackage ./sat.nix {}}       dist/sat
     cp -r ${callPackage ./csources.nix {}}  csources_v2
     chmod 777 -R dist/nimble csources_v2
     popd
