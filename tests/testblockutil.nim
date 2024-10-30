bb8ba8475628ab9d17114ac02fed3522524b2765
Agnish Ghosh <iamagnix@gmail.com>
2024-10-31 02:44:01 +0530

    attempt to fix ci

--- tests/testblockutil.nim
+++ tests/testblockutil.nim
@@ -217,8 +217,9 @@ proc addTestBlock*(
         deposit_count: forkyState.data.eth1_deposit_index + deposits.lenu64,
         block_hash: eth1_data.block_hash),
       graffiti,
-      when consensusFork == ConsensusFork.Electra or
-           consensusFork == ConsensusFork.Fulu:
+      when consensusFork == ConsensusFork.Electra:
+        electraAttestations
+      elif consensusFork == ConsensusFork.Fulu:
         electraAttestations
       elif consensusFork == ConsensusFork.Fulu:
         electraAttestations
