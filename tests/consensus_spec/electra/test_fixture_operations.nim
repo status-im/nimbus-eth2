49b06d1a90b44fa154687109f871e11e5dfde0d6
tersec <tersec@users.noreply.github.com>
2025-01-09 05:03:23 +0000

    fix /eth/v1/beacon/rewards/block/{block_id} sync_aggregate value (#6829)
    * fix /eth/v1/beacon/rewards/block/{block_id} sync_aggregate value
    
    * adjust tests
    
    * copyright year linting

--- tests/consensus_spec/electra/test_fixture_operations.nim
+++ tests/consensus_spec/electra/test_fixture_operations.nim
@@ -1,5 +1,5 @@
 # beacon_chain
-# Copyright (c) 2024 Status Research & Development GmbH
+# Copyright (c) 2024-2025 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -245,9 +245,9 @@ suite baseDescription & "Sync Aggregate " & preset():
       preState: var electra.BeaconState, syncAggregate: SyncAggregate):
       Result[void, cstring] =
     var cache: StateCache
-    doAssert (? process_sync_aggregate(
+    discard ? process_sync_aggregate(
       preState, syncAggregate, get_total_active_balance(preState, cache),
-      {}, cache)) > 0.Gwei
+      {}, cache)
     ok()
 
   for path in walkTests(OpSyncAggregateDir):
