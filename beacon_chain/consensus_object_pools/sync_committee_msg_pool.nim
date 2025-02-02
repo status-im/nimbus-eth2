2471d2dd671c14b59ad5c41107b0372828ea8b76
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-02-02 18:03:06 +0100

    Update sync_committee_msg_pool.nim

--- beacon_chain/consensus_object_pools/sync_committee_msg_pool.nim
+++ beacon_chain/consensus_object_pools/sync_committee_msg_pool.nim
@@ -1,5 +1,5 @@
 # beacon_chain
-# Copyright (c) 2018-2024 Status Research & Development GmbH
+# Copyright (c) 2018-2025 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -23,7 +23,7 @@ logScope:
 const
   syncCommitteeMsgsRetentionSlots = 3
     ## How many slots to retain sync committee
-    ## messsages before discarding them.
+    ## messages before discarding them.
 
 type
   SyncCommitteeMsgKey = object
