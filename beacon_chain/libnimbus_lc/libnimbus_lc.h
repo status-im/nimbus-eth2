status-im/nimbus-eth2

commit 140416bd558d211f40d6340fff9a0419f614d4e0

Change ChainId type to UInt256, bump nim-eth, and nim-web3 (#7005)
* Change ChainId type to UInt256

* Remove TransitionConfigurationV1 usage

* Lint

beacon_chain/libnimbus_lc/libnimbus_lc.nim
@@ -1421,7 +1421,7 @@ type
     storageKeys: seq[Eth2Digest]
 
   ETHAuthorization = object
-    chainId: uint64
+    chainId: UInt256
     address: ExecutionAddress
     nonce: uint64
     authority: ExecutionAddress
@@ -1429,7 +1429,7 @@ type
 
   ETHTransaction = object
     hash: Eth2Digest
-    chainId: uint64
+    chainId: UInt256
     `from`: ExecutionAddress
     nonce: uint64
     maxPriorityFeePerGas: uint64
@@ -1539,8 +1539,8 @@ proc ETHTransactionsCreateFromJson(
 
     # Construct transaction
     static:
-      doAssert sizeof(uint64) == sizeof(ChainId)
-      doAssert sizeof(uint64) == sizeof(data.chainId.get)
+      doAssert sizeof(UInt256) == sizeof(ChainId)
+      doAssert sizeof(UInt256) == sizeof(data.chainId.get)
       doAssert sizeof(uint64) == sizeof(data.gas)
       doAssert sizeof(uint64) == sizeof(data.gasPrice)
       doAssert sizeof(uint64) == sizeof(data.maxPriorityFeePerGas.get)
@@ -1556,13 +1556,13 @@ proc ETHTransactionsCreateFromJson(
         return nil
     if data.authorizationList.isSome:
       for authorization in data.authorizationList.get:
-        static: doAssert sizeof(uint64) == sizeof(authorization.chainId)
+        static: doAssert sizeof(UInt256) == sizeof(authorization.chainId)
         if authorization.v > uint8.high:
           return nil
     let
       tx = eth_types.EthTransaction(
         txType: txType,
-        chainId: data.chainId.get(0.Quantity).ChainId,
+        chainId: data.chainId.get(0.u256),
         nonce: distinctBase(data.nonce),
         gasPrice: data.gasPrice.GasInt,
         maxPriorityFeePerGas:
@@ -1663,7 +1663,7 @@ proc ETHTransactionsCreateFromJson(
         authority = recoverSignerAddress(sig, auth.rlpHashForSigning).valueOr:
           return nil
       authorizationList.add ETHAuthorization(
-        chainId: distinctBase(auth.chainId),
+        chainId: auth.chainId,
         address: ExecutionAddress(data: auth.address.data),
         nonce: auth.nonce,
         authority: ExecutionAddress(data: authority),
@@ -1671,7 +1671,7 @@ proc ETHTransactionsCreateFromJson(
 
     txs.add ETHTransaction(
       hash: keccak256(rlpBytes),
-      chainId: distinctBase(tx.chainId),
+      chainId: tx.chainId,
       `from`: ExecutionAddress(data: fromAddress),
       nonce: tx.nonce,
       maxPriorityFeePerGas: tx.maxPriorityFeePerGas.uint64,
@@ -1755,7 +1755,7 @@ func ETHTransactionGetHash(
   addr transaction[].hash
 
 func ETHTransactionGetChainId(
-    transaction: ptr ETHTransaction): ptr uint64 {.exported.} =
+    transaction: ptr ETHTransaction): ptr UInt256 {.exported.} =
   ## Obtains the chain ID of a transaction.
   ##
   ## * The returned value is allocated in the given transaction.
@@ -2115,7 +2115,7 @@ func ETHAuthorizationListGet(
   addr authorizationList[][authorizationIndex.int]
 
 func ETHAuthorizationGetChainId(
-    authorization: ptr ETHAuthorization): ptr uint64 {.exported.} =
+    authorization: ptr ETHAuthorization): ptr UInt256 {.exported.} =
   ## Obtains the chain ID of an authorization tuple.
   ##
   ## * The returned value is allocated in the given authorization tuple.

research/fakeee.nim
@@ -1,5 +1,5 @@
 # beacon_chain
-# Copyright (c) 2022-2024 Status Research & Development GmbH
+# Copyright (c) 2022-2025 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -44,15 +44,6 @@ proc setupEngineAPI*(server: RpcServer) =
       code: engineApiUnknownPayload,
       msg: "Unknown payload"
     )
-
-  # https://github.com/ethereum/execution-apis/blob/v1.0.0-beta.3/src/engine/paris.md#engine_exchangetransitionconfigurationv1
-  server.rpc("engine_exchangeTransitionConfigurationV1") do(conf: TransitionConfigurationV1) -> TransitionConfigurationV1:
-    info "engine_exchangeTransitionConfigurationV1",
-      ttd = conf.terminalTotalDifficulty,
-      number = uint64(conf.terminalBlockNumber),
-      blockHash = conf.terminalBlockHash
-
-    return conf
 
   # https://github.com/ethereum/execution-apis/blob/v1.0.0-beta.3/src/engine/paris.md#engine_forkchoiceupdatedv1
   server.rpc("engine_forkchoiceUpdatedV1") do(

commit 5295e66f5a914c4049e6d40a02dd4ffec179891b

Fix incompatible uint64 chain ID remenants
Corrects issues introduced in #7005 that changed the Nim signatures of
exported API functions without adjusting the corresponding .h import
header. Also use specific `ChainId` data type where it makes sense,
and remove assertions that are no longer required.

beacon_chain/libnimbus_lc/libnimbus_lc.h
@@ -1207,6 +1207,11 @@ ETH_RESULT_USE_CHECK
 const ETHRoot *ETHTransactionGetHash(const ETHTransaction *transaction);
 
 /**
+ * Chain ID.
+ */
+typedef ETHUInt256 ETHChainId;
+
+/**
  * Obtains the chain ID of a transaction.
  *
  * - The returned value is allocated in the given transaction.
@@ -1218,7 +1223,7 @@ const ETHRoot *ETHTransactionGetHash(const ETHTransaction *transaction);
  * @return Chain ID.
  */
 ETH_RESULT_USE_CHECK
-const uint64_t *ETHTransactionGetChainId(const ETHTransaction *transaction);
+const ETHChainId *ETHTransactionGetChainId(const ETHTransaction *transaction);
 
 /**
  * Obtains the from address of a transaction.
@@ -1570,7 +1575,7 @@ const ETHAuthorization *ETHAuthorizationListGet(
  * @return Chain ID.
  */
 ETH_RESULT_USE_CHECK
-const uint64_t *ETHAuthorizationGetChainId(
+const ETHChainId *ETHAuthorizationGetChainId(
     const ETHAuthorization *authorization);
 
 /**

beacon_chain/libnimbus_lc/libnimbus_lc.nim
@@ -1421,7 +1421,7 @@ type
     storageKeys: seq[Eth2Digest]
 
   ETHAuthorization = object
-    chainId: UInt256
+    chainId: ChainId
     address: ExecutionAddress
     nonce: uint64
     authority: ExecutionAddress
@@ -1429,7 +1429,7 @@ type
 
   ETHTransaction = object
     hash: Eth2Digest
-    chainId: UInt256
+    chainId: ChainId
     `from`: ExecutionAddress
     nonce: uint64
     maxPriorityFeePerGas: uint64
@@ -1539,8 +1539,6 @@ proc ETHTransactionsCreateFromJson(
 
     # Construct transaction
     static:
-      doAssert sizeof(UInt256) == sizeof(ChainId)
-      doAssert sizeof(UInt256) == sizeof(data.chainId.get)
       doAssert sizeof(uint64) == sizeof(data.gas)
       doAssert sizeof(uint64) == sizeof(data.gasPrice)
       doAssert sizeof(uint64) == sizeof(data.maxPriorityFeePerGas.get)
@@ -1556,13 +1554,12 @@ proc ETHTransactionsCreateFromJson(
         return nil
     if data.authorizationList.isSome:
       for authorization in data.authorizationList.get:
-        static: doAssert sizeof(UInt256) == sizeof(authorization.chainId)
         if authorization.v > uint8.high:
           return nil
     let
       tx = eth_types.EthTransaction(
         txType: txType,
-        chainId: data.chainId.get(0.u256),
+        chainId: data.chainId.get(0.chainId),
         nonce: distinctBase(data.nonce),
         gasPrice: data.gasPrice.GasInt,
         maxPriorityFeePerGas:
@@ -1755,7 +1752,7 @@ func ETHTransactionGetHash(
   addr transaction[].hash
 
 func ETHTransactionGetChainId(
-    transaction: ptr ETHTransaction): ptr UInt256 {.exported.} =
+    transaction: ptr ETHTransaction): ptr ChainId {.exported.} =
   ## Obtains the chain ID of a transaction.
   ##
   ## * The returned value is allocated in the given transaction.
@@ -2115,7 +2112,7 @@ func ETHAuthorizationListGet(
   addr authorizationList[][authorizationIndex.int]
 
 func ETHAuthorizationGetChainId(
-    authorization: ptr ETHAuthorization): ptr UInt256 {.exported.} =
+    authorization: ptr ETHAuthorization): ptr ChainId {.exported.} =
   ## Obtains the chain ID of an authorization tuple.
   ##
   ## * The returned value is allocated in the given authorization tuple.

beacon_chain/libnimbus_lc/test_libnimbus_lc.c
@@ -1,6 +1,6 @@
 /**
  * beacon_chain
- * Copyright (c) 2023-2024 Status Research & Development GmbH
+ * Copyright (c) 2023-2025 Status Research & Development GmbH
  * Licensed and distributed under either of
  *   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
  *   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -464,7 +464,7 @@ int main(void)
         printHexString(transactionHash, sizeof *transactionHash);
         printf("\n");
 
-        const uint64_t *transactionChainId = ETHTransactionGetChainId(transaction);
+        const ETHChainId *transactionChainId = ETHTransactionGetChainId(transaction);
         printf("    - chain_id: ");
         printHexStringReversed(transactionChainId, sizeof *transactionChainId);
         printf("\n");
@@ -558,7 +558,7 @@ int main(void)
             printHexString(authority, sizeof *authority);
             printf("\n");
 
-            const uint64_t *chainId = ETHAuthorizationGetChainId(authorization);
+            const ETHChainId *chainId = ETHAuthorizationGetChainId(authorization);
             printf("            - chain_id: ");
             printHexStringReversed(chainId, sizeof *chainId);
             printf("\n");