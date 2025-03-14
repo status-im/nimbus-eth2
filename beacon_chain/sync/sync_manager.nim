bffd95ab38765f3e07648c6a7c764562378a8ae0
Agnish Ghosh <iamagnix@gmail.com>
2025-03-14 23:26:18 +0530

    select supernodes

--- beacon_chain/sync/sync_manager.nim
+++ beacon_chain/sync/sync_manager.nim
@@ -818,8 +818,11 @@ proc syncWorker[A, B](
       await man.notInSyncEvent.wait()
       man.workers[index].status = SyncWorkerStatus.WaitingPeer
       peer = await man.pool.acquire()
-      await man.syncStep(index, peer)
-      man.pool.release(peer)
+      if peer.lookupCgcFromPeer ==
+          NUMBER_OF_COLUMNS.uint64:
+        await man.syncStep(index, peer)
+        man.pool.release(peer)
+        peer = nil
       peer = nil
   finally:
     if not(isNil(peer)):
