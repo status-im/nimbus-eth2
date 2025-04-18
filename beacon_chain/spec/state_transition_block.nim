6776145ae06ac763673d353b7bca4469d1675893
nethoxa <135072738+nethoxa@users.noreply.github.com>
2025-04-18 10:12:10 +0200

    remove duplicated check

--- beacon_chain/spec/state_transition_block.nim
+++ beacon_chain/spec/state_transition_block.nim
@@ -418,9 +418,6 @@ proc check_voluntary_exit*(
     return err("Exit: not in validator set long enough")
 
   when typeof(state).kind >= ConsensusFork.Electra:
-    if voluntary_exit.validator_index >= state.validators.lenu64:
-      return err("Exit: validator index out of range")
-
     # Only exit validator if it has no pending withdrawals in the queue
     if not (get_pending_balance_to_withdraw(
         state, voluntary_exit.validator_index.ValidatorIndex) == 0.Gwei):
