25dbf7a28807c7543e2f447eada1f5fa4d3b33a4
nethoxa <135072738+nethoxa@users.noreply.github.com>
2025-04-22 14:11:17 +0200

    format process_withdrawal_request

--- beacon_chain/spec/state_transition_block.nim
+++ beacon_chain/spec/state_transition_block.nim
@@ -528,8 +528,7 @@ func process_withdrawal_request*(
   if is_full_exit_request:
     # Only exit validator if it has no pending withdrawals in the queue
     if pending_balance_to_withdraw == 0.Gwei:
-      discard initiate_validator_exit(cfg, state, index, default(ExitQueueInfo),
-          cache)
+      discard initiate_validator_exit(cfg, state, index, ExitQueueInfo(), cache)
     return
 
   let
