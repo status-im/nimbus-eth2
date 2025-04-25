ece009904c2c9ad3142114d9532fde087d16e40e
Katya Ryazantseva <sibkatya@gmail.com>
2025-04-25 12:20:54 +0200

    Add formatting

--- beacon_chain/gossip_processing/gossip_validation.nim
+++ beacon_chain/gossip_processing/gossip_validation.nim
@@ -44,14 +44,14 @@ declareCounter beacon_contributions_dropped_queue_full,
   "Number of sync committee contributions dropped because queue is full"
 
 declareCounter beacon_data_column_sidecar_processing_requests_total,
-"Number of data column sidecars submitted for processing"
+  "Number of data column sidecars submitted for processing"
 
 declareCounter  beacon_data_column_sidecar_processing_successes_total,
-"Number of data column sidecars verified for gossip"
+  "Number of data column sidecars verified for gossip"
 
 declareHistogram beacon_data_column_sidecar_gossip_verification_seconds,
-"Full runtime of data column sidecars gossip verification",
-buckets = [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, Inf]
+  "Full runtime of data column sidecars gossip verification",
+  buckets = [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, Inf]
 
 # This result is a little messy in that it returns Result.ok for
 # ValidationResult.Accept and an err for the others - this helps transport
