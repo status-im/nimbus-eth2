3f78ed2e2725daae8a5be42db6d6fa12f93a757f
narimiran <narimiran@disroot.org>
2025-05-07 08:58:22 +0200

    fix indentation

--- tests/test_block_processor.nim
+++ tests/test_block_processor.nim
@@ -69,7 +69,7 @@ suite "Block processor" & preset():
       processor = BlockProcessor.new(
         false, "", "", batchVerifier, consensusManager,
         validatorMonitor, blobQuarantine, getTimeFn)
-      discard processor.runQueueProcessingLoop()
+    discard processor.runQueueProcessingLoop()
 
   asyncTest "Reverse order block add & get" & preset():
     let
