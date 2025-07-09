0ff59817bac39745d229d3c4a62e96b8a7912aa5
tersec <tersec@users.noreply.github.com>
2025-07-09 11:39:53 +0000

    fix beacon block broadcast not using BPO forkdigests (#7285)

--- beacon_chain/spec/forks.nim
+++ beacon_chain/spec/forks.nim
@@ -354,7 +354,7 @@ type
     capella*:   ForkDigest
     deneb*:     ForkDigest
     electra*:   ForkDigest
-    fulu*:      ForkDigest
+    fuluInt:    ForkDigest
     bpos*:      seq[(Epoch, ConsensusFork, ForkDigest)]
 
 template kind*(
@@ -1127,9 +1127,9 @@ func consensusForkAtEpoch*(cfg: RuntimeConfig, epoch: Epoch): ConsensusFork =
 func consensusForkForDigest*(
     forkDigests: ForkDigests, forkDigest: ForkDigest): Opt[ConsensusFork] =
   static: doAssert high(ConsensusFork) == ConsensusFork.Fulu
-  if   forkDigest == forkDigests.fulu:
+  if   forkDigest == forkDigests.fuluInt:
     ok ConsensusFork.Fulu
-  elif   forkDigest == forkDigests.electra:
+  elif forkDigest == forkDigests.electra:
     ok ConsensusFork.Electra
   elif forkDigest == forkDigests.deneb:
     ok ConsensusFork.Deneb
@@ -1151,7 +1151,7 @@ func atConsensusFork*(
     forkDigests: ForkDigests, consensusFork: ConsensusFork): ForkDigest {.deprecated.} =
   case consensusFork
   of ConsensusFork.Fulu:
-    forkDigests.fulu
+    forkDigests.fuluInt
   of ConsensusFork.Electra:
     forkDigests.electra
   of ConsensusFork.Deneb:
@@ -1751,7 +1751,7 @@ func init*(T: type ForkDigests,
       compute_fork_digest(cfg.DENEB_FORK_VERSION, genesis_validators_root),
     electra:
       compute_fork_digest(cfg.ELECTRA_FORK_VERSION, genesis_validators_root),
-    fulu:
+    fuluInt:
       compute_fork_digest_fulu(
         cfg, genesis_validators_root, cfg.FULU_FORK_EPOCH),
     bpos: mapIt(
