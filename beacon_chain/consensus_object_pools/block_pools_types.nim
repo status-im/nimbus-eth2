6baa905b1267f25686c6986cc7095c62252fea36
Agnish Ghosh <iamagnix@gmail.com>
2025-07-31 15:32:53 +0530

    another fix

--- beacon_chain/consensus_object_pools/block_pools_types.nim
+++ beacon_chain/consensus_object_pools/block_pools_types.nim
@@ -408,7 +408,7 @@ func horizon*(dag: ChainDAGRef): Slot =
 
 func earliestAvailableSlot*(dag: ChainDAGRef): Slot =
   if dag.backfill.slot < dag.tail.slot and
-      dag.backfill.slot == GENESIS_SLOT:
+      dag.backfill.slot != GENESIS_SLOT:
     # When the BN is backfilling, backfill slot is the earliest
     # persisted block.
     dag.backfill.slot
