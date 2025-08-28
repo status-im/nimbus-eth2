a3f445facc977aff8c4b67a58fa25b910b772cfb
tersec <tersec@users.noreply.github.com>
2025-08-28 01:00:17 +0000

    properly fill in next_fork_epoch field in eth2 in ENR

--- tests/test_discovery.nim
+++ tests/test_discovery.nim
@@ -5,7 +5,7 @@
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-{.push raises: [].}
+{.push raises: [], gcsafe.}
 {.used.}
 
 import
@@ -235,7 +235,7 @@ suite "Discovery fork ID":
           forkId = ENRForkID(
             fork_digest: fork_digest,
             next_fork_version: next_fork_version,
-            next_fork_epoch: FAR_FUTURE_EPOCH)
+            next_fork_epoch: cfg.ALTAIR_FORK_EPOCH)
         for epoch in GENESIS_EPOCH ..< cfg.ALTAIR_FORK_EPOCH - 1:
           check cfg.getDiscoveryForkID(epoch, genesis_validators_root) == forkId
         forkId
@@ -265,7 +265,7 @@ suite "Discovery fork ID":
           forkId = ENRForkID(
             fork_digest: fork_digest,
             next_fork_version: next_fork_version,
-            next_fork_epoch: FAR_FUTURE_EPOCH)
+            next_fork_epoch: cfg.BELLATRIX_FORK_EPOCH)
         for epoch in cfg.ALTAIR_FORK_EPOCH ..< cfg.BELLATRIX_FORK_EPOCH - 1:
           check cfg.getDiscoveryForkID(epoch, genesis_validators_root) == forkId
         forkId
