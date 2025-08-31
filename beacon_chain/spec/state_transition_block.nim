0b41e97e002f67791b5f96fed30176dc9f18b608
tersec <tersec@users.noreply.github.com>
2025-08-31 04:37:28 +0000

    other one

--- beacon_chain/spec/state_transition_block.nim
+++ beacon_chain/spec/state_transition_block.nim
@@ -1158,16 +1158,16 @@ func process_withdrawals*(
     when payload is ForkyExecutionPayloadHeader:
       debugEcho "BAZ1: ", state.slot, "; ", hash_tree_root(expected_withdrawals), "; ", expected_withdrawals
   else:
-    let expected_withdrawals = List[capella.Withdrawal, MAX_WITHDRAWALS_PER_PAYLOAD](get_expected_withdrawals(state))
+    let expected_withdrawals = get_expected_withdrawals(state)
 
   when payload is ForkyExecutionPayloadHeader:
-    if not (payload.withdrawals_root == hash_tree_root(expected_withdrawals)):
+    if not (payload.withdrawals_root == hash_tree_root(List[capella.Withdrawal, MAX_WITHDRAWALS_PER_PAYLOAD].init(expected_withdrawals))):
       debugEcho "BAZ2: process_withdrawals: withdrawals_root does not match expected withdrawals: ", state.slot,
         "; payload_root = ", payload.withdrawals_root, "; htr(expected_withdrawals) = ", hash_tree_root(expected_withdrawals), "; ", expected_withdrawals
-    if false and not (payload.withdrawals_root == hash_tree_root(expected_withdrawals)):
+    if not (payload.withdrawals_root == hash_tree_root(List[capella.Withdrawal, MAX_WITHDRAWALS_PER_PAYLOAD].init(expected_withdrawals))):
       return err("process_withdrawals: withdrawals_root does not match expected withdrawals")
   else:
-    if payload.withdrawals.asSeq() != distinctBase(expected_withdrawals):
+    if payload.withdrawals.asSeq() != expected_withdrawals:
       return err("process_withdrawals: payload withdrawals don't match expected withdrawals")
 
   for withdrawal in expected_withdrawals:
