fc6fcf42b4c2de314b1280f48aa9b062fc45d1b1
Sam Shum <4080806+ahshum@users.noreply.github.com>
2025-10-23 08:43:20 +0100

    feat: gloas verify_data_column_sidecar

--- beacon_chain/spec/peerdas_helpers.nim
+++ beacon_chain/spec/peerdas_helpers.nim
@@ -268,8 +268,7 @@ proc assemble_data_column_sidecars*(
     sidecars
 
 # https://github.com/ethereum/consensus-specs/blob/v1.6.0-alpha.3/specs/fulu/p2p-interface.md#verify_data_column_sidecar
-func verify_data_column_sidecar*(sidecar: fulu.DataColumnSidecar |
-                                          gloas.DataColumnSidecar):
+func verify_data_column_sidecar*(_: RuntimeConfig, sidecar: fulu.DataColumnSidecar):
                                  Result[void, cstring] =
   ## Verify if the data column sidecar is valid.
 
@@ -278,6 +277,28 @@ func verify_data_column_sidecar*(sidecar: fulu.DataColumnSidecar |
 
   if sidecar.kzg_commitments.len == 0:
     return err("Data column contains zero blob")
+
+  if sidecar.column.len != sidecar.kzg_commitments.len or
+      sidecar.column.len != sidecar.kzg_proofs.len:
+    return err("Data column length must be equal to the number of commitments/proofs")
+
+  ok()
+
+# https://github.com/ethereum/consensus-specs/blob/v1.6.0-beta.1/specs/gloas/p2p-interface.md#modified-verify_data_column_sidecar
+func verify_data_column_sidecar*(cfg: RuntimeConfig, sidecar: gloas.DataColumnSidecar):
+                                 Result[void, cstring] =
+  ## Verify if the data column sidecar is valid.
+
+  if sidecar.index >= NUMBER_OF_COLUMNS:
+    return err("Data column sidecar index exceeds the NUMBER_OF_COLUMNS")
+
+  if sidecar.kzg_commitments.len == 0:
+    return err("Data column contains zero blob")
+
+  template epoch: untyped = sidecar.slot.epoch()
+  if sidecar.kzg_commitments.lenu64 >
+      cfg.get_blob_parameters(epoch).MAX_BLOBS_PER_BLOCK:
+    return err("Data column contains too many blobs")
 
   if sidecar.column.len != sidecar.kzg_commitments.len or
       sidecar.column.len != sidecar.kzg_proofs.len:
