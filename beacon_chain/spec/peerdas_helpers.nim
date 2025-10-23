02c1e23994c260b0b01320190719b4091925b2b1
Sam Shum <4080806+ahshum@users.noreply.github.com>
2025-10-23 08:52:25 +0100

    feat: update fulu verify_data_column_sidecar

--- beacon_chain/spec/peerdas_helpers.nim
+++ beacon_chain/spec/peerdas_helpers.nim
@@ -267,8 +267,8 @@ proc assemble_data_column_sidecars*(
 
     sidecars
 
-# https://github.com/ethereum/consensus-specs/blob/v1.6.0-alpha.3/specs/fulu/p2p-interface.md#verify_data_column_sidecar
-func verify_data_column_sidecar*(_: RuntimeConfig, sidecar: fulu.DataColumnSidecar):
+# https://github.com/ethereum/consensus-specs/blob/v1.6.0-beta.1/specs/fulu/p2p-interface.md#verify_data_column_sidecar
+func verify_data_column_sidecar*(cfg: RuntimeConfig, sidecar: fulu.DataColumnSidecar):
                                  Result[void, cstring] =
   ## Verify if the data column sidecar is valid.
 
@@ -277,6 +277,11 @@ func verify_data_column_sidecar*(_: RuntimeConfig, sidecar: fulu.DataColumnSidec
 
   if sidecar.kzg_commitments.len == 0:
     return err("Data column contains zero blob")
+
+  template epoch: untyped = sidecar.signed_block_header.message.slot.epoch()
+  if sidecar.kzg_commitments.lenu64 >
+      cfg.get_blob_parameters(epoch).MAX_BLOBS_PER_BLOCK:
+    return err("Data column contains too many blobs")
 
   if sidecar.column.len != sidecar.kzg_commitments.len or
       sidecar.column.len != sidecar.kzg_proofs.len:
