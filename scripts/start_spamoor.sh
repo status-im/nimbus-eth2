7c0932b75ee27f2d359968414e002491c78c80b0
cheatfate <eugene.kabanov@status.im>
2025-11-06 17:26:38 +0200

    Some more fixes to spamoor starter script.

--- scripts/start_spamoor.sh
+++ scripts/start_spamoor.sh
@@ -11,12 +11,14 @@ set -euo pipefail
 
 SCRIPTS_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
+source "${SCRIPTS_DIR}/geth_vars.sh"
+source "${SCRIPTS_DIR}/nimbus_el_vars.sh"
+source "${SCRIPTS_DIR}/spamoor_binaries.sh"
+
 # This is private key which corresponds to
 # 0xC9D2DaA6dd812745B5732aFd0b367dbcB2c38d88 address which was allocated in
 # `execution_genesis.json.template`.
 SPAMOOR_PRIVATE_KEY="65975debdc6b09ef5d40871d371861e98cbf1582ccf3f5466ed8ca8999f09388"
-
-source "${SCRIPTS_DIR}/spamoor_binaries.sh"
 
 log "Using ${SPAMOOR_BINARY}"
 
@@ -24,9 +26,11 @@ SPAMOOR_RPC_ENDPOINTS=""
 
 for GETH_NODE_IDX in $(seq 0 $GETH_LAST_NODE_IDX); do
   SPAMOOR_RPC_ENDPOINTS+="--rpchost http://127.0.0.1:${GETH_RPC_PORTS[GETH_NODE_IDX]} "
+done
 
 for NIMBUS_ETH1_NODE_IDX in $(seq 0 $NIMBUS_ETH1_LAST_NODE_IDX); do
   SPAMOOR_RPC_ENDPOINTS+="--rpchost http://127.0.0.1:${NIMBUS_ETH1_RPC_PORTS[NIMBUS_ETH1_NODE_IDX]} "
+done
 
 ${SPAMOOR_BINARY} \
   blobs \
