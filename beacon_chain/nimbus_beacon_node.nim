d9935b3b37ef0d25cf3173cd6103becdf2dc23f8
Agnish Ghosh <iamagnix@gmail.com>
2025-12-25 03:11:50 +0530

    remove commented code

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -1776,10 +1776,6 @@ proc reconstructDataColumns(node: BeaconNode, slot: Slot) =
       node.dag.cfg.NUMBER_OF_CUSTODY_GROUPS div 2:
     return
 
-  # # Currently, this logic is broken
-  # if true:
-  #   return
-
   logScope:
     slot = slot
 
