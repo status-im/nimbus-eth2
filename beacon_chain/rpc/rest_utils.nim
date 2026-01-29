edece56d97b3079ea235db7dda7401e6651fb24e
Jacek Sieka <jacek@status.im>
2026-01-29 12:15:13 +0100

    lint

--- beacon_chain/rpc/rest_utils.nim
+++ beacon_chain/rpc/rest_utils.nim
@@ -1,5 +1,5 @@
 # beacon_chain
-# Copyright (c) 2022-2024 Status Research & Development GmbH
+# Copyright (c) 2022-2026 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -14,7 +14,7 @@ import std/macros,
        ../validators/beacon_validators,
        ../consensus_object_pools/blockchain_dag,
        ../beacon_node,
-       "."/[rest_constants, state_ttl_cache]
+       ./[rest_constants, state_ttl_cache]
 
 export
   results, eth2_rest_serialization, blockchain_dag, rest_types,
