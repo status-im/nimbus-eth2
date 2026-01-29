550c1b8fcb1e474f50f647df95afea308e07aae4
Sam Shum <4080806+ahshum@users.noreply.github.com>
2026-01-29 12:07:45 +0000

    chore: clean up and comment

--- beacon_chain/gossip_processing/block_processor.nim
+++ beacon_chain/gossip_processing/block_processor.nim
@@ -344,7 +344,6 @@ proc newExecutionPayload*(
 
   Opt.some payloadStatus
 
-debugGloasComment("check calls to this, mostly LC")
 proc newExecutionPayload*(
     elManager: ELManager,
     blck: SomeForkyBeaconBlock
@@ -959,6 +958,7 @@ proc storePayload(
   # The execution payload has added to the clearance state successfully, so try
   # adding to the current state.
   debugGloasComment("should be decided by Fork Choice")
+  # Temporary call to updateHeadExecutionPayload() without import
   blockchain_dag.updateHeadExecutionPayload(dag, blck, signedEnvelope)
 
   # Store sidecars into db.
