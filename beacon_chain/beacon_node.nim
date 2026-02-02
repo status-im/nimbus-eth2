84b162b93123c0c0691c64b8608bad542ab71948
Agnish Ghosh <iamagnix@gmail.com>
2026-02-02 21:50:56 +0530

    add init

--- beacon_chain/beacon_node.nim
+++ beacon_chain/beacon_node.nim
@@ -189,6 +189,7 @@ func init*(T: type EventBus): T =
     headQueue: newAsyncEventQueue[HeadChangeInfoObject](),
     blocksQueue: newAsyncEventQueue[EventBeaconBlockObject](),
     blockGossipQueue: newAsyncEventQueue[EventBeaconBlockGossipObject](),
+    blockGossipPeerQueue: newAsyncEventQueue[EventBeaconBlockGossipPeerObject](),
     phase0AttestQueue: newAsyncEventQueue[phase0.Attestation](),
     singleAttestQueue: newAsyncEventQueue[SingleAttestation](),
     exitQueue: newAsyncEventQueue[SignedVoluntaryExit](),
