08b4cdda8a830fdae95e2c368b63c82b23431c56
Sam Shum <4080806+ahshum@users.noreply.github.com>
2026-02-18 17:06:57 +0000

    fix: fork range

--- beacon_chain/gossip_processing/block_processor.nim
+++ beacon_chain/gossip_processing/block_processor.nim
@@ -280,7 +280,7 @@ proc storeBackfillBlock(
 
   const consensusFork = typeof(signedBlock).kind
 
-  when consensusFork <= ConsensusFork.Fulu:
+  when consensusFork in ConsensusFork.Deneb .. ConsensusFork.Fulu:
     ?verifySidecars(signedBlock, noEnvelope, sidecarsOpt)
 
   let res = self.consensusManager.dag.addBackfillBlock(signedBlock)
@@ -694,7 +694,7 @@ proc storeBlock(
 
   let newPayloadTick = Moment.now()
 
-  when consensusFork <= ConsensusFork.Fulu:
+  when consensusFork in ConsensusFork.Deneb .. ConsensusFork.Fulu:
     ?verifySidecars(signedBlock, noEnvelope, sidecarsOpt)
 
   let blck =
@@ -713,7 +713,7 @@ proc storeBlock(
   self[].lastPayload = signedBlock.message.slot
 
   # write blobs now that block has been written.
-  when consensusFork <= ConsensusFork.Fulu:
+  when consensusFork in ConsensusFork.Deneb .. ConsensusFork.Fulu:
     self[].storeSidecars(sidecarsOpt)
 
   let addHeadBlockTick = Moment.now()
