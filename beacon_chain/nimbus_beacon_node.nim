e0f0e39cc9720e40981cd17030d97324228ef45f
Agnish Ghosh <iamagnix@gmail.com>
2026-02-24 15:30:34 +0530

    not

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -1777,7 +1777,7 @@ proc reconstructDataColumns(node: BeaconNode, slot: Slot) =
     return
 
   # Currently, this logic is broken
-  if node.config.debugEnableReconstruction:
+  if not node.config.debugEnableReconstruction:
     return
 
   logScope:
