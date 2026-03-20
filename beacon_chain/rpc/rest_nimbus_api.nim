7ca8968c99a8839cd6a5ce7608e87c27774a9c30
cheatfate <eugene.kabanov@status.im>
2026-03-20 20:25:06 +0200

    Add `summaryMap` field to queues response. Add /nimbus/v1/debug/sync/roots REST endpoint.

--- beacon_chain/rpc/rest_nimbus_api.nim
+++ beacon_chain/rpc/rest_nimbus_api.nim
@@ -668,3 +668,8 @@ proc installNimbusApiHandlers*(router: var RestRouter, node: BeaconNode) =
         node.syncOverseer.bsqueue.debugJsonDump() &
     "}}"
     RestApiResponse.response(res, Http200, "application/json")
+
+  router.api2(MethodGet, "/nimbus/v1/debug/sync/roots") do (
+    ) -> RestApiResponse:
+    let res = "{\"data\": " & node.syncOverseer.debugRootSyncJsonDump() & "}"
+    RestApiResponse.response(res, Http200, "application/json")
