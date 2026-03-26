0825fdb8a7ba42b897ced5676648728abc664632
Caleb <calebomoniyitomiwa@gmail.com>
2026-03-26 11:23:54 +0000

    update constants

--- beacon_chain/fork_choice/fork_choice_types.nim
+++ beacon_chain/fork_choice/fork_choice_types.nim
@@ -195,16 +195,6 @@ type
     root*: Eth2Digest
     payloadStatus*: PayloadStatus
 
-const
-  # https://github.com/ethereum/consensus-specs/blob/v1.7.0-alpha.2/specs/gloas/fork-choice.md#constants
-  PAYLOAD_TIMELY_THRESHOLD*: uint64 = PTC_SIZE div 2
-  PAYLOAD_STATUS_PENDING* = PayloadStatus(0)
-  PAYLOAD_STATUS_EMPTY* = PayloadStatus(1)
-  PAYLOAD_STATUS_FULL* = PayloadStatus(2)
-  ATTESTATION_TIMELINESS_INDEX*: uint8 = 0
-  PTC_TIMELINESS_INDEX*: uint8 = 1
-  NUM_BLOCK_TIMELINESS_DEADLINES*: uint8 = 2
-
 func shortLog*(vote: VoteTracker): auto =
   (
     slot: vote.slot,
