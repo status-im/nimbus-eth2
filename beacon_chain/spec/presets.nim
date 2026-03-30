89a38a3ad7f969707e6d2a84f7f78e3acb465606
Caleb <calebomoniyitomiwa@gmail.com>
2026-03-30 22:27:26 +0100

    update copyright years

--- beacon_chain/spec/presets.nim
+++ beacon_chain/spec/presets.nim
@@ -713,7 +713,7 @@ elif const_preset == "minimal":
     SECONDS_PER_ETH1_BLOCK: 14,
     # 2**8 (= 256) epochs
     MIN_VALIDATOR_WITHDRAWABILITY_DELAY: 256,
-    # 2**6 (= 64) epochs ~7 hours
+    # 2**1 (= 2) epochs
     MIN_BUILDER_WITHDRAWABILITY_DELAY: 2,
     # [customized] higher frequency of committee turnover and faster time to acceptable voluntary exit
     SHARD_COMMITTEE_PERIOD: 64,
