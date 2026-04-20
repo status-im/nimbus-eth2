ff36fea8f992b448677048b16b324cfcdda94911
Agnish Ghosh <iamagnix@gmail.com>
2026-04-20 15:59:28 +0530

    add checkConfigConsistency

--- beacon_chain/spec/datatypes/base.nim
+++ beacon_chain/spec/datatypes/base.nim
@@ -1045,6 +1045,8 @@ func checkForkConsistency*(cfg: RuntimeConfig) =
 
   doAssert isSorted(cfg.BLOB_SCHEDULE, cmp = cmpBlobParameters)
 
+func checkConfigConsistency*(cfg: RuntimeConfig) =
+  cfg.checkForkConsistency()
   doAssert cfg.NUMBER_OF_CUSTODY_GROUPS <= NUMBER_OF_COLUMNS
 
 func ofLen[T, N](ListType: type List[T, N], n: int): ListType =
