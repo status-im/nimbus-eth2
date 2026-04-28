5bffe48c1b3e9d6a127715002e29c7ebefb37e33
Caleb <calebomoniyitomiwa@gmail.com>
2026-04-28 10:22:23 +0100

    optimize data column pruning and prune payload envelopes along blocks

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -1766,20 +1766,17 @@ proc pruneDataColumns(node: BeaconNode, slot: Slot) =
   let dataColumnPruneEpoch = (slot.epoch -
                               node.dag.cfg.MIN_EPOCHS_FOR_BLOB_SIDECARS_REQUESTS - 1)
   if slot.is_epoch() and dataColumnPruneEpoch >= node.dag.cfg.FULU_FORK_EPOCH:
+    let consensusFork = node.dag.cfg.consensusForkAtEpoch(dataColumnPruneEpoch)
+    if consensusFork < ConsensusFork.Fulu:
+      return
+
     var blocks: array[SLOTS_PER_EPOCH.int, BlockId]
     var count = 0
     let startIndex = node.dag.getBlockRange(
       dataColumnPruneEpoch.start_slot, blocks.toOpenArray(0, SLOTS_PER_EPOCH - 1))
     for i in startIndex..<SLOTS_PER_EPOCH:
-      let blck = node.dag.getForkedBlock(blocks[int(i)]).valueOr: continue
-      withBlck(blck):
-        when consensusFork < ConsensusFork.Fulu: continue
-        else:
-          # Iterate the full column space rather than just the local custody
-          # set so late-arriving or reconstructed columns outside of this
-          # node's custody groups are also cleaned up.
-          count += node.db.delDataColumnSidecars(
-            consensusFork, blocks[int(i)].root)
+      count += node.db.delDataColumnSidecars(
+        consensusFork, blocks[int(i)].root)
     debug "pruned data columns", count, dataColumnPruneEpoch
 
 proc reconstructDataColumns(node: BeaconNode, slot: Slot) =
