c4474aaa46312f3377ed3d567a6cfca479a108af
Sam Shum <4080806+ahshum@users.noreply.github.com>
2026-04-28 16:34:08 +0100

    chore: ref and description

--- beacon_chain/gossip_processing/block_processor.nim
+++ beacon_chain/gossip_processing/block_processor.nim
@@ -960,6 +960,10 @@ proc storePayload(
           chronos.nanoseconds((slotTime - wallTime).nanoseconds)
     deadline = sleepAsync(deadlineTime)
 
+  # https://github.com/ethereum/beacon-APIs/blob/31f7d04f869d40a643b68ac22e10fb27644d20e7/apis/eventstream/index.yaml
+  # execution_payload_available: The node has verified that the execution
+  # payload and blobs for a block are available and ready for payload
+  # attestation
   if not isNil(dag.onEnvelopeAvailable):
     dag.onEnvelopeAvailable(signedEnvelope)
 
