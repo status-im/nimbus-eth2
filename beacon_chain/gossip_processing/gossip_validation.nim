84de12a669db5200f21d577749618259a0dd19f2
tersec <tersec@users.noreply.github.com>
2026-05-03 16:57:53 +0000

    Apply suggestion from @tersec

--- beacon_chain/gossip_processing/gossip_validation.nim
+++ beacon_chain/gossip_processing/gossip_validation.nim
@@ -18,7 +18,7 @@ import
     helpers, network, signatures, peerdas_helpers],
   ../consensus_object_pools/[
     attestation_pool, blockchain_dag, block_clearance, block_quarantine,
-    column_quarantine,envelope_quarantine, execution_payload_pool,
+    column_quarantine, envelope_quarantine, execution_payload_pool,
     light_client_pool, payload_attestation_pool,spec_cache,
     sync_committee_msg_pool, validator_change_pool],
   ../beacon_clock,
