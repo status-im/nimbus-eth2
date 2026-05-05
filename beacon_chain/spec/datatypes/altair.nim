5890ce5df260af956560145cbc4644713ebc62c5
Etan Kissling <etan@nimbus.team>
2026-05-05 15:38:35 +0200

    Cleanup distinct jank

--- beacon_chain/spec/datatypes/altair.nim
+++ beacon_chain/spec/datatypes/altair.nim
@@ -407,20 +407,22 @@ chronicles.formatIt SyncSubcommitteeIndex: uint8(it)
 template asList*(epochFlags: EpochParticipationFlags): untyped =
   List[ParticipationFlags, Limit VALIDATOR_REGISTRY_LIMIT] epochFlags
 template asList*(epochFlags: var EpochParticipationFlags): untyped =
-  let tmp = cast[ptr List[ParticipationFlags, Limit VALIDATOR_REGISTRY_LIMIT]](addr epochFlags)
-  tmp[]
+  List[ParticipationFlags, Limit VALIDATOR_REGISTRY_LIMIT] epochFlags
 
 template asSeq*(epochFlags: EpochParticipationFlags): untyped =
-  seq[ParticipationFlags] asList(epochFlags)
-
+  distinctBase(epochFlags)
 template asSeq*(epochFlags: var EpochParticipationFlags): untyped =
-  let tmp = cast[ptr seq[ParticipationFlags]](addr epochFlags)
-  tmp[]
+  distinctBase(epochFlags)
 
-template `[]`*(epochFlags: EpochParticipationFlags, idx: ValidatorIndex|uint64|int): ParticipationFlags =
+template `[]`*(
+    epochFlags: EpochParticipationFlags,
+    idx: ValidatorIndex|uint64|int): ParticipationFlags =
   asList(epochFlags)[idx]
 
-template `[]=`*(epochFlags: EpochParticipationFlags, idx: ValidatorIndex, flags: ParticipationFlags) =
+template `[]=`*(
+    epochFlags: var EpochParticipationFlags,
+    idx: ValidatorIndex|uint64|int,
+    flags: ParticipationFlags) =
   asList(epochFlags)[idx] = flags
 
 template add*(epochFlags: var EpochParticipationFlags, flags: ParticipationFlags): bool =
@@ -435,19 +437,15 @@ template high*(epochFlags: EpochParticipationFlags): int =
   asSeq(epochFlags).high
 
 template assign*(v: var EpochParticipationFlags, src: EpochParticipationFlags) =
-  # TODO https://github.com/nim-lang/Nim/issues/21123
   mixin assign
-  var tmp = cast[ptr seq[ParticipationFlags]](addr v)
-  assign(tmp[], distinctBase src)
+  assign(distinctBase(v), distinctBase(src))
 
 template toSszType*(v: EpochParticipationFlags): auto = asList v
 
 func fromSszBytes*(
     T: type EpochParticipationFlags, bytes: openArray[byte]
 ): T {.raises: [SszError].} =
-  # TODO https://github.com/nim-lang/Nim/issues/21123
-  let tmp = cast[ptr List[ParticipationFlags, Limit VALIDATOR_REGISTRY_LIMIT]](addr result)
-  readSszValue(bytes, tmp[])
+  readSszValue(bytes, asList(result))
 
 template `[]`*(a: auto; i: SyncSubcommitteeIndex): auto =
   a[i.asInt]
