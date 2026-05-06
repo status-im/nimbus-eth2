544f320e409b2bf7cd9a91ccd1ecdb4ec93fb995
Sam Shum <4080806+ahshum@users.noreply.github.com>
2026-05-06 11:23:09 +0100

    feat: update quarantines to use MissingTable

--- beacon_chain/consensus_object_pools/envelope_quarantine.nim
+++ beacon_chain/consensus_object_pools/envelope_quarantine.nim
@@ -10,21 +10,17 @@
 import
   std/tables,
   minilru,
+  ./quarantine_types,
   ../spec/[digest, forks]
 
 const
-  MaxRetriesPerMissingItem = 7
-    ## Exponential backoff, double interval between each attempt
-  MaxMissingItems* = 1024
-    ## Revisit the setting and same as block quarantine for now
   MaxUnviables = 16 * 1024
     ## Set to same as max unviable blocks.
 
+export quarantine_types
+
 type
   UnviableLru = LruCache[Eth2Digest, ()]
-
-  MissingEnvelope* = object
-    tries*: int
 
   EnvelopeQuarantine* = object
     orphans*: Table[Eth2Digest, Table[uint64, SignedExecutionPayloadEnvelope]]
@@ -32,7 +28,7 @@ type
       ## ideal scenario, block should arrive before envelope but that is not
       ## guaranteed.
 
-    missing*: Table[Eth2Digest, MissingEnvelope]
+    missing*: MissingTable
       ## List of block roots that we would like to have the envelopes but we
       ## have not got yet. Missing envelopes should usually be found when we
       ## received a block, blob or data column.
@@ -43,40 +39,20 @@ type
 func init*(T: typedesc[EnvelopeQuarantine]): T =
   T(
     unviable: UnviableLru.init(MaxUnviables),
+    missing: MissingTable.init(),
   )
 
-template root(v: SignedExecutionPayloadEnvelope): Eth2Digest =
-  v.message.beacon_block_root
+func addMissing*(self: var EnvelopeQuarantine, root: Eth2Digest) =
+  self.missing.add(root)
 
-func addMissing*(
-    self: var EnvelopeQuarantine,
-    root: Eth2Digest) =
-  if self.missing.len() >= MaxMissingItems:
-    return
-  discard self.missing.hasKeyOrPut(root, MissingEnvelope())
-
-func checkMissing*(self: var EnvelopeQuarantine, max: int): seq[Eth2Digest] =
-  # Remove items that have reached max retries
-  var done: seq[Eth2Digest]
-  for k, v in self.missing.mpairs():
-    if v.tries > static(1 shl MaxRetriesPerMissingItem):
-      done.add(k)
-  for k in done:
-    self.missing.del(k)
-
-  # Get items
-  for k, v in self.missing.mpairs():
-    v.tries += 1
-    if countOnes(v.tries.uint64) == 1:
-      result.add(k)
-      if result.len >= max:
-        break
+func checkMissing*(self: var EnvelopeQuarantine, max: int): seq[FetchRecord] =
+  self.missing.checkMissing(max)
 
 func addOrphan*(
     self: var EnvelopeQuarantine,
     envelope: SignedExecutionPayloadEnvelope) =
   discard self.orphans
-    .mgetOrPut(envelope.root)
+    .mgetOrPut(envelope.message.beacon_block_root)
     .hasKeyOrPut(envelope.message.builder_index, envelope)
 
 func popOrphan*(
