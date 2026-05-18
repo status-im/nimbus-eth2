status-im/nimbus-eth2

commit 1d3f0b8a35b62cef62580d2bbf4e54fc1c977f07

Add config option for controlling LC data backfill (#8445)
* Add config option for controlling LC data backfill

As a first step towards enabling peers to backfill historical LC data in
a trust-minimized way, add a debug flag that toggles activation, default
off for any user-facing instantiations, and default on for all tests.

The eventual protocol will involve these steps:

1. Sync a LightClientStore (either light client or full node)
2. When period P+1 finalizes, fetch LightClientPeriodData for P, with:
   - first_header: BeaconBlockHeader corresponding to block_roots[0]
   - finality_transition_pre: Last block with finalized cp pre P start,
     block_roots[8191] if did not finalize during P, default for genesis
     (header, finalized_checkpoint, finality_branch)
   - finality_transition_post: First block with finalized cp in P,
     has finality_transition_pre as parent (or 0 if genesis as parent),
     default if finality_transition_pre is block_roots[8191]
     (header, finalized_checkpoint, finality_branch)
   - block_roots: All block_roots of P
   - current_sync_committee: Sync committee for P (if post-altair)
   - current_sync_committee_branch: Proof relative to state_roots[8191]
   - latest_state_root: state_root after slot 8191 into P
   - latest_state_root_branch: Proof relative to htr(state_roots)
   - historical_summary_branch: HistoricalSummary can be recovered from
     block_roots + latest_state_root + latest_state_root_branch. This
     proofs htr(HistoricalSummary) relative to optimistic_update.header.
   - optimistic_update: Reduced version of best known LightClientUpdate
     for P+x with x >= 1 but as small as possible, makes it canonical
3. Assumption, client has full knowledge of periods > P from backfill
   or from local full node import. Verify optimistic_update is best, if
   P+1 is final, alternatively have to re-fetch later for canonical best
4. Fetch LightClientBlockData for post-altair epochs in P, with:

beacon_chain/nimbus_beacon_node.nim
@@ -154,7 +154,9 @@ proc setupDatabase(
 ): Future[Opt[BeaconChainDB]] {.async: (raises: [CancelledError]).} =
   # Open the database and initialize it with genesis/checkpoint if it wasn't
   # setup before - fails if the data sources we use are broken
-  let db = BeaconChainDB.new(config.databaseDir, metadata.cfg, inMemory = false)
+  let db = BeaconChainDB.new(
+    config.databaseDir, metadata.cfg, inMemory = false,
+    lightClientDataImportBackfill = config.lightClientDataImportBackfill)
 
   if ChainDAGRef.isInitialized(db).isOk():
     if config.finalizedCheckpointState.isSome:
@@ -398,6 +400,7 @@ proc loadChainDag(
     lcDataConfig = LightClientDataConfig(
       serve: config.lightClientDataServe,
       importMode: config.lightClientDataImportMode,
+      importBackfill: config.lightClientDataImportBackfill,
       maxPeriods: config.lightClientDataMaxPeriods,
       onLightClientFinalityUpdate: onLightClientFinalityUpdateCb,
       onLightClientOptimisticUpdate: onLightClientOptimisticUpdateCb))
@@ -2933,7 +2936,9 @@ proc handleStartUpCmd(config: var BeaconNodeConf) {.raises: [CatchableError].} =
 
     let
       metadata = loadEth2Network(config)
-      db = BeaconChainDB.new(config.databaseDir, metadata.cfg, inMemory = false)
+      db = BeaconChainDB.new(
+        config.databaseDir, metadata.cfg, inMemory = false,
+        lightClientDataImportBackfill = config.lightClientDataImportBackfill)
       genesisState = (waitFor fetchGenesisState(metadata, config.eraDir)).valueOr:
         quit 1
     waitFor db.doRunTrustedNodeSync(

beacon_chain/beacon_chain_db.nim
@@ -505,7 +505,8 @@ proc new*(T: type BeaconChainDBV0,
 
 proc new*(T: type BeaconChainDB,
           db: SqStoreRef,
-          cfg: RuntimeConfig
+          cfg: RuntimeConfig,
+          lightClientDataImportBackfill = false
     ): BeaconChainDB =
   if not db.readOnly:
     # Remove the deposits table we used before we switched
@@ -679,7 +680,8 @@ proc new*(T: type BeaconChainDB,
           dir: string,
           cfg: RuntimeConfig,
           inMemory = false,
-          readOnly = false
+          readOnly = false,
+          lightClientDataImportBackfill = false
     ): BeaconChainDB =
   let db =
     if inMemory:
@@ -694,7 +696,7 @@ proc new*(T: type BeaconChainDB,
 
       SqStoreRef.init(
         dir, "nbc", readOnly = readOnly, manualCheckpoint = true).expectDb()
-  BeaconChainDB.new(db, cfg)
+  BeaconChainDB.new(db, cfg, lightClientDataImportBackfill)
 
 template getQuarantineDB*(db: BeaconChainDB): QuarantineDB =
   db.quarantine

beacon_chain/conf.nim
@@ -554,6 +554,12 @@ type
         defaultValueDesc: $LightClientDataImportMode.OnlyNew
         name: "light-client-data-import-mode" .}: LightClientDataImportMode
 
+      lightClientDataImportBackfill* {.
+        hidden
+        desc: "Collect additional data for enabling peers to backfill light client data (experimental)"
+        defaultValue: false
+        name: "debug-light-client-data-import-backfill" .}: bool
+
       lightClientDataMaxPeriods* {.
         desc: "Maximum number of sync committee periods to retain light client data"
         name: "light-client-data-max-periods" .}: Option[uint64]

beacon_chain/consensus_object_pools/block_pools_types_light_client.nim
@@ -62,6 +62,8 @@ type
       ## Whether to make local light client data available or not
     importMode*: LightClientDataImportMode
       ## Which classes of light client data to import
+    importBackfill*: bool
+      ## Whether to collect data for backfilling historical light client data
     maxPeriods*: Option[uint64]
       ## Maximum number of sync committee periods to retain light client data
     onLightClientFinalityUpdate*: OnLightClientFinalityUpdateCallback
@@ -85,6 +87,8 @@ type
       ## Whether to make local light client data available or not
     importMode*: LightClientDataImportMode
       ## Which classes of light client data to import
+    importBackfill*: bool
+      ## Whether to collect data for backfilling historical light client data
     maxPeriods*: uint64
       ## Maximum number of sync committee periods to retain light client data
 

beacon_chain/consensus_object_pools/blockchain_dag_light_client.nim
@@ -110,6 +110,7 @@ proc initLightClientDataStore*(
     db: db,
     serve: config.serve,
     importMode: config.importMode,
+    importBackfill: config.importBackfill,
     maxPeriods: maxPeriods,
     onLightClientFinalityUpdate: config.onLightClientFinalityUpdate,
     onLightClientOptimisticUpdate: config.onLightClientOptimisticUpdate)

tests/consensus_spec/test_fixture_light_client_data_collection.nim
@@ -141,7 +141,8 @@ proc runTest(suiteName, path: string, consensusFork: static ConsensusFork) =
       validatorMonitor = newClone(ValidatorMonitor.init(cfg))
       dag = ChainDAGRef.init(cfg, db, validatorMonitor, {},
         lcDataConfig = LightClientDataConfig(
-          serve: true, importMode: LightClientDataImportMode.Full))
+          serve: true, importMode: LightClientDataImportMode.Full,
+          importBackfill: true))
       rng = HmacDrbgContext.new()
       taskpool = Taskpool.new()
     var

tests/test_light_client.nim
@@ -84,7 +84,8 @@ suite "Light client" & preset():
         cfg, cfg.makeTestDB(numValidators), validatorMonitor, {},
         lcDataConfig = LightClientDataConfig(
           serve: true,
-          importMode: LightClientDataImportMode.OnlyNew))
+          importMode: LightClientDataImportMode.OnlyNew,
+          importBackfill: true))
       quarantine = newClone(Quarantine.init(cfg))
       rng = HmacDrbgContext.new()
       taskpool = Taskpool.new()
@@ -238,13 +239,14 @@ suite "Light client" & preset():
         let finalizedSlot = (epoch + 2).start_slot
         dag.advanceToSlot(finalizedSlot, verifier, quarantine[])
 
-        let cpDb = BeaconChainDB.new("", cfg, inMemory = true)
+        let cpDb = BeaconChainDB.new(
+          "", cfg, inMemory = true, lightClientDataImportBackfill = true)
         ChainDAGRef.preInit(cpDb, genesisState[])
         ChainDAGRef.preInit(cpDb, dag.headState)
         let cpDag = ChainDAGRef.init(
           cfg, cpDb, validatorMonitor, {},
           lcDataConfig = LightClientDataConfig(
-            serve: true, importMode: importMode))
+            serve: true, importMode: importMode, importBackfill: true))
 
         for i in 1'u64 .. 10:
           let headSlot = (finalizedSlot.epoch + i).start_slot

tests/test_light_client_processor.nim
@@ -46,7 +46,8 @@ suite "Light client processor" & preset():
       cfg, cfg.makeTestDB(numValidators), validatorMonitor, {},
       lcDataConfig = LightClientDataConfig(
         serve: true,
-        importMode: LightClientDataImportMode.OnlyNew))
+        importMode: LightClientDataImportMode.OnlyNew,
+        importBackfill: true))
     quarantine = newClone(Quarantine.init(dag.cfg))
     rng = HmacDrbgContext.new()
     taskpool = Taskpool.new()

commit 359c94e9b03627d4ee8775210e8f1ce8cb2afd0a

Fix lightClientDataImportBackfill property getting accessed in tns
When starting in trustedNodeSync mode, the lightClientDataImportBackfill
property is not available, so don't try to access it, use default.
Regression from #8445

beacon_chain/nimbus_beacon_node.nim
@@ -2940,8 +2940,7 @@ proc handleStartUpCmd(config: var BeaconNodeConf) {.raises: [CatchableError].} =
     let
       metadata = loadEth2Network(config)
       db = BeaconChainDB.new(
-        config.databaseDir, metadata.cfg, inMemory = false,
-        lightClientDataImportBackfill = config.lightClientDataImportBackfill)
+        config.databaseDir, metadata.cfg, inMemory = false)
       genesisState = (waitFor fetchGenesisState(metadata, config.eraDir)).valueOr:
         quit 1
     waitFor db.doRunTrustedNodeSync(