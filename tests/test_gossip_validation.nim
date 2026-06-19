6b8a19b838e4c2d1c8e61aaa35d068ae451e628d
Caleb <calebomoniyitomiwa@gmail.com>
2026-06-19 16:46:20 +0100

    update test

--- tests/test_gossip_validation.nim
+++ tests/test_gossip_validation.nim
@@ -499,7 +499,8 @@ suite "Proposer preferences validation " & preset():
       dag = ChainDAGRef.init(
         cfg, cfg.makeTestDB(SLOTS_PER_EPOCH * 3),
         validatorMonitor, {})
-      seen: array[2, array[SLOTS_PER_EPOCH, seq[ProposerPreferences]]]
+      seen: array[2, array[
+        SLOTS_PER_EPOCH, Table[Eth2Digest, ProposerPreferences]]]
 
     # Pick an upcoming slot and its scheduled proposer from the head state's
     # proposer_lookahead.
