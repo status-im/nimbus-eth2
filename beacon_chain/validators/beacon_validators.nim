f6249efcf144188db32a7a8adb48014dda2d8e0b
Etan Kissling <etan@nimbus.team>
2026-06-19 20:56:15 +0200

    Check validator registration to match latest params
    We check for the situation where validator is removed via keymanager
    while being registered (async), but we don't handle case where settings
    are updated via keymanager for fee recipient and/or gas limit.
    Treat these same / don't save to validator.externalBuilderRegistration.

--- beacon_chain/validators/beacon_validators.nim
+++ beacon_chain/validators/beacon_validators.nim
@@ -1291,7 +1291,7 @@ proc getValidatorRegistration(
   if validator.index.isNone:
     # The validator index will be missing when the validator was not
     # activated for duties yet. We can safely skip the registration then.
-    return
+    return err("Validator not yet activated")
 
   let
     feeRecipient = node.getFeeRecipient(validator.pubkey, validator.index, epoch)
@@ -1314,6 +1314,16 @@ proc getValidatorRegistration(
     )
 
   ok validatorRegistration
+
+proc isConsistent(
+    node: BeaconNode,
+    validatorRegistration: SignedValidatorRegistrationV1,
+    validator: AttachedValidator,
+    epoch: Epoch): bool =
+  validatorRegistration.message.fee_recipient ==
+    node.getFeeRecipient(validator.pubkey, validator.index, epoch) and
+  validatorRegistration.message.gas_limit ==
+    node.getGasLimit(validator.pubkey)
 
 proc registerValidatorsPerBuilder(
     node: BeaconNode, payloadBuilderAddress: string, epoch: Epoch,
@@ -1412,22 +1422,18 @@ proc registerValidatorsPerBuilder(
       addValidatorRegistration validator.externalBuilderRegistration.get
     else:
       let validatorRegistration =
-        await node.getValidatorRegistration(validator, epoch)
-      if validatorRegistration.isErr:
-        error "registerValidators: validatorRegistration failed",
-                validatorRegistration
+          (await node.getValidatorRegistration(validator, epoch)).valueOr:
+        error "registerValidators: validatorRegistration failed", reason = error
         continue
 
-      # Time passed during await; REST keymanager API might have removed it
-      if key notin node.attachedValidators[].validators:
+      # Time passed during await; REST keymanager API might change / remove it
+      node.attachedValidators[].validators.withValue(key, validator):
+        if not node.isConsistent(validatorRegistration, validator[], epoch):
+          continue
+        validator[].externalBuilderRegistration.ok validatorRegistration
+        addValidatorRegistration validatorRegistration
+      do:
         continue
-      let validator = try:
-        node.attachedValidators[].validators[key]
-      except KeyError:
-        raiseAssert "just checked"
-
-      validator.externalBuilderRegistration = Opt.some validatorRegistration.get
-      addValidatorRegistration validatorRegistration.get
 
   if validatorRegistrations == emptyNestedSeq:
     return
