4a4ac9c1d1a230eaa0ed83fb2ab85acd8df35391
tersec <tersec@users.noreply.github.com>
2026-07-07 04:49:10 +0000

    enable QUIC by default (#8711)

--- beacon_chain/conf_light_client.nim
+++ beacon_chain/conf_light_client.nim
@@ -76,11 +76,10 @@ type LightClientConf* = object
     name: "debug-quic" .}: bool
 
   quicPort* {.
-    hidden
     desc: "Listening UDP port for Ethereum LibP2P traffic over QUIC"
     defaultValue: defaultEth2QuicPort
     defaultValueDesc: $defaultEth2QuicPortDesc
-    name: "debug-quic-port" .}: Port
+    name: "quic-port" .}: Port
 
   udpPort* {.
     desc: "Listening UDP port for node discovery"
