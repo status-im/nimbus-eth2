7c29aa3f85d70d0f88d4fe6340e0d714127fd5b4
Etan Kissling <etan@nimbus.team>
2026-07-08 08:10:37 +0200

    Heze constants

--- beacon_chain/rpc/rest_config_api.nim
+++ beacon_chain/rpc/rest_config_api.nim
@@ -518,8 +518,10 @@ proc installConfigApiHandlers*(router: var RestRouter, node: BeaconNode) =
             Base10.toString(MAX_SIGNED_BEACON_BLOCK_SIZE),
           INCLUSION_LIST_COMMITTEE_SIZE:
             Base10.toString(uint64(INCLUSION_LIST_COMMITTEE_SIZE)),
-          MAX_SIGNED_INCLUSION_LIST_SIZE:
-            Base10.toString(MAX_SIGNED_INCLUSION_LIST_SIZE),
+          MAX_SIGNED_EXECUTION_PAYLOAD_BID_SIZE_HEZE:
+            Base10.toString(MAX_SIGNED_EXECUTION_PAYLOAD_BID_SIZE_HEZE),
+          MAX_SIGNED_INCLUSION_LIST_SIZE_HEZE:
+            Base10.toString(MAX_SIGNED_INCLUSION_LIST_SIZE_HEZE),
         )
       )
     cachedDepositContract =
