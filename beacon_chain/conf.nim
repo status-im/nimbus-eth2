5e5307f1bfd47d5fa986cd6042b65b716b56c5c6
tersec <tersec@users.noreply.github.com>
2026-07-14 09:24:00 +0000

    enable QUIC by default

--- beacon_chain/conf.nim
+++ beacon_chain/conf.nim
@@ -318,7 +318,7 @@ type
       quicEnabled* {.
         hidden
         desc: "Enable QUIC transport"
-        defaultValue: false
+        defaultValue: true
         name: "debug-quic" .}: bool
 
       quicPort* {.
@@ -848,7 +848,7 @@ type
         quicExtEnabled* {.
           hidden
           desc: "Enable QUIC transport"
-          defaultValue: false
+          defaultValue: true
           name: "debug-quic" .}: bool
 
         quicPortExt* {.
