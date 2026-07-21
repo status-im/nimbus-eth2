85045c2ea40579a80116db854e8e3cf630443679
ahshum <4080806+ahshum@users.noreply.github.com>
2026-07-21 10:35:44 +0100

    Add verifier error for payload (#8778)
    * feat: add payload verifier error
    
    * refactor: use payload error
    
    * refactor: verify forky payload on CL
    
    * chore: style
    
    * refactor: error message

--- beacon_chain/sync/request_manager.nim
+++ beacon_chain/sync/request_manager.nim
@@ -56,7 +56,7 @@ type
 
   EnvelopeVerifierFn = proc(
       signedEnvelope: gloas.SignedExecutionPayloadEnvelope,
-  ): Future[Result[void, VerifierError]] {.async: (raises: [CancelledError]).}
+  ): Future[Result[void, PayloadVerifierError]] {.async: (raises: [CancelledError]).}
 
   BlockLoaderFn = proc(
       blockRoot: Eth2Digest
@@ -316,16 +316,13 @@ proc fetchEnvelopesFromNetwork(self: RequestManager, roots: seq[Eth2Digest])
           let res = await self.envelopeVerifier(envelope[])
           if res.isErr():
             case res.error():
-            of VerifierError.MissingParent:
-              # Ignoring due to it should have checked in processing the valid
-              # block.
+            of PayloadVerifierError.MissingParent,
+                PayloadVerifierError.Duplicate,
+                PayloadVerifierError.InvalidSidecars:
               discard
-            of VerifierError.Duplicate:
-              # Ignoring as it could occur when making parallel requests.
-              discard
-            of VerifierError.UnviableFork:
+            of PayloadVerifierError.UnviableFork:
               gotUnviableEnvelope = true
-            of VerifierError.Invalid:
+            of PayloadVerifierError.Invalid:
               notice "Received invalid envelope",
                 peer = peer, envelopes = shortLog(roots)
               peer.updateScore(PeerScoreBadValues)
@@ -615,7 +612,7 @@ proc requestManagerEnvelopeLoop(self: RequestManager)
       assign(blockRoots, missingBlockRoots)
     else:
       var verifiers:
-        seq[Future[Result[void, VerifierError]].Raising([CancelledError])]
+        seq[Future[Result[void, PayloadVerifierError]].Raising([CancelledError])]
       for blockRoot in missingBlockRoots:
         let envelope = self.envelopeLoader(blockRoot).valueOr:
           blockRoots.add blockRoot
