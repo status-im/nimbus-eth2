31b321558b380308131295d4c43d0631daf2789f
tersec <tersec@users.noreply.github.com>
2026-07-27 11:33:12 +0000

    rm old --rpc warning messages (#8807)

--- beacon_chain/nimbus_beacon_node.nim
+++ beacon_chain/nimbus_beacon_node.nim
@@ -2676,9 +2676,6 @@ proc doRunBeaconNode(
 
   createPidFile(config.dataDir.string / "beacon_node.pid")
 
-  if config.rpcEnabled.isSome:
-    warn "Nimbus's JSON-RPC server has been removed. This includes the --rpc, --rpc-port, and --rpc-address configuration options. https://nimbus.guide/rest-api.html shows how to enable and configure the REST Beacon API server which replaces it."
-
   # Trusted setup is needed for Cancun+ blocks and is shared between threads,
   # so it needs to be initalized from the main thread before anything else tries
   # to use it
