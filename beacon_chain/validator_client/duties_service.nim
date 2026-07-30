status-im/nimbus-eth2

commit 0f535ba8ac81e9259d6f47a7b9233415e33bdef6

Add usage of postStateValidators instead of getStateValidators. (#8822)

beacon_chain/validator_client/duties_service.nim
@@ -53,22 +53,19 @@ proc pollForValidatorIndices*(
 ) {.async: (raises: [CancelledError]).} =
   let vc = service.client
 
-  let validatorIdents =
-    block:
-      var res: seq[ValidatorIdent]
-      for validator in vc.attachedValidators[].items():
-        if validator.needsUpdate():
-          res.add(ValidatorIdent.init(validator.pubkey))
-      res
-
-  let start = Moment.now()
-
-  var validators: seq[RestValidator]
-
-  for idents in chunks(validatorIdents, ClientMaximumValidatorIds):
-    let res =
+  let
+    validatorIdents =
+      block:
+        var res: seq[ValidatorIdent]
+        for validator in vc.attachedValidators[].items():
+          if validator.needsUpdate():
+            res.add(ValidatorIdent.init(validator.pubkey))
+        res
+    start = Moment.now()
+    validators =
       try:
-        await vc.getValidators(idents, vc.getMode()[FnKind.getValidators])
+        await vc.postValidators(
+          validatorIdents, vc.getMode()[FnKind.getValidators])
       except ValidatorApiError as exc:
         warn "Unable to get head state's validator information",
               reason = exc.getFailureReason()
@@ -76,9 +73,6 @@ proc pollForValidatorIndices*(
       except CancelledError as exc:
         debug "Validator's indices processing was interrupted"
         raise exc
-
-    for item in res:
-      validators.add(item)
 
   var
     missing: seq[string]

beacon_chain/spec/eth2_apis/eth2_rest_serialization.nim
@@ -62,7 +62,8 @@ type
     SignedVoluntaryExit |
     Web3SignerRequest |
     RestNimbusTimestamp1 |
-    SetGraffitiRequest
+    SetGraffitiRequest |
+    RestValidatorRequest
 
   EncodeOctetTypes* =
     altair.SignedBeaconBlock |

beacon_chain/spec/eth2_apis/rest_beacon_calls.nim
@@ -60,6 +60,14 @@ proc getStateValidatorsPlain*(
      meth: MethodGet.}
   ## https://ethereum.github.io/beacon-APIs/#/Beacon/getStateValidators
 
+proc postStateValidatorsPlain*(
+    state_id: StateIdent,
+    body: RestValidatorRequest
+): RestPlainResponse {.
+    rest, endpoint: "/eth/v1/beacon/states/{state_id}/validators",
+    meth: MethodPost.}
+  ## https://ethereum.github.io/beacon-APIs/#/Beacon/postStateValidators
+
 proc getStateValidatorPlain*(state_id: StateIdent,
                         validator_id: ValidatorIdent
                        ): RestPlainResponse {.

beacon_chain/validator_client/api.nim
@@ -1386,6 +1386,103 @@ proc getHeadBlockRoot*(
     raise (ref ValidatorApiError)(
       msg: "Failed to get head block root", data: failures)
 
+proc postValidators*(
+    vc: ValidatorClientRef,
+    id: seq[ValidatorIdent],
+    strategy: ApiStrategyKind
+): Future[seq[RestValidator]] {.
+   async: (raises: [CancelledError, ValidatorApiError]).} =
+  const RequestName = "postStateValidators"
+
+  let
+    stateIdent = StateIdent.init(StateIdentType.Head)
+    request = RestValidatorRequest(
+      ids: Opt.some(id), status: Opt.some({ValidatorFilterKind.ActiveOngoing}))
+
+  var failures: seq[ApiNodeFailure]
+
+  case strategy
+  of ApiStrategyKind.First, ApiStrategyKind.Best:
+    let res = vc.firstSuccessParallel(
+      RestPlainResponse,
+      GetStateValidatorsResponse,
+      vc.SlotDuration,
+      ViableNodeStatus,
+      {BeaconNodeRole.Duties},
+      postStateValidatorsPlain(it, stateIdent, request)):
+      if apiResponse.isErr():
+        handleCommunicationError()
+        ApiResponse[GetStateValidatorsResponse].err(apiResponse.error)
+      else:
+        let response = apiResponse.get()
+        case response.status
+        of 200:
+          let res = decodeBytes(GetStateValidatorsResponse, response.data,
+                                response.contentType)
+          if res.isErr():
+            handleUnexpectedData()
+            ApiResponse[GetStateValidatorsResponse].err($res.error)
+          else:
+            let data = res.get()
+            if data.execution_optimistic.get(false):
+              handleOptimistic()
+            ApiResponse[GetStateValidatorsResponse].ok(data)
+        of 400:
+          handle400()
+          ApiResponse[GetStateValidatorsResponse].err(ResponseInvalidError)
+        of 404:
+          handle404()
+          ApiResponse[GetStateValidatorsResponse].err(ResponseNotFoundError)
+        of 500:
+          handle500()
+          ApiResponse[GetStateValidatorsResponse].err(ResponseInternalError)
+        else:
+          handleUnexpectedCode()
+          ApiResponse[GetStateValidatorsResponse].err(ResponseUnexpectedError)
+
+    if res.isErr():
+      raise (ref ValidatorApiError)(msg: res.error, data: failures)
+    return res.get().data
+
+  of ApiStrategyKind.Priority:
+    vc.firstSuccessSequential(RestPlainResponse,
+                              vc.SlotDuration,
+                              ViableNodeStatus,
+                              {BeaconNodeRole.Duties},
+                              postStateValidatorsPlain(
+                                it, stateIdent, request)):
+      if apiResponse.isErr():
+        handleCommunicationError()
+        false
+      else:
+        let response = apiResponse.get()
+        case response.status
+        of 200:
+          let res = decodeBytes(GetStateValidatorsResponse, response.data,
+                                response.contentType)
+          if res.isOk():
+            let data = res.get()
+            if data.execution_optimistic.get(false):
+              handleOptimistic()
+            return data.data
+          handleUnexpectedData()
+          false
+        of 400:
+          handle400()
+          false
+        of 404:
+          handle404()
+          false
+        of 500:
+          handle500()
+          false
+        else:
+          handleUnexpectedCode()
+          false
+
+    raise (ref ValidatorApiError)(
+      msg: "Failed to get state's validators", data: failures)
+
 proc getValidators*(
     vc: ValidatorClientRef,
     id: seq[ValidatorIdent],

commit 5f09e630aef1ebaec6b082fee3582790f93e379d

Avoid calling postStateValidators with empty list
In the POST version for getting validators, they made it so that passing
an empty list returns ALL validators. This is different from the GET
version where omitting the list is handled differently from empty list.

If we don't have any validator idents of interest, don't send request.
Regression from #8822.

beacon_chain/validator_client/duties_service.nim
@@ -63,16 +63,23 @@ proc pollForValidatorIndices*(
         res
     start = Moment.now()
     validators =
-      try:
-        await vc.postValidators(
-          validatorIdents, vc.getMode()[FnKind.getValidators])
-      except ValidatorApiError as exc:
-        warn "Unable to get head state's validator information",
-              reason = exc.getFailureReason()
-        return
-      except CancelledError as exc:
-        debug "Validator's indices processing was interrupted"
-        raise exc
+      if len(validatorIdents) == 0:
+        # If the supplied list is empty (i.e. the value is []) [...]
+        # then all validators will be returned.
+        # https://ethereum.github.io/beacon-APIs/#/Beacon/postStateValidators
+        # -> Request body -> Schema -> ids -> Expand all
+        @[]
+      else:
+        try:
+          await vc.postValidators(
+            validatorIdents, vc.getMode()[FnKind.getValidators])
+        except ValidatorApiError as exc:
+          warn "Unable to get head state's validator information",
+                reason = exc.getFailureReason()
+          return
+        except CancelledError as exc:
+          debug "Validator's indices processing was interrupted"
+          raise exc
 
   var
     missing: seq[string]