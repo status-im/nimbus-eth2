0d08a8ccac1c9534bf56672f3b6942762a962220
Etan Kissling <etan@nimbus.team>
2026-08-20 11:41:30 +0200

    Fix edge case when attester duties modify during selections fill

--- beacon_chain/validator_client/selection_proofs.nim
+++ beacon_chain/validator_client/selection_proofs.nim
@@ -146,7 +146,8 @@ proc fillAttestationSelectionProofs*(
               if signature.isSome():
                 vc.attesters.withValue(mreq.validator.pubkey, map):
                   map[].duties.withValue(mreq.slot.epoch(), dap):
-                    dap[].slotSig = signature
+                    if dap[].data.slot == mreq.slot:
+                      dap[].slotSig = signature
           res
 
   if vc.config.distributedEnabled:
@@ -222,8 +223,9 @@ proc fillAttestationSelectionProofs*(
 
         vc.attesters.withValue(validator.pubkey, map):
           map[].duties.withValue(selection.slot.epoch(), dap):
-            dap[].slotSig = Opt.some(selectionProof.toValidatorSig())
-            inc(sigres.selectionsProcessed)
+            if dap[].data.slot == selection.slot:
+              dap[].slotSig = Opt.some(selectionProof.toValidatorSig())
+              inc(sigres.selectionsProcessed)
 
   sigres
 
