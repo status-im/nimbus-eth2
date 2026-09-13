14143bb5e41f13d78fc1f74a617158fe2f7799e8
tersec <tersec@users.noreply.github.com>
2026-09-13 07:41:45 +0000

    avoid hang on missing block on epoch boundary

--- beacon_chain/sync/sync_overseer2.nim
+++ beacon_chain/sync/sync_overseer2.nim
@@ -3533,10 +3533,11 @@ proc finalMonitoringLoop(
         event = events[0]
         checkpoint = dag.headState.finalized_checkpoint
 
-      doAssert(dag.finalizedHead.slot > GENESIS_SLOT)
+      let fblck = dag.finalizedHead.blck
+      doAssert(fblck.slot > GENESIS_SLOT)
       let
-        slot = dag.finalizedHead.slot
-        blockRoot = dag.finalizedHead.blck.root
+        slot = fblck.slot
+        blockRoot = fblck.root
         parentRoot =
           block:
             let parentBid = dag.getBlockIdAtSlot(slot - 1)
