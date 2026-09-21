fd385eb3051dad67228b63e2054abf353434088c
Caleb <calebomoniyitomiwa@gmail.com>
2026-09-21 09:05:54 +0100

    fix

--- beacon_chain/spec/eth2_apis/eth2_rest_serialization.nim
+++ beacon_chain/spec/eth2_apis/eth2_rest_serialization.nim
@@ -79,7 +79,9 @@ type
     fulu_mev.SignedBlindedBeaconBlock |
     BuilderPreferencesRequest |
     SignedBuilderRequestAuth |
-    gloas.SignedBeaconBlock
+    gloas.SignedBeaconBlock |
+    SignedExecutionPayloadEnvelope |
+    SignedExecutionPayloadEnvelopeContents
 
   EncodeArrays* =
     seq[phase0.Attestation] |
