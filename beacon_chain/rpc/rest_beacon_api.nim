6da21c2fe4c54db61fb6e32da03815cf13dc63bf
Caleb <calebomoniyitomiwa@gmail.com>
2026-09-21 20:26:46 +0100

    add block root to validateBeaconApiQueries

--- beacon_chain/rpc/rest_beacon_api.nim
+++ beacon_chain/rpc/rest_beacon_api.nim
@@ -49,6 +49,8 @@ func validateBeaconApiQueries*(key: string, value: string): int =
     0
   of "{block_root}":
     0
+  of "{beacon_block_root}":
+    0
   of "{structure}":
     0
   of "{pubkey}":
