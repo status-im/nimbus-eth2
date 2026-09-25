c961165dbe70c8019e2c53b0c571e0711aa85123
tersec <tersec@users.noreply.github.com>
2026-09-25 02:13:36 +0000

    it's fine/expected by spec to return pre-ea slot columns in req/resp (#9121)

--- beacon_chain/sync/sync_protocol.nim
+++ beacon_chain/sync/sync_protocol.nim
@@ -589,11 +589,18 @@ p2pProtocol BeaconSync(version = 1,
           continue
         requiredBid = bsid.bid
 
-      # The requested block predates the earliest slot for which we can
-      # guarantee serving data columns - respond with `ResourceUnavailable`.
-      if requiredBid.slot < dag.earliestAvailableSlot():
-        raise newException(ResourceUnavailableError, DataColumnsOutOfRange)
-
+      # https://github.com/ethereum/consensus-specs/tree/v1.7.0-beta.2/specs/fulu/p2p-interface.md#datacolumnsidecarsbyroot-v1
+      # "It may be less in the case that the responding peer is missing blocks
+      # or sidecars."
+      # "If any root in the request content references a block earlier than
+      # `minimum_request_epoch`, peers MAY respond with error code
+      # `3: ResourceUnavailable` or not include the data column sidecar in the
+      # response."
+      # "Clients MUST respond with at least one sidecar, if they have it."
+      #
+      # https://github.com/ethereum/consensus-specs/tree/v1.7.0-beta.2/specs/fulu/p2p-interface.md#status-v2
+      # "it should advertise the earliest slot from which it can serve all
+      # sidecars."
       if requiredBid.slot.epoch < epochBoundary:
         continue
 
